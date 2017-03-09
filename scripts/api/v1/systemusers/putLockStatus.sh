4a7cdc97dfd3a39f88e239676242fa4f85ffc329
Rob Holden <rob.holden@jumpcloud.com>
2017-03-09 14:19:54 -0700

    fixed filenames

---
+++ scripts/api/v1/systemusers/putLockStatus.sh
@@ -0,0 +1,27 @@
+# Require API Key to run
+
+readApiKey() {
+echo -n "Enter your API Key: "
+        read apiKey;
+if [ -z "$apiKey" ]
+        then
+                echo "Input cannot be null"; readApiKey;
+        else apiCall;
+fi
+}
+
+# API call example
+
+apiCall() { 
+
+curl \
+  -X 'PUT' \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -H "x-api-key: ${apiKey}" \
+  -d '{ "account_locked" : "false" } ' \
+  "https://console.jumpcloud.com/api/systemusers/:id"
+
+}
+
+readApiKey
