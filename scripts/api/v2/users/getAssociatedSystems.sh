a386ee5a65e99a7c866e0db9e75e471423b433af
Rob Holden <rob.holden@jumpcloud.com>
2017-06-15 15:26:04 -0600

    get all systems associated with a user

---
+++ scripts/api/v2/users/getAssociatedSystems.sh
@@ -0,0 +1,29 @@
+# Require API Key to run
+
+readApiKey() {
+
+echo -n "Enter your API Key: "
+        read apiKey;
+if [ -z "$apiKey" ]
+        then
+                echo "Input cannot be null"; readApiKey;
+        else apiCall;
+fi
+
+}
+
+# API call example
+# user :id required in below URL
+
+apiCall() { 
+
+curl \
+  -X 'GET' \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -H "x-api-key: ${apiKey}" \
+  "https://console.jumpcloud.com/api/v2/users/:id/systems"
+
+}
+
+readApiKey
