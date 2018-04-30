b6095f28bd7c39a99863cab5d8c988c2b82746ee
John P. Vajda <jpvajda@users.noreply.github.com>
2018-04-30 16:51:26 -0600

    added script to get mfa totp enabled users

---
+++ scripts/api/v1/systemusers/get_MFA_systemusers_TOTP_enabled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Use this script to return all users in your Organization that have TOTP enabled. 
+# This script works in Python3 only.
+
+import json
+import sys
+import requests
+
+apikey=input('Please enter your API key:  ')
+
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json','x-api-key':''+apikey+''}
+url = 'https://console.jumpcloud.com/api/search/systemusers'
+payload={'filter': [{'totp_enabled' : 'true'}], 'fields' : 'name username email'}
+response = requests.post(url, headers = headers, data=json.dumps(payload))
+json_data =json.loads(response.text)
+print(json_data)
