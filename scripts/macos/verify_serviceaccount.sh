4e1c28dd52fe390314fd9ac47208a72ede978300
Scott Reed <34069891+scottd3v@users.noreply.github.com>
2018-07-18 14:07:59 -0600

    Update verify_serviceaccount.sh

--- scripts/macos/verify_serviceaccount.sh
+++ scripts/macos/verify_serviceaccount.sh
@@ -1,41 +1,41 @@
-#
-# Populate the below variables before running the command
-#
-
-# Enter your connect key within the "" of YOUR_CONNECT_KEY="" with your orgs key found on the new system aside in the admin console
-YOUR_CONNECT_KEY=""
-
-# Enter the SECURETOKEN_ADMIN_USERNAME within the "" of SECURETOKEN_ADMIN_USERNAME=""  
-SECURETOKEN_ADMIN_USERNAME=""
-
-# Enter the SECURETOKEN_ADMIN_PASSWORD within the "" of SECURETOKEN_ADMIN_PASSWORD="" with the credentials of the admin with a secure token 
-SECURETOKEN_ADMIN_PASSWORD=""
-
-#--------------------Do not modify below this line--------------------
-
-MacOSMinorVersion=$(sw_vers -productVersion | cut -d '.' -f 2)
-MacOSPatchVersion=$(sw_vers -productVersion | cut -d '.' -f 3)
-
-if [[ $MacOSMinorVersion -lt 13 ]]; then
-    echo "Error:  Target system is not on macOS 10.13"
-    exit 2
-else
-
-curl --silent --output /tmp/jumpcloud-agent.pkg "https://s3.amazonaws.com/jumpcloud-windows-agent/production/jumpcloud-agent.pkg" > /dev/null
-mkdir -p /opt/jc
-cat <<-EOF > /opt/jc/agentBootstrap.json
-{
-"publicKickstartUrl": "https://kickstart.jumpcloud.com:443",
-"privateKickstartUrl": "https://private-kickstart.jumpcloud.com:443",
-"connectKey": "$YOUR_CONNECT_KEY"
-}
-EOF
-
-
-cat <<-EOF > /var/run/JumpCloud-SecureToken-Creds.txt
-$SECURETOKEN_ADMIN_USERNAME;$SECURETOKEN_ADMIN_PASSWORD
-EOF
-
-installer -pkg /tmp/jumpcloud-agent.pkg -target / &
-fi
-exit 0
+ MacOSMinorVersion=$(sw_vers -productVersion | cut -d '.' -f 2)	 MacOSMinorVersion=$(sw_vers -productVersion | cut -d '.' -f 2)
+ MacOSPatchVersion=$(sw_vers -productVersion | cut -d '.' -f 3)	 MacOSPatchVersion=$(sw_vers -productVersion | cut -d '.' -f 3)
+ 	 
+ if [[ $MacOSMinorVersion -lt 13 ]]; then	 if [[ $MacOSMinorVersion -lt 13 ]]; then
+     echo "Error:  Target system is not on macOS 10.13"	     echo "Error:  Target system is not on macOS 10.13"
+     exit 2	     exit 2
+-fi	+else
+ 	 
+-JCSA_Username="_jumpcloudserviceaccount"	+curl --silent --output /tmp/jumpcloud-agent.pkg "https://s3.amazonaws.com/jumpcloud-windows-agent/production/jumpcloud-agent.pkg" > /dev/null
+-JCSA_FullName="JumpCloud Service Account"	+mkdir -p /opt/jc
++cat <<-EOF > /opt/jc/agentBootstrap.json
++{
++"publicKickstartUrl": "https://kickstart.jumpcloud.com:443",
++"privateKickstartUrl": "https://private-kickstart.jumpcloud.com:443",
++"connectKey": "$YOUR_CONNECT_KEY"
++}
++EOF
+ 	 
+-sysadmin_name="sysadminctl"	
+-if [[ $MacOSMinorVersion -eq 13 ]]; then	
+-    if [[ $MacOSPatchVersion -lt 4 ]]; then	
+-        sysadmin_name="/opt/jc/bin/sysadminkludge"	
+-    fi	
+-fi	
+ 	 
+-result=$($sysadmin_name -secureTokenStatus $JCSA_Username 2>&1 )	+cat <<-EOF > /var/run/JumpCloud-SecureToken-Creds.txt
+-unknown_user=$(echo $result | grep "Unknown user $JCSA_Username")	+$SECURETOKEN_ADMIN_USERNAME;$SECURETOKEN_ADMIN_PASSWORD
+-enabled=$(echo $result | grep "Secure token is ENABLED for user $JCSA_FullName")	+EOF
+ 	 
+-if [[ ! -z $unknown_user ]]; then	+installer -pkg /tmp/jumpcloud-agent.pkg -target / &
+-    echo "Error:  JumpCloud Service Account not installed"	
+-    exit 2	
+ fi	 fi
+-	+exit 0 
+-if [[ -z $enabled ]]; then	
+-    echo "Error:  JumpCloud Service Account does not have a secure token"	
+-    exit 3	
+-fi	
+-	
+-echo "Success: JumpCloud Service Account has been properly created"	
+-exit 0
