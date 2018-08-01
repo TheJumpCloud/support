0d204e1aa47ac4ffcb4b96a097044bd425080343
Tristan Miller <tristan.miller@jumpcloud.com>
2018-08-01 11:57:51 -0600

    Feedback

--- scripts/macos/hide_mac_app.sh
+++ scripts/macos/hide_mac_app.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+if [ $UID != 0 ]; then
+  (>&2 echo "Error:  $0 must be run as root")
+  exit 1
+fi
 defaults write /Library/LaunchAgents/com.jumpcloud.jcagent-tray Disabled -bool true
 mkdir -p /opt/jc_user_ro
 touch /opt/jc_user_ro/dont_show_tray_app
-PERMISSIONS="$(ls -l /opt/jc_user_ro/dont_show_tray_app | cut -c -10)"
-if [ "$PERMISSIONS" != "-rw-r--r--" ]; then
-        echo "Permissions not right for file:  $PERMISSIONS"
-        exit 1
-fi
