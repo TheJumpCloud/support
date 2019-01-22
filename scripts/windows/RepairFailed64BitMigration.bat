5073d3aa6e41545152f449151f6d0c5e61abf362
Jordan Montgomery <jordan.montgomery@jumpcloud.com>
2019-01-22 16:50:40 -0700

    Remove Pauses so this can be run unattended

--- scripts/windows/RepairFailed64BitMigration.bat
+++ scripts/windows/RepairFailed64BitMigration.bat
@@ -4,7 +4,6 @@ rem We don't care about the output of this call, we just use it to verify we are
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Error: This script must be run as administrator
-    pause
     exit /B 1
 )
 
@@ -12,7 +11,6 @@ echo Reconfiguring the jumpcloud-agent service binary path
 sc config jumpcloud-agent binPath= "C:\Program Files\JumpCloud\jumpcloud-agent.exe"
 if %errorlevel% neq 0 (
     echo Error reconfiguring jumpcloud-agent service binary path. Cannot continue.
-    pause
     exit /B 1
 )
     
@@ -20,10 +18,8 @@ echo Restarting the jumpcloud-agent service
 sc start jumpcloud-agent
 if %errorlevel% neq 0 (
     echo Error restarting jumpcloud-agent service. Cannot continue.
-    pause
     exit /B 1
 )
 
 echo Successfully reconfigured and restarted the jumpcloud-agent service
-pause
 exit /B 0
