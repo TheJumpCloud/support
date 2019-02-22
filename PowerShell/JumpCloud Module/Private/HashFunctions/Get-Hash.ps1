9857c06a9a95dcb610e910d07ea27935411cb9b5
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-02-22 09:04:50 -0700

    JCApi Changes

---
+++ PowerShell/JumpCloud Module/Private/HashFunctions/Get-Hash.ps1
@@ -0,0 +1,27 @@
+Function Get-Hash()
+{
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    Param(
+        [Parameter(Mandatory = $true,  ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$URL,
+        [Parameter(Mandatory = $true,  ValueFromPipelineByPropertyName = $true, Position = 1)][ValidateNotNullOrEmpty()][string]$Method,
+        [Parameter(Mandatory = $true,  ValueFromPipelineByPropertyName = $true, Position = 2)][ValidateNotNullOrEmpty()][string]$Key,
+        [Parameter(Mandatory = $true,  ValueFromPipelineByPropertyName = $true, Position = 3)][ValidateNotNullOrEmpty()][array]$Values,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 4)][ValidateNotNullOrEmpty()][int]$Limit,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 5)][ValidateNotNullOrEmpty()][string]$Body
+    )
+    $Values += $Key
+    If ($Body)
+    {
+        $DataSet = Invoke-JCApi -Url:($URL) -Method:($Method) -Fields:($Values) -Paginate -Body:($Body)
+    }
+    Else
+    {
+        $DataSet = Invoke-JCApi -Url:($URL) -Method:($Method) -Fields:($Values) -Paginate
+    }
+    $Hashtable = New-Object System.Collections.Hashtable
+    ForEach ($Item In $DataSet)
+    {
+        $Hashtable.Add($Item.$Key, ($Item | Select-Object -Property * -ExcludeProperty $Key))
+    }
+    Return $Hashtable
+}
--- PowerShell/JumpCloud Module/Private/NestedFunctions/Invoke-JCApi.ps1
+++ PowerShell/JumpCloud Module/Private/NestedFunctions/Invoke-JCApi.ps1
@@ -1,10 +1,13 @@
-Function Invoke-JCApiGet
+Function Invoke-JCApi
 { 
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$Url,
-        [Parameter(Mandatory = $false, Position = 1)][ValidateNotNullOrEmpty()][ValidateRange(1, [int]::MaxValue)][int]$Limit = 100,
-        [Parameter(Mandatory = $false, Position = 2)][ValidateNotNullOrEmpty()][array]$Fields = ''
+        [Parameter(Mandatory = $true, Position = 1)][ValidateNotNullOrEmpty()][string]$Method,
+        [Parameter(Mandatory = $false, Position = 2)][ValidateNotNullOrEmpty()][ValidateRange(1, [int]::MaxValue)][int]$Limit = 100,
+        [Parameter(Mandatory = $false, Position = 3)][ValidateNotNullOrEmpty()][array]$Fields = '',
+        [Parameter(Mandatory = $false, Position = 4)][ValidateNotNullOrEmpty()][string]$Body = '',
+        [Parameter(Mandatory = $false, Position = 5)][ValidateNotNullOrEmpty()][switch]$Paginate
     )
     Begin
     {
@@ -27,7 +30,7 @@ Function Invoke-JCApiGet
         $Skip = 0
         $UriQueryString_Template = '{0}{1}limit={2}&skip={3}&fields={4}'
         $Results_Output = @()
-        If($Url -notlike ('*' + $JCUrlBasePath + '*'))
+        If ($Url -notlike ('*' + $JCUrlBasePath + '*'))
         {
             $Url = $JCUrlBasePath + $Url
         }
@@ -39,12 +42,42 @@ Function Invoke-JCApiGet
         {
             $SearchOperator = '?'
         }
-        $JoinedFields = ($Fields -join '&')
+        $JoinedFields = ($Fields -join ' ')
         Do
         {
+            # Build body to include skip and limit by default plus what ever else is passed in.
+            If ($Body)
+            {
+                $ObjectBody = $Body | ConvertFrom-Json
+            }
+            Else
+            {
+                $ObjectBody = ''
+            }
+            If ($ObjectBody.PSObject.Properties.name -eq 'skip')
+            {
+                $ObjectBody.skip = $Skip
+            }
+            Else
+            {
+                $ObjectBody = $ObjectBody | Select-Object @{Name = 'skip'; Expression = {$Skip}}, *
+            }
+            If (!($ObjectBody.PSObject.Properties.name -eq 'limit'))
+            {
+                $ObjectBody = $ObjectBody | Select-Object @{Name = 'limit'; Expression = {$Limit}}, *
+            }
+            If (!($ObjectBody.PSObject.Properties.name -eq 'fields'))
+            {
+                $ObjectBody = $ObjectBody | Select-Object @{Name = 'fields'; Expression = {$JoinedFields}}, *
+            }
+            $ObjectBody = $ObjectBody | Select-Object -Property * -ExcludeProperty Length
+            $Body = $ObjectBody | ConvertTo-Json -Depth:(10) -Compress
+
+            # Build url and query string
             $Uri = $UriQueryString_Template -f $Url, $SearchOperator, $Limit, $Skip, $JoinedFields
             Write-Verbose ('Connecting to: ' + $Uri)
-            $Results = Invoke-RestMethod -Method:('GET') -Headers:($hdrs) -Uri:($Uri)
+            Write-Verbose ('Sending body: ' + $Body)
+            $Results = Invoke-RestMethod -Method:($Method) -Headers:($hdrs) -Uri:($Uri) -Body:($Body)
             If ($Results)
             {
                 $ResultsPopulated = $false
@@ -77,7 +110,7 @@ Function Invoke-JCApiGet
             # If ($Results) {Write-Host "Results are true" -BackgroundColor Cyan } Else {Write-Host "Results are false" -BackgroundColor red}
             # If ($ResultsCount -ge 1) {Write-Host ("Result count is greater than or equal to 1. Current count:" + [string]$ResultsCount ) -BackgroundColor Cyan} Else {Write-Host ("Result count is less than 1. Current count:" + [string]$ResultsCount ) -BackgroundColor red}
         }
-        While ($ResultsCount -eq $Limit)
+        While ($Paginate -and $ResultsCount -eq $Limit)
     }
     End
     {
