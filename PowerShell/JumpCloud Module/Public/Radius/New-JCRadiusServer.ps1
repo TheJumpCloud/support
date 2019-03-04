d3e73eac6eb2d8797d2ea43824828a37f13b6ea5
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-03-04 16:51:10 -0700

    rename parameter for splatting

--- PowerShell/JumpCloud Module/Public/Radius/New-JCRadiusServer.ps1
+++ PowerShell/JumpCloud Module/Public/Radius/New-JCRadiusServer.ps1
@@ -21,6 +21,12 @@ Function New-JCRadiusServer ()
         $PSBoundParameters.GetEnumerator() | ForEach-Object {$FunctionParameters.Add($_.Key, $_.Value) | Out-Null}
         # Remove PowerShell CommonParameters
         @($FunctionParameters.Keys)| ForEach-Object {If ($_ -in @([System.Management.Automation.PSCmdlet]::CommonParameters)) {$FunctionParameters.Remove($_) | Out-Null}};
+        # Rename parameters in the FunctionParameters hashtable
+        If ($FunctionParameters.Contains('RadiusServerName'))
+        {
+            $FunctionParameters.Add('name', $FunctionParameters['RadiusServerName']) | Out-Null
+            $FunctionParameters.Remove('RadiusServerName') | Out-Null
+        }
         # Convert body to json.
         $JsonBody = $FunctionParameters | ConvertTo-Json -Depth 10 -Compress
         # Run command
