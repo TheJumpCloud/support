c9501f4c3465920be4ade4e196a540a9525b60b4
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-03-05 11:37:59 -0700

    Add Invoke-JCRadiusServer

---
+++ PowerShell/JumpCloud Module/Private/NestedFunctions/New-DynamicParameter.ps1
@@ -0,0 +1,91 @@
+Function New-DynamicParameter ()
+{
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    Param
+    (
+        [Parameter(Mandatory = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$ParameterName,
+        [Parameter(Mandatory = $true, Position = 1)][ValidateNotNullOrEmpty()][ValidateSet('string', 'char', 'byte', 'int', 'long', 'bool', 'decimal', 'single', 'double', 'DateTime', 'xml', 'array', 'hashtable')][string]$ParameterType,
+        [Parameter(Mandatory = $false, Position = 2)][ValidateNotNullOrEmpty()][bool]$DontShow = $false,
+        [Parameter(Mandatory = $false, Position = 3)][ValidateNotNullOrEmpty()][string]$HelpMessage,
+        [Parameter(Mandatory = $false, Position = 4)][ValidateNotNullOrEmpty()][bool]$Mandatory = $false,
+        [Parameter(Mandatory = $false, Position = 5)][ValidateNotNullOrEmpty()][string]$ParameterSetName = '__AllParameterSets',
+        [Parameter(Mandatory = $false, Position = 6)][ValidateNotNullOrEmpty()][int]$Position = 0,
+        [Parameter(Mandatory = $false, Position = 7)][ValidateNotNullOrEmpty()][bool]$Alias = @(),
+        [Parameter(Mandatory = $false, Position = 8)][ValidateNotNullOrEmpty()][bool]$ValueFromPipeline = $false,
+        [Parameter(Mandatory = $false, Position = 9)][ValidateNotNullOrEmpty()][bool]$ValueFromPipelineByPropertyName = $false,
+        [Parameter(Mandatory = $false, Position = 10)][ValidateNotNullOrEmpty()][bool]$ValueFromRemainingArguments = $false,
+        [Parameter(Mandatory = $false, Position = 11)][ValidateNotNullOrEmpty()][array]$ValidateSet = @(),
+        [Parameter(Mandatory = $false, Position = 12)][ValidateNotNullOrEmpty()][ValidateCount(2, 2)][array]$ValidateLength = $null,
+        [Parameter(Mandatory = $false, Position = 13)][ValidateNotNullOrEmpty()][bool]$ValidateNotNullOrEmpty = $false,
+        [Parameter(Mandatory = $false, Position = 14)][ValidateScript( {
+                If (-not ( $_ -is [System.Management.Automation.RuntimeDefinedParameterDictionary] -or -not $_) )
+                {
+                    Throw 'RuntimeDefinedParameterDictionary must be a System.Management.Automation.RuntimeDefinedParameterDictionary object, or not exist'
+                }
+                $True
+            })]
+        $RuntimeDefinedParameterDictionary = $false
+    )
+    # Create the parameters attributes
+    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+    # Create the collection of attributes
+    $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+    # Set the parameters attributes
+    If ($DontShow) {$ParameterAttribute.DontShow = $DontShow}
+    If ($HelpMessage) {$ParameterAttribute.HelpMessage = $HelpMessage}
+    If ($Mandatory) {$ParameterAttribute.Mandatory = $Mandatory}
+    If ($ParameterSetName) {$ParameterAttribute.ParameterSetName = $ParameterSetName}
+    If ($Position) {$ParameterAttribute.Position = $Position}
+    If ($ValueFromPipeline) {$ParameterAttribute.ValueFromPipeline = $ValueFromPipeline}
+    If ($ValueFromPipelineByPropertyName) {$ParameterAttribute.ValueFromPipelineByPropertyName = $ValueFromPipelineByPropertyName}
+    If ($ValueFromRemainingArguments) {$ParameterAttribute.ValueFromRemainingArguments = $ValueFromRemainingArguments}
+    # $ParameterAttribute.ExperimentAction = 'None'
+    # $ParameterAttribute.ExperimentName = $null
+    # $ParameterAttribute.HelpMessageBaseName = $null
+    # $ParameterAttribute.HelpMessageResourceId = $null
+    # $ParameterAttribute.TypeId
+    # Add the attributes to the attributes collection
+    $AttributeCollection.Add($ParameterAttribute)
+
+    # Set the ValidateNotNullOrEmpty
+    If ($Alias)
+    {
+        $AliasAttribute = New-Object -TypeName System.Management.Automation.AliasAttribute($Alias)
+        $AttributeCollection.Add($AliasAttribute)
+    }
+    # Set the ValidateNotNullOrEmpty
+    If ($ValidateNotNullOrEmpty)
+    {
+        # Set the ValidateNotNullOrEmpty
+        $ValidateNotNullOrEmptyAttribute = New-Object System.Management.Automation.ValidateNotNullOrEmptyAttribute
+        # Add the ValidateNotNullOrEmpty to the attributes collection
+        $AttributeCollection.Add($ValidateNotNullOrEmptyAttribute)
+    }
+    # Set the ValidateSet
+    If ($ValidateSet)
+    {
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet);
+        $AttributeCollection.Add($ValidateSetAttribute)
+    }
+    # Set the ValidateLengthAttribute
+    If ($ValidateLength)
+    {
+        $ValidateLengthAttribute = New-Object -TypeName System.Management.Automation.ValidateLengthAttribute($ValidateLength[0], $ValidateLength[1])
+        $AttributeCollection.Add($ValidateLengthAttribute)
+    }
+    # Create the dynamic parameter
+    $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, $ParameterType, $AttributeCollection)
+
+    #Add the dynamic parameter to an existing dynamic parameter dictionary, or create the dictionary and add it
+    If ($RuntimeDefinedParameterDictionary)
+    {
+        $RuntimeDefinedParameterDictionary.Add($ParameterName, $RuntimeParameter)
+    }
+    Else
+    {
+        $RuntimeDefinedParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $RuntimeDefinedParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        Return $RuntimeDefinedParameterDictionary
+    }
+}
---
+++ PowerShell/JumpCloud Module/Private/RadiusServer/Invoke-JCRadiusServer.ps1
@@ -0,0 +1,109 @@
+Function Invoke-JCRadiusServer ()
+{
+    # This endpoint allows you to update Radius Servers in your organization.
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$Action
+    )
+    DynamicParam
+    {
+        # Create the parameter dictionary
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        # Define the new parameters
+        New-DynamicParameter -ParameterName:('RadiusServerId') -ParameterType:('string') -Position:(0) -Mandatory:($true) -ValueFromPipelineByPropertyName:($true) -ParameterSetName:('ById') -ValidateNotNullOrEmpty:($true) -Alias:(@('_id', 'id')) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+        New-DynamicParameter -ParameterName:('RadiusServerName') -ParameterType:('string') -Position:(0) -Mandatory:($true) -ValueFromPipelineByPropertyName:($true) -ParameterSetName:('ByName') -ValidateNotNullOrEmpty:($true) -Alias:('Name') -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+        If ($Action -eq 'GET')
+        {
+        }
+        ElseIf ($Action -eq 'PUT')
+        {
+            New-DynamicParameter -ParameterName:('NewRadiusServerName') -ParameterType:('string') -Position:(1) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true)  -ValidateNotNullOrEmpty:($true) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+            New-DynamicParameter -ParameterName:('NewNetworkSourceIp') -ParameterType:('string') -Position:(2) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true) -ValidateNotNullOrEmpty:($true) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+            New-DynamicParameter -ParameterName:('NewSharedSecret') -ParameterType:('string') -Position:(3) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true) -ValidateNotNullOrEmpty:($true) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+        }
+        ElseIf ($Action -eq 'DELETE')
+        {
+            New-DynamicParameter -ParameterName:('force') -ParameterType:('switch') -Position:(1) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true) -ValidateNotNullOrEmpty:($true) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+        }
+        ElseIf ($Action -eq 'POST')
+        {
+            New-DynamicParameter -ParameterName:('networkSourceIp') -ParameterType:('string') -Position:(1) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true) -ValidateNotNullOrEmpty:($true) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+            New-DynamicParameter -ParameterName:('sharedSecret') -ParameterType:('string') -Position:(2) -Mandatory:($false) -ValueFromPipelineByPropertyName:($true) -ValidateNotNullOrEmpty:($true) -ValidateLength:(@(1, 31)) -RuntimeDefinedParameterDictionary:($RuntimeParameterDictionary)
+        }
+        Return $RuntimeParameterDictionary
+    }
+    Begin
+    {
+        Write-Verbose ('Parameter Set: ' + $PSCmdlet.ParameterSetName)
+        # Show all parameters
+        # $PsBoundParameters.GetEnumerator()
+        # Bind the parameter to a friendly variable
+        $RadiusServerId = $PsBoundParameters.RadiusServerId
+        $RadiusServerName = $PsBoundParameters.RadiusServerName
+        $NewRadiusServerName = $PsBoundParameters.NewRadiusServerName
+        $NewNetworkSourceIp = $PsBoundParameters.NewNetworkSourceIp
+        $NewSharedSecret = $PsBoundParameters.NewSharedSecret
+        $force = $PsBoundParameters.force
+        $networkSourceIp = $PsBoundParameters.networkSourceIp
+        $sharedSecret = $PsBoundParameters.sharedSecret
+        $Method = $Action
+    }
+    Process
+    {
+        $Uri_RadiusServers = '/api/radiusservers'
+        $RadiusServerObject = Switch ($PSCmdlet.ParameterSetName)
+        {
+            'ReturnAll'
+            {
+                Get-JCObject -Type:('radiusservers');
+            }
+            'ById'
+            {
+                Get-JCObject -Type:('radiusservers') -SearchBy:('ById') -SearchByValue:($RadiusServerId);
+            }
+            'ByName'
+            {
+                Get-JCObject -Type:('radiusservers') -SearchBy:('ByName') -SearchByValue:($RadiusServerName);
+            }
+        }
+        If ($RadiusServerObject)
+        {
+            If ($Action -eq 'GET')
+            {
+                $Results = $RadiusServerObject
+            }
+            ElseIf ($Action -eq 'PUT')
+            {
+                # Build Url
+                $Uri_RadiusServers = $Uri_RadiusServers + '/' + $RadiusServerObject.($RadiusServerObject.ById)
+                # Build Json body
+                If (!($NewRadiusServerName)) {$NewRadiusServerName = $RadiusServerObject.($RadiusServerObject.ByName)}
+                If (!($NewNetworkSourceIp)) {$NewNetworkSourceIp = $RadiusServerObject.networkSourceIp}
+                If (!($NewSharedSecret)) {$NewSharedSecret = $RadiusServerObject.sharedSecret}
+                $JsonBody = '{"name":"' + $NewRadiusServerName + '","networkSourceIp":"' + $NewNetworkSourceIp + '","sharedSecret":"' + $NewSharedSecret + '"}'
+            }
+            ElseIf ($Action -eq 'DELETE')
+            {
+                # Send body to RadiusServers endpoint.
+                If (!($force)) {Write-Warning ('Are you sure you wish to delete object: ' + $RadiusServerObject.($RadiusServerObject.ByName) + ' ?') -WarningAction:('Inquire')}
+                # Build body to be sent to RadiusServers endpoint.
+                $JsonBody = '{"isSelectAll":false,"models":[{"_id":"' + $RadiusServerObject.($RadiusServerObject.ById) + '"}]}'
+            }
+            ElseIf ($Action -eq 'POST')
+            {
+                # Build body to be sent to RadiusServers endpoint.
+                $JsonBody = '{"name":"' + $RadiusServerName + '","networkSourceIp":"' + $networkSourceIp + '","sharedSecret":"' + $sharedSecret + '"}'
+            }
+            # Send body to RadiusServers endpoint.
+            $Results = Invoke-JCApi -Method:($Method) -Url:($Uri_RadiusServers) -Body:($JsonBody)
+        }
+        Else
+        {
+            Write-Error ('Unable to find radius server. Run Get-JCRadiusServer to get a list of all radius servers.')
+        }
+    }
+    End
+    {
+        Return $Results
+    }
+}
