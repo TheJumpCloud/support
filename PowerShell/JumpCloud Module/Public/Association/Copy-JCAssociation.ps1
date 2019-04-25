826c519ae463956b684a1c8727bd730d3ba370a7
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-04-25 09:34:03 -0600

    removed extra lookup

--- PowerShell/JumpCloud Module/Public/Association/Copy-JCAssociation.ps1
+++ PowerShell/JumpCloud Module/Public/Association/Copy-JCAssociation.ps1
@@ -1,4 +1,3 @@
-# Copy-JCAssociation -type:() -Id:() -TargetId:() -KeepExisting -Force
 Function Copy-JCAssociation
 {
     [CmdletBinding(DefaultParameterSetName = 'ById')]
@@ -20,94 +19,77 @@ Function Copy-JCAssociation
     Process
     {
         $SearchBy = $PSCmdlet.ParameterSetName
+        # Get the associations from the source and target
         Switch ($SearchBy)
         {
             'ById'
             {
-                $SourceSearchByValue = $Id
-                $TargetSearchByValue = $TargetId
+                $SourceAssociations = Get-JCAssociation -Type:($Type) -Id:($Id)
+                $TargetAssociations = Get-JCAssociation -Type:($Type) -Id:($TargetId)
+                $Target = Get-JCObject -Type:($Type) -Id:($TargetName)
             }
             'ByName'
             {
-                $SourceSearchByValue = $Name
-                $TargetSearchByValue = $TargetName
+                $SourceAssociations = Get-JCAssociation -Type:($Type) -Name:($Name)
+                $TargetAssociations = Get-JCAssociation -Type:($Type) -Name:($TargetName)
+                $Target = Get-JCObject -Type:($Type) -Name:($TargetName)
             }
         }
-        # Get the source and target items
-        $Source = Get-JCObject -Type:($Type) -SearchBy:($SearchBy) -SearchByValue:($SourceSearchByValue)
-        $Target = Get-JCObject -Type:($Type) -SearchBy:($SearchBy) -SearchByValue:($TargetSearchByValue)
-        If ($Source)
+        # Compare the associations
+        $CompareResults = Compare-Object -ReferenceObject:(@($SourceAssociations)) -DifferenceObject:(@($TargetAssociations)) -Property:('targetType', 'targetId') -IncludeEqual -PassThru
+        # | ForEach-Object {
+        #     If ($_.SideIndicator -eq '=>')
+        #     {
+        #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:($Target.($Target.ByName))
+        #     }
+        #     ElseIf ($_.SideIndicator -eq '<=')
+        #     {
+        #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:($Source.($Source.ByName))
+        #     }
+        #     ElseIf ($_.SideIndicator -eq '==')
+        #     {
+        #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:('NoDiff')
+        #     }
+        #     $_
+        # }
+        $AssociationsToSame = $CompareResults.Where( {$_.SideIndicator -eq '=='} )
+        $AssociationsToAdd = $CompareResults.Where( {$_.SideIndicator -eq '<=' -and $_.associationType -eq 'Direct' -and $_.TargetId -notin ($AssociationsToSame.targetId) })
+        $AssociationsToRemove = $CompareResults.Where( { $_.SideIndicator -eq '=>' -and $_.associationType -eq 'Direct' -and $_.TargetId -notin ($AssociationsToSame.targetId) })
+        # Send the results of the ones that are the same to the output
+        $Results += $TargetAssociations.Where( {$_.TargetId -in ($AssociationsToSame.targetId)} )
+        If ($KeepExisting)
         {
-            If ($Target)
-            {
-                # Get the associations from the source and target
-                $SourceAssociations = $Source | Get-JCAssociation
-                $TargetAssociations = $Target | Get-JCAssociation
-                # Compare the associations
-                $CompareResults = Compare-Object -ReferenceObject:(@($SourceAssociations)) -DifferenceObject:(@($TargetAssociations)) -Property:('targetType', 'targetId') -IncludeEqual -PassThru
-                # | ForEach-Object {
-                #     If ($_.SideIndicator -eq '=>')
-                #     {
-                #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:($Target.($Target.ByName))
-                #     }
-                #     ElseIf ($_.SideIndicator -eq '<=')
-                #     {
-                #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:($Source.($Source.ByName))
-                #     }
-                #     ElseIf ($_.SideIndicator -eq '==')
-                #     {
-                #         Add-Member -InputObject:($_) -MemberType:('NoteProperty') -Name:('SideIndicatorName') -Value:('NoDiff')
-                #     }
-                #     $_
-                # }
-                $AssociationsToSame = $CompareResults.Where( {$_.SideIndicator -eq '=='} )
-                $AssociationsToAdd = $CompareResults.Where( {$_.SideIndicator -eq '<=' -and $_.associationType -eq 'Direct' -and $_.TargetId -notin ($AssociationsToSame.targetId) })
-                $AssociationsToRemove = $CompareResults.Where( { $_.SideIndicator -eq '=>' -and $_.associationType -eq 'Direct' -and $_.TargetId -notin ($AssociationsToSame.targetId) })
-                # Send the results of the ones that are the same to the output
-                $Results += $TargetAssociations.Where( {$_.TargetId -in ($AssociationsToSame.targetId)} )
-                If ($KeepExisting)
-                {
-                    # Send the existing association results to the output
-                    $Results += $TargetAssociations.Where( {$_.TargetId -in ($AssociationsToRemove.targetId)} )
-                }
-                Else
-                {
-                    # Remove exist associations from target
-                    $TargetAssociationsRemoved = If ($Force)
-                    {
-                        $AssociationsToRemove | Get-JCAssociation | Remove-JCAssociation -Force
-                    }
-                    Else
-                    {
-                        $AssociationsToRemove | Get-JCAssociation | Remove-JCAssociation
-                    }
-                    # Send the results of the removal to the output
-                    $Results += $TargetAssociationsRemoved
-                }
-                # Add the associations to the target
-                $TargetAssociationsAdded = If ($Force)
-                {
-                    $AssociationsToAdd | Add-JCAssociation -Id:($Target.($Target.ById)) -Force
-                }
-                Else
-                {
-                    $AssociationsToAdd | Add-JCAssociation -Id:($Target.($Target.ById))
-                }
-                # Send the results of the addition to the output
-                $Results += $TargetAssociationsAdded
-                If (!($Results))
-                {
-                    $Results += $CompareResults
-                }
-            }
-            Else
-            {
-                Write-Error ('Target does not exist. Unable to find the "' + $Type + '" called "' + $TargetSearchByValue + '".')
-            }
+            # Send the existing association results to the output
+            $Results += $TargetAssociations.Where( {$_.TargetId -in ($AssociationsToRemove.targetId)} )
         }
         Else
         {
-            Write-Error ('Source does not exist. Unable to find the "' + $Type + '" called "' + $SourceSearchByValue + '".')
+            # Remove exist associations from target
+            $TargetAssociationsRemoved = If ($Force)
+            {
+                $AssociationsToRemove | Get-JCAssociation | Remove-JCAssociation -Force
+            }
+            Else
+            {
+                $AssociationsToRemove | Get-JCAssociation | Remove-JCAssociation
+            }
+            # Send the results of the removal to the output
+            $Results += $TargetAssociationsRemoved
+        }
+        # Add the associations to the target
+        $TargetAssociationsAdded = If ($Force)
+        {
+            $AssociationsToAdd | Add-JCAssociation -Id:($Target.($Target.ById)) -Force
+        }
+        Else
+        {
+            $AssociationsToAdd | Add-JCAssociation -Id:($Target.($Target.ById))
+        }
+        # Send the results of the addition to the output
+        $Results += $TargetAssociationsAdded
+        If (!($Results))
+        {
+            $Results += $CompareResults
         }
     }
     End
