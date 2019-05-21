9612e7057af119037a40516ae56df5fbeb5cf9e2
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-05-21 09:26:20 -0600

    update for improved error handling

--- PowerShell/JumpCloud Module/Private/ScriptBlocks/ScriptBlock_TryCatchError.ps1
+++ PowerShell/JumpCloud Module/Private/ScriptBlocks/ScriptBlock_TryCatchError.ps1
@@ -13,41 +13,17 @@ $ScriptBlock_TryCatchError = {
     )
     $CallStack = Get-PSCallStack
     [array]::Reverse($CallStack)
-    # $DebugPreference = 'Continue' # SilentlyContinue (Default), Continue, Inquire, Stop
     Write-Debug ('Command: ' + ($CallStack.Command -join ' -> '))
     Write-Debug ('Arguments: ' + ($CallStack.Arguments -join ' -> '))
     Write-Debug ('Location: ' + ($CallStack.Location -join ' -> '))
-    # $DebugPreference = 'SilentlyContinue' # SilentlyContinue (Default), Continue, Inquire, Stop
-    # $Error.Clear()
-    # $EM_ErrorDetailsMessage = $ErrorObject.ErrorDetails.Message
-    # $Exception = $ErrorObject.Exception
-    # $EM_ExceptionMessage = $Exception.Message
-    # While ($Exception.InnerException)
-    # {
-    #     $Exception = $Exception.InnerException
-    #     $EM_InnerException_Message += "`n" + $Exception.Message
-    #     Write-Host ('InnerExceptionFound!!!') -BackgroundColor Cyan
-    # }
-    # # Build error message
-    # $OutputArray = @(
-    #     "`n"
-    #     , "START||EM_ErrorDetailsMessage||START`n'$($EM_ErrorDetailsMessage)'`nEND||EM_ErrorDetailsMessage||END;"
-    #     , "START||EM_ExceptionMessage||START`n'$($EM_ExceptionMessage)'`nEND||EM_ExceptionMessage||END;"
-    #     , "START||EM_InnerException_Message||START`n'$($EM_InnerException_Message)'`nEND||EM_InnerException_Message||END;"
-    #     , "START||PositionMessage||START`n'$($ErrorObject.InvocationInfo.PositionMessage)'`nEND||PositionMessage||END;"
-    #     , "START||FullyQualifiedErrorId||START`n'$($ErrorObject.FullyQualifiedErrorId.ToString())'`nEND||FullyQualifiedErrorId||END;"
-    # )
-    # $OutputString = (($OutputArray | Where-Object {$_}) -join "`n")
-    # $OutputString = ($OutputArray -join "`n`n") + "`n########################################################################`n########################################################################"
-    $OutputString = $ErrorObject
     # If it should be a terminating error
     If ($BreakInd)
     {
-        Throw ($OutputString)
+        Throw ($ErrorObject)
         Break
     }
     Else
     {
-        Write-Error ($OutputString)
+        Write-Error ($ErrorObject)
     }
 }
