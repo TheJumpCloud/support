4cf1924253e420ccb3e6bd0b250b8b9b575d1c4a
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-05-29 15:10:34 -0600

    add missing function

--- PowerShell/JumpCloud Module/Tests/HelperFunctions.ps1
+++ PowerShell/JumpCloud Module/Tests/HelperFunctions.ps1
@@ -131,6 +131,24 @@ Function New-RandomUserCustom  ()
 
     return $NewRandomUser
 }
+Function New-RandomStringLower ()
+{
+    [CmdletBinding()]
+    param(
+
+        [Parameter()]
+        [ValidateRange(0, 52)]
+        [Int]
+        $NumberOfChars = 8
+
+    )
+    begin {}
+    process
+    {
+        $Random = -join ((65..90) + (97..122) | Get-Random -Count $NumberOfChars | % {[char]$_})
+    }
+    end {Return $Random.tolower()}
+}
 Function Connect-JCOnlineTest ()
 {
     $Connect = Connect-JCOnline -JumpCloudAPIKey $TestOrgAPIKey -force
