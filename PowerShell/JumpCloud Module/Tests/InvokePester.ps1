beb6a83c5befd3684e691a9a004dc4ae647ca5d5
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-05-30 07:46:51 -0600

    adding additional steps

--- PowerShell/JumpCloud Module/Tests/InvokePester.ps1
+++ PowerShell/JumpCloud Module/Tests/InvokePester.ps1
@@ -1,5 +1,37 @@
+# Load config and helper files
 . "$PSScriptRoot/HelperFunctions.ps1"
 . "$PSScriptRoot/TestEnvironmentVariables.ps1"
-
-
-$PesterResults = Invoke-Pester -Script @{ Path = $PSScriptRoot; Parameters = $PesterParms; } -PassThru
+# Get list of tags and validate that tags have been applied
+$PesterTests = Get-ChildItem -Path:($PSScriptRoot + '/*.Tests.ps1') -Recurse
+$Tags = ForEach ($PesterTest In $PesterTests)
+{
+    $PesterTestFullName = $PesterTest.FullName
+    $FileContent = Get-Content -Path:($PesterTestFullName)
+    $DescribeLines = $FileContent | Select-String -Pattern:([RegEx]'(Describe)')#.Matches.Value
+    ForEach ($DescribeLine In $DescribeLines)
+    {
+        If ($DescribeLine.Line -match 'Tag')
+        {
+            $TagParameterValue = ($DescribeLine.Line | Select-String -Pattern:([RegEx]'(?<=-Tag)(.*?)(?=\s)')).Matches.Value
+            @(":", "(", ")", "'") | ForEach-Object {If ($TagParameterValue -like ('*' + $_ + '*')) {$TagParameterValue = $TagParameterValue.Replace($_, '')}}
+            $TagParameterValue
+        }
+        Else
+        {
+            Write-Error ('Tag missing in "' + $PesterTestFullName + '" on line number "' + $DescribeLine.LineNumber + '" value "' + ($DescribeLine.Line).Trim() + '"')
+        }
+    }
+}
+# Filters on tags
+$ExcludeTagList = ''
+$IncludeTagList = ''
+$IncludeTags = If ($IncludeTagList)
+{
+    $IncludeTagList
+}
+Else
+{
+    $Tags | Where-Object {$_ -notin $ExcludeTags} | Select-Object -Unique
+}
+# Run Pester tests
+$PesterResults = Invoke-Pester -Script @{ Path = $PSScriptRoot ; Parameters = $PesterParams; } -PassThru -Tag:($IncludeTags) -ExcludeTag:($ExcludeTagList)
