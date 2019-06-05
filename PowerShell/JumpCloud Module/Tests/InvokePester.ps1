53ac820c19e0702f49cd82b189690fb4d8efa989
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-06-05 08:41:47 -0600

    update run pester test scripts

--- PowerShell/JumpCloud Module/Tests/InvokePester.ps1
+++ PowerShell/JumpCloud Module/Tests/InvokePester.ps1
@@ -1,6 +1,7 @@
+#Requires -Modules Pester, JumpCloud
 # Load config and helper files
-. "$PSScriptRoot/HelperFunctions.ps1"
-. "$PSScriptRoot/TestEnvironmentVariables.ps1"
+. ($PSScriptRoot + '/HelperFunctions.ps1')
+. ($PSScriptRoot + '/TestEnvironmentVariables.ps1')
 # Get list of tags and validate that tags have been applied
 $PesterTests = Get-ChildItem -Path:($PSScriptRoot + '/*.Tests.ps1') -Recurse
 $Tags = ForEach ($PesterTest In $PesterTests)
@@ -13,7 +14,7 @@ $Tags = ForEach ($PesterTest In $PesterTests)
         If ($DescribeLine.Line -match 'Tag')
         {
             $TagParameterValue = ($DescribeLine.Line | Select-String -Pattern:([RegEx]'(?<=-Tag)(.*?)(?=\s)')).Matches.Value
-            @(":", "(", ")", "'") | ForEach-Object {If ($TagParameterValue -like ('*' + $_ + '*')) {$TagParameterValue = $TagParameterValue.Replace($_, '')}}
+            @(":", "(", ")", "'") | ForEach-Object { If ($TagParameterValue -like ('*' + $_ + '*')) { $TagParameterValue = $TagParameterValue.Replace($_, '') }}
             $TagParameterValue
         }
         Else
@@ -23,15 +24,31 @@ $Tags = ForEach ($PesterTest In $PesterTests)
     }
 }
 # Filters on tags
-$ExcludeTagList = ''
-$IncludeTagList = ''
+$ExcludeTagList = ('')
+$IncludeTagList = ('')
 $IncludeTags = If ($IncludeTagList)
 {
     $IncludeTagList
 }
 Else
 {
-    $Tags | Where-Object {$_ -notin $ExcludeTags} | Select-Object -Unique
+    $Tags | Where-Object { $_ -notin $ExcludeTags } | Select-Object -Unique
 }
 # Run Pester tests
-$PesterResults = Invoke-Pester -Script @{ Path = $PSScriptRoot ; Parameters = $PesterParams; } -PassThru -Tag:($IncludeTags) -ExcludeTag:($ExcludeTagList)
+$PesterResultsFileXml = $PSScriptRoot + '/Pester.Tests.Results.xml'
+$PesterResultsFileCsv = $PSScriptRoot + '/Pester.Tests.Results.csv'
+$PesterResults = Invoke-Pester -Script:(@{ Path = $PSScriptRoot; Parameters = $PesterParams; }) -PassThru -Tag:($IncludeTags) -ExcludeTag:($ExcludeTagList) -OutputFormat:('NUnitXml') -OutputFile:($PesterResultsFileXml)
+# $PesterResults.TestResult | Where-Object {$_.Passed -eq $false} | Export-Csv $PesterResultsFileCsv
+
+
+## Notes for future reporting dashboard for pester
+# Install-PackageProvider -Name:('NuGet')
+# Install-Package -Name:('extent')
+# Install-Package extent
+# $Package = Get-Package -Name:('ReportUnit')
+# $ReportUnitExePath = ($Package.Source).Replace($Package.PackageFilename, 'toolsReportUnit.exe')
+# # $ReportUnitExePath = '{PathTo}\extent.exe'
+# # Invoke-Pester -Path:('PesterTests.ps1') -OutputFormat:('NUnitXml') -OutputFile:($PesterResultsFileXml) -Show None -PassThru -Strict
+# $ReportUnitCommand = '"' + $ReportUnitExePath + '" "' + $PesterResultsFileXml + '"'
+# Invoke-Expression -Command:($ReportUnitCommand)
+# Start-Process chrome $htmlFile
