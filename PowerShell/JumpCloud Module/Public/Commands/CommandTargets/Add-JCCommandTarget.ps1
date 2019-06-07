5d47a7858e86c1d65b864ecc6c3aa47d7f74a321
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-06-07 10:10:07 -0600

    converted

--- PowerShell/JumpCloud Module/Public/Commands/CommandTargets/Add-JCCommandTarget.ps1
+++ PowerShell/JumpCloud Module/Public/Commands/CommandTargets/Add-JCCommandTarget.ps1
@@ -1,22 +1,53 @@
 Function Add-JCCommandTarget
 {
+    <#
+.SYNOPSIS
+
+Associates a JumpCloud system or a JumpCloud system group with a JumpCloud command
+
+.DESCRIPTION
+
+The Add-JCCommandTarget function allows you to add JumpCloud systems or JumpCloud system groups to the target list of a specific JumpCloud command. Group associations can be made by system group name or system group ID system associations can only be made using the SystemID. When JumpCloud commands are run they target all the systems on their target list.
+
+.EXAMPLE
+Add-JCCommandTarget -CommandID 5b99777710a3690ssisr3a1w -SystemID 5l0o2fu426041i79st3c35
+
+Adds the JumpCloud system with System ID '5l0o2fu426041i79st3c35' to the target list for the JumpCloud command with command ID '5b99777710a3690ssisr3a1w'
+
+.EXAMPLE
+Add-JCCommandTarget -CommandID 5b99777710a3690ssisr3a1w -GroupName WindowsMachines
+
+Adds the JumpCloud system group 'WindowsMachines' and the systems within this group to the target list for the JumpCloud command with command ID '5b99777710a3690ssisr3a1w'
+
+.EXAMPLE
+Add-JCCommandTarget -CommandID 5b99777710a3690ssisr3a1w -GroupID 5j03458a232z115210z66913
+
+Adds the JumpCloud system group with the GroupID '5j03458a232z115210z66913' and the systems within this group to the target list for the JumpCloud command with command ID '5b99777710a3690ssisr3a1w'
+
+#>
     [CmdletBinding(DefaultParameterSetName = 'SystemID')]
     param (
 
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'SystemID',
-            Position = 0)]
+            Position = 0,
+            HelpMessage = 'The id value of the JumpCloud command. Use the command "Get-JCCommand | Select-Object _id, name" to find the "_id" value for all the JumpCloud commands in your tenant.'
+        ) ]
 
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'GroupName',
-            Position = 0)]
+            Position = 0,
+            HelpMessage = 'The id value of the JumpCloud command. Use the command "Get-JCCommand | Select-Object _id, name" to find the "_id" value for all the JumpCloud commands in your tenant.'
+        ) ]
 
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'GroupID',
-            Position = 0)]
+            Position = 0,
+            HelpMessage = 'The id value of the JumpCloud command. Use the command "Get-JCCommand | Select-Object _id, name" to find the "_id" value for all the JumpCloud commands in your tenant.'
+        ) ]
 
         [Alias('_id', 'id')]
         [String]$CommandID,
@@ -24,20 +55,26 @@ Function Add-JCCommandTarget
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'SystemID',
-            Position = 1)]
+            Position = 1,
+            HelpMessage = 'The _id of a JumpCloud system. To find the _id of all JumpCloud systems within your tenant run "Get-JCSystem | select _id, hostname"'
+        ) ]
         $SystemID,
 
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'GroupName',
-            Position = 1)]
+            Position = 1,
+            HelpMessage = 'The name of the JumpCloud system group. If the name includes a space enter the name within quotes. Example: -GroupName "The Space"'
+        ) ]
         [Alias('name')]
         $GroupName,
 
         [Parameter(Mandatory,
             ValueFromPipelineByPropertyName,
             ParameterSetName = 'GroupID',
-            Position = 1)]
+            Position = 1,
+            HelpMessage = 'The id value of a JumpCloud system group.'
+        ) ]
         $GroupID
 
     )
