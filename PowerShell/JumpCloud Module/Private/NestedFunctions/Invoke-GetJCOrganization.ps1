a8f12b8ef6f694476f934c553d239e2dda32a97d
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-07-22 10:28:25 -0600

    add limit to url [skip ci]

--- PowerShell/JumpCloud Module/Private/NestedFunctions/Invoke-GetJCOrganization.ps1
+++ PowerShell/JumpCloud Module/Private/NestedFunctions/Invoke-GetJCOrganization.ps1
@@ -1,55 +1,34 @@
 function Invoke-GetJCOrganization
 {
     [CmdletBinding()]
-    param (
+    Param (
         [String]$JumpCloudAPIKey
     )
-
-    begin
+    Begin
     {
-
-        Write-Verbose 'Populating API headers'
-
+        Write-Verbose ('Populating API headers')
         $hdrs = @{
-
             'Content-Type' = 'application/json'
             'Accept'       = 'application/json'
             'X-API-KEY'    = "$JumpCloudAPIKey"
-
         }
-
         $resultsArrayList = New-Object System.Collections.ArrayList
-
-
     }
-
-    process
+    Process
     {
-
-
-
-        $MultiTenantURL = "$JCUrlBasePath/api/organizations/"
-
+        $MultiTenantURL = "$JCUrlBasePath/api/organizations/?limit=100"
         $RawResults = Invoke-RestMethod -Method GET -Uri $MultiTenantURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
-
-        foreach ($org in $RawResults.results)
+        ForEach ($org In $RawResults.results)
         {
-
             $MSPOrg = [PSCustomObject]@{
                 'OrgID'       = $org._id
                 'displayName' = $org.displayName
             }
-
             $resultsArrayList.add($MSPOrg) | Out-Null
-
         }
-
-
     }
-
-    end
+    End
     {
         Return $resultsArrayList
     }
-
 }
