dd6a047e5dca9ea957ae1dc4316a883d3aab54df
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-08-06 14:08:12 -0600

    temp comment out tests

--- PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
@@ -1,9 +1,10 @@
-Describe -Tag:('JCModule') 'Test for Update-JCModule' {
-    It ('Installs old version of module and then updates it.') {
-        Install-Module -Name:('JumpCloud') -RequiredVersion:('1.0.0') -Scope:('CurrentUser') -Force
-        $OldVersion = (Get-InstalledModule -Name:('JumpCloud')).Version
-        Update-Module -Force
-        $NewVersion = (Get-InstalledModule -Name:('JumpCloud')).Version
-        $OldVersion | Should -Not -Be $NewVersion
-    }
-}
+## ToDo: Figure out how to run this check without removing the version of the module that we are currently testing and without overwriting it with an older version of the module.
+# Describe -Tag:('JCModule') 'Test for Update-JCModule' {
+#     It ('Installs old version of module and then updates it.') {
+#         Install-Module -Name:('JumpCloud') -RequiredVersion:('1.0.0') -Scope:('CurrentUser') -Force
+#         $OldVersion = (Get-InstalledModule -Name:('JumpCloud')).Version
+#         Update-Module -Force
+#         $NewVersion = (Get-InstalledModule -Name:('JumpCloud')).Version
+#         $OldVersion | Should -Not -Be $NewVersion
+#     }
+# }
