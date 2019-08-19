8d7adbad23a40f27c40e8e9b1efcd3aacf26bf64
epanipinto-jc <epanipinto-jc@users.noreply.github.com>
2019-08-19 10:22:48 -0600

    remove unnecessary function

--- PowerShell/JumpCloud Module/Tests/Public/Authentication/Get-JCOrganization.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Authentication/Get-JCOrganization.tests.ps1
@@ -1,29 +1,22 @@
-Describe -Tag:('JCOrganization') "Get-JCOrganization 1.6" {
-
-    It "Returns JumpCloud Organizations " {
-
-        Connect-JCOnlineMultiTenant -JumpCloudOrgID $PesterParams.MultiTernateOrgId1
-        $Connect | Should -be $null
+Describe -Tag:('JCOrganization') 'Get-JCOrganization 1.6' {
+    It 'Returns JumpCloud Organizations ' {
+        Connect-JCOnline -JumpCloudApiKey:($MultiTenantAPIKey) -JumpCloudOrgId:($PesterParams.MultiTernateOrgId1) -force
+        $Connect | Should -Be $null
         $Organizations = Get-JCOrganization
         $OrgVerify = $Organizations | Select-Object OrgID -Unique
         $OrgVerify.Count | Should -BeGreaterThan 1
-
     }
-
-    It "Returns JumpCloud Organizations connected to two different orgs" {
-
-        Connect-JCOnlineMultiTenant -JumpCloudOrgID $PesterParams.MultiTernateOrgId1
-        $Connect | Should -be $null
+    It 'Returns JumpCloud Organizations connected to two different orgs' {
+        Connect-JCOnline -JumpCloudApiKey:($MultiTenantAPIKey) -JumpCloudOrgId:($PesterParams.MultiTernateOrgId1) -force
+        $Connect | Should -Be $null
         $Organizations = Get-JCOrganization
         $OrgVerify = $Organizations | Select-Object OrgID -Unique
         $OrgVerify.Count | Should -BeGreaterThan 1
 
-        Connect-JCOnlineMultiTenant -JumpCloudOrgID $PesterParams.MultiTernateOrgId2
-        $Connect | Should -be $null
+        Connect-JCOnline -JumpCloudApiKey:($MultiTenantAPIKey) -JumpCloudOrgId:($PesterParams.MultiTernateOrgId2) -force
+        $Connect | Should -Be $null
         $Organizations = Get-JCOrganization
         $OrgVerify = $Organizations | Select-Object OrgID -Unique
         $OrgVerify.Count | Should -BeGreaterThan 1
-
-
     }
 }
