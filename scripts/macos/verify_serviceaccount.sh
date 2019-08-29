58b6d32331aac428c3e93cfd44543c54c3f96cb4
kyle-randolph <kyle.randolph@jumpcloud.com>
2019-08-29 11:20:56 -0600

    Update verify_serviceaccount.sh

--- scripts/macos/verify_serviceaccount.sh
+++ scripts/macos/verify_serviceaccount.sh
@@ -2,12 +2,13 @@ MacOSMinorVersion=$(sw_vers -productVersion | cut -d '.' -f 2)
 MacOSPatchVersion=$(sw_vers -productVersion | cut -d '.' -f 3)
 
 if [[ $MacOSMinorVersion -lt 13 ]]; then
-    echo "Error:  Target system is not on macOS 10.13"
+    echo "Error:  System must be running 10.13+ to install Service Account."
     exit 2
 fi
 
 JCSA_Username="_jumpcloudserviceaccount"
 JCSA_FullName="JumpCloud Service Account"
+JCSA_FullName2="JumpCloud"
 
 sysadmin_name="sysadminctl"
 if [[ $MacOSMinorVersion -eq 13 ]]; then
@@ -18,16 +19,19 @@ fi
 
 result=$($sysadmin_name -secureTokenStatus $JCSA_Username 2>&1 )
 unknown_user=$(echo $result | grep "Unknown user $JCSA_Username")
-enabled=$(echo $result | grep "Secure token is ENABLED for user $JCSA_FullName")
+newEnabled=$(echo $result | grep "Secure token is ENABLED for user $JCSA_FullName2")
 
 if [[ ! -z $unknown_user ]]; then
     echo "Error:  JumpCloud Service Account not installed"
     exit 2
 fi
 
-if [[ -z $enabled ]]; then
-    echo "Error:  JumpCloud Service Account does not have a secure token"
-    exit 3
+if [[ -z $newEnabled ]]; then
+    oldEnabled=$(echo $result | grep "Secure token is ENABLED for user $JCSA_FullName")
+        if [[ -z $oldEnabled ]]; then
+                echo "Error:  JumpCloud Service Account does not have a secure token"
+                exit 3
+        fi
 fi
 
 echo "Success: JumpCloud Service Account has been properly created"
