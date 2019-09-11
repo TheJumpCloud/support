31611cca9baa8a31d72051bc58f2febfb131164d
epanipinto-jc <epanipinto-jc@users.noreply.github.com>
2019-09-11 11:21:51 -0600

    updated features

--- PowerShell/JumpCloud Module/ModuleUpdate.ps1
+++ PowerShell/JumpCloud Module/ModuleUpdate.ps1
@@ -1,5 +1,10 @@
 # Populate if you want to exclude files from help file creation
-$ExcludeList = @('')
+$ExcludeList = @()
+$IncludeList = @()
+$ExcludeListMd = $ExcludeList | ForEach-Object { $_ + '.md' }
+$ExcludeListPs1 = $ExcludeList | ForEach-Object { $_ + '.Ps1' }
+$IncludeListMd = $IncludeList | ForEach-Object { $_ + '.md' }
+$IncludeListPs1 = $IncludeList | ForEach-Object { $_ + '.Ps1' }
 # Define path variables
 $ModulePath = $PSScriptRoot
 $FilePath_Psd1 = $ModulePath + '/JumpCloud.psd1'
@@ -12,23 +17,46 @@ Write-Host ('[status]Importing current module: ' + $ModuleName)
 Import-Module ($FilePath_Psd1) -Force
 Write-Host ('[status]Installing module: PlatyPS')
 Install-Module -Name:('PlatyPS') -Force -Scope:('CurrentUser')
-# # Clear out existing docs
-# Remove-Item -Path:($FolderPath_Docs) -Recurse -Force
-# Remove-Item -Path:($FolderPath_enUS) -Recurse -Force
+# Clear out existing docs
+If (-not [System.String]::IsNullOrEmpty($IncludeList))
+{
+    Remove-Item -Path:($FolderPath_Docs) -Recurse -Force -Include:($IncludeListMd)
+}
+Else
+{
+    Remove-Item -Path:($FolderPath_Docs) -Recurse -Force
+}
+Remove-Item -Path:($FolderPath_enUS) -Recurse -Force
 # Create/update markdown help files using platyPS
 Write-Host ('[status]Creating/Updating help files')
-(Get-ChildItem -Path:($FolderPath_Public) -File -Recurse) | Where-Object { $_.Extension -eq '.ps1' -and $_.BaseName -notin $ExcludeList } | ForEach-Object {
+$Files = If (-not [System.String]::IsNullOrEmpty($IncludeList) -and [System.String]::IsNullOrEmpty($ExcludeList))
+{
+    Get-ChildItem -Path:($FolderPath_Public) -File -Recurse | Where-Object { $_.Extension -eq '.ps1' -and $_.Name -in $IncludeListPs1 }
+}
+ElseIf ([System.String]::IsNullOrEmpty($IncludeList) -and -not [System.String]::IsNullOrEmpty($ExcludeList))
+{
+    Get-ChildItem -Path:($FolderPath_Public) -File -Recurse | Where-Object { $_.Extension -eq '.ps1' -and $_.Name -notin $ExcludeListMd }
+}
+ElseIf (-not [System.String]::IsNullOrEmpty($IncludeList) -and -not [System.String]::IsNullOrEmpty($ExcludeList))
+{
+    Get-ChildItem -Path:($FolderPath_Public) -File -Recurse | Where-Object { $_.Extension -eq '.ps1' -and $_.Name -in $IncludeListPs1 -and $_.Name -notin $ExcludeListMd }
+}
+Else
+{
+    Get-ChildItem -Path:($FolderPath_Public) -File -Recurse | Where-Object { $_.Extension -eq '.ps1' }
+}
+$Files | ForEach-Object {
     $FunctionName = $_.BaseName
     $FilePath_Md = $FolderPath_Docs + '/' + $FunctionName + '.md'
     If (Test-Path -Path:($FilePath_Md))
     {
         # Write-Host ('Updating: ' + $FunctionName + '.md')
-        Update-MarkdownHelp -Path:($FilePath_Md) -Force -ExcludeDontShow -UpdateInputOutput
+        Update-MarkdownHelp -Path:($FilePath_Md) -Force -ExcludeDontShow -UpdateInputOutput -UseFullTypeName
     }
     Else
     {
         # Write-Host ('Creating: ' + $FunctionName + '.md')
-        New-MarkdownHelp  -Command:($FunctionName) -OutputFolder:($FolderPath_Docs) -Force  -ExcludeDontShow -OnlineVersionUrl:($GitHubWikiUrl + $FunctionName)
+        New-MarkdownHelp  -Command:($FunctionName) -OutputFolder:($FolderPath_Docs) -Force  -ExcludeDontShow -OnlineVersionUrl:($GitHubWikiUrl + $FunctionName) -UseFullTypeName
     }
 }
 # Create new ExternalHelp file.
@@ -75,3 +103,14 @@ New-ExternalHelp -Path:($FolderPath_Docs) -OutputPath:($FolderPath_enUS) -Force
 #     Set-Content -Path:($SupportWikiDocFullName) -Value:($NewDocContent) -Force
 # }
 # ### Add step check in changes to support wiki
+
+
+
+
+### Use this if we change over to updating the module as a whole instead of file by file
+# $InstalledModule = Get-InstalledModule -Name:('JumpCloud')
+# Import-Module -Name:('JumpCloud')
+# $FolderPath_Docs = $InstalledModule.InstalledLocation + '/Docs'
+
+# New-MarkdownHelp -Module:('JumpCloud') -OutputFolder:($FolderPath_Docs)
+# Update-MarkdownHelpModule -Path:($FolderPath_Docs) -RefreshModulePage -UpdateInputOutput -ExcludeDontShow #-UseFullTypeName
