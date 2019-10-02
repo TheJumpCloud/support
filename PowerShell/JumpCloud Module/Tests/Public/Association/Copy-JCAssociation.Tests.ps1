abcc5b02a13dec64101a1a7be152de861195d0ee
epanipinto-jc <epanipinto-jc@users.noreply.github.com>
2019-10-02 13:55:42 -0600

    initial commit

---
+++ PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
@@ -0,0 +1,21 @@
+Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
+    $Users = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
+    $SourceUser = $Users[0]
+    $TargetUser = $Users[1]
+    # Remove all associations from target user
+    $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
+    Context ('Copy the associations of a user to another user ById.') {
+        It("Associations should be the same.") {
+            Copy-JCAssociation -Type:('user') -Id:($SourceUser._id) -TargetId:($TargetUser._id) -Force
+            ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
+        }
+    }
+    # Remove all associations from target user
+    $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
+    Context ('Copy the associations of a user to another user ByName.') {
+        It("Associations should be the same.") {
+            Copy-JCAssociation -Type:('user') -Name:($SourceUser.username) -TargetName:($TargetUser.username) -Force
+            ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
+        }
+    }
+}
