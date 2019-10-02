c93f2c571112e2b2625cb54789450bebef0e48b1
epanipinto-jc <epanipinto-jc@users.noreply.github.com>
2019-10-02 14:44:50 -0600

    updated backup tests

--- PowerShell/JumpCloud Module/Tests/Public/Utilities/Get-JCBackup.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Utilities/Get-JCBackup.tests.ps1
@@ -1,60 +1,49 @@
 Describe -Tag:('JCBackup') "Get-JCBackup 1.5.0" {
-    Connect-JCOnline -JumpCloudApiKey:($TestOrgAPIKey) -force | Out-Null
+    # Connect-JCOnline -JumpCloudApiKey:($TestOrgAPIKey) -force | Out-Null
     It "Backs up JumpCloud users" {
-
         Get-JCBackup -All
-
-        Test-Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV"
-
-        Test-Path ./"JumpCloudSystemUsers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystemUsers_$(Get-Date -Format MMddyyyy).CSV"
-
-        Test-Path ./"JumpCloudSystems_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystems_$(Get-Date -Format MMddyyyy).CSV"
-
-        Test-Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV"
-
-        Test-Path ./"JumpCloudSystemGroupMembers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystemGroupMembers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUsers_' }) | Should -BeTrue
+        ($Files | Where-Object { $_.Name -match 'JumpCloudSystemUsers_' }) | Should -BeTrue
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUserGroupMembers_' }) | Should -BeTrue
+        ($Files | Where-Object { $_.Name -match 'JumpCloudSystemGroupMembers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
-
     It "Backs up JumpCloud users" {
         Get-JCBackup -Users
-        Test-Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUsers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
     It "Backs up JumpCloud systems" {
         Get-JCBackup -Systems
-        Test-Path ./"JumpCloudSystems_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystems_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudSystems_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
     It "Backs up JumpCloud system users" {
         Get-JCBackup -SystemUsers
-        Test-Path ./"JumpCloudSystemUsers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystemUsers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudSystemUsers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
     It "Backs up JumpCloud system groups" {
         Get-JCBackup -SystemGroups
-        Test-Path ./"JumpCloudSystemGroupMembers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudSystemGroupMembers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudSystemGroupMembers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
-
     It "Backs up JumpCloud user groups" {
         Get-JCBackup -UserGroups
-        Test-Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUserGroupMembers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
-
     It "Backs up JumpCloud users and user groups" {
         Get-JCBackup -Users -UserGroups
-
-        Test-Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUsers_$(Get-Date -Format MMddyyyy).CSV"
-
-        Test-Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV" | Should -Be $true
-        Remove-Item -Path ./"JumpCloudUserGroupMembers_$(Get-Date -Format MMddyyyy).CSV"
+        $Files = Get-ChildItem -Path:('JumpCloud*_*.csv')
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUsers_' }) | Should -BeTrue
+        ($Files | Where-Object { $_.Name -match 'JumpCloudUserGroupMembers_' }) | Should -BeTrue
+        $Files | Remove-Item
     }
-
 }
