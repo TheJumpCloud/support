cb4669cc86d69f0cf4d3cf139288b7e4c3e89687
scottd3v <scottd3v@gmail.com>
2019-11-25 10:26:43 -0700

    updated names added get examples

--- scripts/api/system_context/system_get_self.sh
+++ scripts/api/system_context/system_get_self.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-
 ##
 ## This API call fetches the system record.
 ##
 
 # Parse the systemKey from the conf file.
 # The conf file is JSON and can be parsed using JSON.parse() in a supported language.
-conf="`cat /opt/jc/jcagent.conf`"
+conf="$(cat /opt/jc/jcagent.conf)"
 regex='\"systemKey\":\"([a-zA-Z0-9_]+)\"'
 
-if [[ ${conf} =~ $regex ]] ; then
+if [[ ${conf} =~ $regex ]]; then
   systemKey="${BASH_REMATCH[1]}"
 fi
 
 # Get the current time.
-now=`date -u "+%a, %d %h %Y %H:%M:%S GMT"`;
+now=$(date -u "+%a, %d %h %Y %H:%M:%S GMT")
 
 # create the string to sign from the request-line and the date
 signstr="GET /api/systems/${systemKey} HTTP/1.1\ndate: ${now}"
 
 # create the signature
-signature=`printf "$signstr" | openssl dgst -sha256 -sign /opt/jc/client.key | openssl enc -e -a | tr -d '\n'` ;
+signature=$(printf "$signstr" | openssl dgst -sha256 -sign /opt/jc/client.key | openssl enc -e -a | tr -d '\n')
 
 # make the api call passing the signature in the authorization header
 curl -iq \
