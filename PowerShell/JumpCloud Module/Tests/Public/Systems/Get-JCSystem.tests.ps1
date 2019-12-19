c0200182e39e2a63f60f58179541369968a19d06
scottd3v <scottd3v@gmail.com>
2019-12-19 09:10:40 -0700

    updating tests

--- PowerShell/JumpCloud Module/Tests/Public/Systems/Get-JCSystem.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Systems/Get-JCSystem.tests.ps1
@@ -300,13 +300,13 @@ Describe -Tag:('JCSystem') "Get-JCSystem 1.4" {
 
 Describe -Tag:('JCSystem') "Get-JCSystem 1.15.2" {
     It "Searches for a JumpCloud system using filterDateProperty lastContact and after" {
-        [DateTime]$NewestSystemlastContact = Get-JCSystem -returnProperties lastContact | Where-Object lastContact -ne $null | Select-Object -Last 1 -ExpandProperty lastContact
+        [DateTime]$NewestSystemlastContact = Get-JCSystem -active $false -returnProperties lastContact | Where-Object lastContact -ne $null | Select-Object -Last 1 -ExpandProperty lastContact
         $NewestSystem = Get-JCSystem -filterDateProperty lastContact -dateFilter after -date $NewestSystemlastContact.addSeconds(-1)
         $NewestSystemlastContactVerify = $NewestSystem | Select-Object -Last 1 -ExpandProperty lastContact
         $NewestSystemlastContact | Should -Be $NewestSystemlastContactVerify
     }
     It "Searches for a JumpCloud system using  filterDateProperty lastContact and before" {
-        [DateTime]$OldestSystemlastContact = Get-JCSystem -returnProperties lastContact | Where-Object lastContact -ne $null | Select-Object -Last 1 -ExpandProperty lastContact
+        [DateTime]$OldestSystemlastContact = Get-JCSystem -active $false -returnProperties lastContact | Where-Object lastContact -ne $null | Select-Object -First 1 -ExpandProperty lastContact
         $OldestSystem = Get-JCSystem -filterDateProperty lastContact -dateFilter before -date $OldestSystemlastContact.addSeconds(1)
         $OldestSystemlastContactVerify = $OldestSystem | Select-Object -First 1 -ExpandProperty lastContact
         $OldestSystemlastContact | Should -Be $OldestSystemlastContactVerify
