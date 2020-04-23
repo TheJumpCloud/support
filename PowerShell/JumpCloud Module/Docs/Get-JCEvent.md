eeda76faa89ca83894223f70cf490468704206f2
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-04-23 08:13:55 -0600

    updated function

--- PowerShell/JumpCloud Module/Docs/Get-JCEvent.md
+++ PowerShell/JumpCloud Module/Docs/Get-JCEvent.md
@@ -8,7 +8,7 @@ schema: 2.0.0
 # Get-JCEvent
 
 ## SYNOPSIS
-{{ Fill in the Synopsis }}
+Query the API for Directory Insights events
 
 ## SYNTAX
 
@@ -25,36 +25,39 @@ Get-JCEvent -EventQueryBody <IEventQuery> [-Paginate <Boolean>] [<CommonParamete
 ```
 
 ## DESCRIPTION
-{{ Fill in the Description }}
+Query the API for Directory Insights events
 
 ## EXAMPLES
 
-### Example 1
-```powershell
-PS C:\> {{ Add example code here }}
+### EXAMPLE 1
+```
+(Get-JCEvent -Service:('all') -StartTime:('2020-04-15T00:00:00Z') -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
 ```
 
-{{ Add example description here }}
+Pull all event records between Tue, 14 Apr 2020 18:00:00 -0600 to Thu, 16 Apr 2020 17:00:00 -0600
+
+### EXAMPLE 2
+```
+(Get-JCEvent -Service:('directory') -StartTime:('2020-04-15T00:00:00Z') -Limit:('10') -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
+```
+
+Limit results to last 10 in the time range
+
+### EXAMPLE 3
+```
+((Get-JCEvent -Service:('directory') -StartTime:('2020-04-15T00:00:00Z') -Sort:("DESC") -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
+```
+
+Sort descending results against timestamp value
 
 ## PARAMETERS
 
-### -EndTime
-{{ Fill EndTime Description }}
-
-```yaml
-Type: System.String
-Parameter Sets: GetExpanded
-Aliases:
-
-Required: False
-Position: Named
-Default value: None
-Accept pipeline input: False
-Accept wildcard characters: False
-```
-
 ### -EventQueryBody
-{{ Fill EventQueryBody Description }}
+EventQuery is the users' command to search our auth logs
+Limit - row count (validated/repaired)
+SearchTerm - see above
+Projection - list of field names to return from document (not validated)
+To construct, see NOTES section for EVENTQUERYBODY properties and create a hash table.
 
 ```yaml
 Type: JumpCloud.SDK.DirectoryInsights.Models.IEventQuery
@@ -68,8 +71,23 @@ Accept pipeline input: True (ByValue)
 Accept wildcard characters: False
 ```
 
+### -EndTime
+.
+
+```yaml
+Type: System.String
+Parameter Sets: GetExpanded
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
 ### -Fields
-{{ Fill Fields Description }}
+.
 
 ```yaml
 Type: System.String[]
@@ -84,7 +102,7 @@ Accept wildcard characters: False
 ```
 
 ### -Limit
-{{ Fill Limit Description }}
+.
 
 ```yaml
 Type: System.Int64
@@ -93,28 +111,13 @@ Aliases:
 
 Required: False
 Position: Named
-Default value: None
-Accept pipeline input: False
-Accept wildcard characters: False
-```
-
-### -Paginate
-{{ Fill Paginate Description }}
-
-```yaml
-Type: System.Boolean
-Parameter Sets: (All)
-Aliases:
-
-Required: False
-Position: Named
-Default value: None
+Default value: 0
 Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
 ### -SearchAfter
-{{ Fill SearchAfter Description }}
+.
 
 ```yaml
 Type: System.String[]
@@ -129,7 +132,7 @@ Accept wildcard characters: False
 ```
 
 ### -SearchTermAnd
-{{ Fill SearchTermAnd Description }}
+.
 
 ```yaml
 Type: System.Collections.Hashtable
@@ -144,7 +147,7 @@ Accept wildcard characters: False
 ```
 
 ### -SearchTermOr
-{{ Fill SearchTermOr Description }}
+.
 
 ```yaml
 Type: System.Collections.Hashtable
@@ -159,7 +162,7 @@ Accept wildcard characters: False
 ```
 
 ### -Service
-{{ Fill Service Description }}
+.
 
 ```yaml
 Type: System.String[]
@@ -174,7 +177,7 @@ Accept wildcard characters: False
 ```
 
 ### -Sort
-{{ Fill Sort Description }}
+.
 
 ```yaml
 Type: System.String
@@ -189,7 +192,7 @@ Accept wildcard characters: False
 ```
 
 ### -StartTime
-{{ Fill StartTime Description }}
+.
 
 ```yaml
 Type: System.String
@@ -203,19 +206,47 @@ Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
+### -Paginate
+Set to $true to return all results.
+
+```yaml
+Type: System.Boolean
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: True
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
 ### CommonParameters
 This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
 
 ## INPUTS
 
 ### JumpCloud.SDK.DirectoryInsights.Models.IEventQuery
-
 ## OUTPUTS
 
 ### JumpCloud.SDK.DirectoryInsights.Models.IGet200ApplicationJsonItemsItem
-
 ### System.String
-
 ## NOTES
+COMPLEX PARAMETER PROPERTIES
+To create the parameters described below, construct a hash table containing the appropriate properties.
+For information on hash tables, run Get-Help about_Hash_Tables.
+
+EVENTQUERYBODY \<IEventQuery\>: EventQuery is the users' command to search our auth logs         Limit - row count (validated/repaired)         SearchTerm - see above         Projection - list of field names to return from document (not validated)
+  \[EndTime \<String\>\]: 
+  \[Fields \<String\[\]\>\]: 
+  \[Limit \<Int64?\>\]: 
+  \[SearchAfter \<String\[\]\>\]: 
+  \[SearchTermAnd \<ISearchTermAnd\>\]: 
+    \[(Any) \<Object\>\]: This indicates any property can be added to this object.
+  \[SearchTermOr \<ISearchTermOr\>\]: 
+    \[(Any) \<Object\>\]: This indicates any property can be added to this object.
+  \[Service \<String\[\]\>\]: 
+  \[Sort \<String\>\]: 
+  \[StartTime \<String\>\]:
 
 ## RELATED LINKS
--- PowerShell/JumpCloud Module/Public/DirectoryInsights/Get-JCEvent.ps1
+++ PowerShell/JumpCloud Module/Public/DirectoryInsights/Get-JCEvent.ps1
@@ -1,34 +1,119 @@
+<#
+.Synopsis
+Query the API for Directory Insights events
+.Description
+Query the API for Directory Insights events
+.Example
+PS C:\> (Get-JCEvent -Service:('all') -StartTime:('2020-04-15T00:00:00Z') -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
+
+Pull all event records between Tue, 14 Apr 2020 18:00:00 -0600 to Thu, 16 Apr 2020 17:00:00 -0600
+.Example
+PS C:\> (Get-JCEvent -Service:('directory') -StartTime:('2020-04-15T00:00:00Z') -Limit:('10') -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
+
+Limit results to last 10 in the time range
+.Example
+PS C:\> ((Get-JCEvent -Service:('directory') -StartTime:('2020-04-15T00:00:00Z') -Sort:("DESC") -EndTime:('2020-04-16T23:00:00Z')).ToJsonString()| ConvertFrom-Json
+
+Sort descending results against timestamp value
+
+.Inputs
+JumpCloud.SDK.DirectoryInsights.Models.IEventQuery
+.Outputs
+JumpCloud.SDK.DirectoryInsights.Models.IGet200ApplicationJsonItemsItem
+.Outputs
+System.String
+.Notes
+COMPLEX PARAMETER PROPERTIES
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+EVENTQUERYBODY <IEventQuery>: EventQuery is the users' command to search our auth logs         Limit - row count (validated/repaired)         SearchTerm - see above         Projection - list of field names to return from document (not validated)
+  [EndTime <String>]: 
+  [Fields <String[]>]: 
+  [Limit <Int64?>]: 
+  [SearchAfter <String[]>]: 
+  [SearchTermAnd <ISearchTermAnd>]: 
+    [(Any) <Object>]: This indicates any property can be added to this object.
+  [SearchTermOr <ISearchTermOr>]: 
+    [(Any) <Object>]: This indicates any property can be added to this object.
+  [Service <String[]>]: 
+  [Sort <String>]: 
+  [StartTime <String>]: 
+#>
 Function Get-JCEvent
 {
     #Requires -modules JumpCloud.SDK.DirectoryInsights
     [OutputType([JumpCloud.SDK.DirectoryInsights.Models.IGet200ApplicationJsonItemsItem], [System.String])]
     [CmdletBinding(DefaultParameterSetName='GetExpanded', PositionalBinding=$false)]
     Param(
-        [Parameter(
-            ParameterSetName = 'Get',
-            Mandatory = $true,
-            ValueFromPipeline = $true
-        )]
-        [JumpCloud.SDK.DirectoryInsights.Models.IEventQuery]$EventQueryBody,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String]$EndTime,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String[]]$Fields,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.Int64]$Limit,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String[]]$SearchAfter,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.Collections.Hashtable]$SearchTermAnd,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.Collections.Hashtable]$SearchTermOr,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String[]]$Service,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String]$Sort,
-        [Parameter(ParameterSetName = 'GetExpanded')]
-        [System.String]$StartTime,
-        [System.Boolean]$Paginate = $true
+    [Parameter(ParameterSetName='Get', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [JumpCloud.SDK.DirectoryInsights.Models.IEventQuery]
+    # EventQuery is the users' command to search our auth logs
+    # Limit - row count (validated/repaired)
+    # SearchTerm - see above
+    # Projection - list of field names to return from document (not validated)
+    # To construct, see NOTES section for EVENTQUERYBODY properties and create a hash table.
+    ${EventQueryBody},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String]
+    # .
+    ${EndTime},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String[]]
+    # .
+    ${Fields},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.Int64]
+    # .
+    ${Limit},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String[]]
+    # .
+    ${SearchAfter},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes=([JumpCloud.SDK.DirectoryInsights.Models.ISearchTermAnd]))]
+    [System.Collections.Hashtable]
+    # .
+    ${SearchTermAnd},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes=([JumpCloud.SDK.DirectoryInsights.Models.ISearchTermOr]))]
+    [System.Collections.Hashtable]
+    # .
+    ${SearchTermOr},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String[]]
+    # .
+    ${Service},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String]
+    # .
+    ${Sort},
+
+    [Parameter(ParameterSetName='GetExpanded')]
+    [JumpCloud.SDK.DirectoryInsights.Category('Body')]
+    [System.String]
+    # .
+    ${StartTime},
+
+    [System.Boolean]
+    # Set to $true to return all results.
+    $Paginate = $true
     )
     Begin
     {
