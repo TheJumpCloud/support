4b7ea2227c59c21ec12b08b8f0abfe747eabf856
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-04-28 15:01:54 -0600

    add check if exist

--- PowerShell/JumpCloud Module/Private/HashFunctions/Get-Hash_ID_SystemGroupName.ps1
+++ PowerShell/JumpCloud Module/Private/HashFunctions/Get-Hash_ID_SystemGroupName.ps1
@@ -1,13 +1,13 @@
 Function Get-Hash_ID_SystemGroupName ()
 {
-
-    $SystemGroupHash =  New-Object System.Collections.Hashtable
-
+    $SystemGroupHash = New-Object System.Collections.Hashtable
     $SystemGroups = Get-JCGroup -Type System
-
-        foreach ($SystemGroup in $SystemGroups)
+    foreach ($SystemGroup in $SystemGroups)
+    {
+        If (-not $SystemGroupHash.ContainsKey($SystemGroup.name))
         {
             $SystemGroupHash.Add($SystemGroup.id, $SystemGroup.name)
         }
+    }
     return $SystemGroupHash
 }
