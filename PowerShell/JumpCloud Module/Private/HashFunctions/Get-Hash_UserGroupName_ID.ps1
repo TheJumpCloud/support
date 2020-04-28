b7c76057cba0d1126a5c354046a21d0391ce055c
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-04-28 11:43:15 -0600

    Add check before adding key

--- PowerShell/JumpCloud Module/Private/HashFunctions/Get-Hash_UserGroupName_ID.ps1
+++ PowerShell/JumpCloud Module/Private/HashFunctions/Get-Hash_UserGroupName_ID.ps1
@@ -1,14 +1,13 @@
 Function Get-Hash_UserGroupName_ID ()
 {
-
     $UserGroupHash = New-Object System.Collections.Hashtable
-
     $UserGroups = Get-JCGroup -Type User
-
     foreach ($Group in $UserGroups)
     {
-        $UserGroupHash.Add($Group.name, $Group.id)
+        If (-not $UserGroupHash.ContainsKey($Group.name))
+        {
+            $UserGroupHash.Add($Group.name, $Group.id)
+        }
     }
-
     return $UserGroupHash
 }
