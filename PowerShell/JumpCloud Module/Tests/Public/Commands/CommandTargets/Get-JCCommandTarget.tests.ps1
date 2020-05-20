f2ec03b2231785b47a0767532e878280c6b74a6d
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-20 17:23:47 -0600

    Undoing what I did

--- PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Get-JCCommandTarget.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Get-JCCommandTarget.tests.ps1
@@ -1,23 +1,21 @@
 Describe -Tag:('JCCommandTarget') 'Get-JCCommandTarget 1.3' {
     Connect-JCOnline -JumpCloudApiKey:($TestOrgAPIKey) -force | Out-Null
     It "Returns a JumpCloud commands system targets" {
+        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams.CommandID -SystemID $PesterParams.SystemID
         $TargetAdd = Add-JCCommandTarget -CommandID $PesterParams.CommandID -SystemID $PesterParams.SystemID
 
         $SystemTarget = Get-JCCommandTarget -CommandID $PesterParams.CommandID
 
         $SystemTarget.SystemID.count | Should -BeGreaterThan 0
-
-        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams.CommandID -SystemID $PesterParams.SystemID
     }
 
     It "Returns a JumpCloud commands group targets by groupname" {
+        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams.CommandID -GroupName $PesterParams.SystemGroupName
         $TargetAdd = Add-JCCommandTarget -CommandID $PesterParams.CommandID -GroupName $PesterParams.SystemGroupName
 
         $SystemGroupTarget = Get-JCCommandTarget -CommandID $PesterParams.CommandID -Groups
 
         $SystemGroupTarget.GroupID.count | Should -BeGreaterThan 0
-
-        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams.CommandID -GroupName $PesterParams.SystemGroupName
     }
 
 }
