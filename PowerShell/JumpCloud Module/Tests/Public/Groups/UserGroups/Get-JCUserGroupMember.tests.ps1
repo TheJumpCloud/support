729c0170328f4deca3eec3191e70de3898411b9c
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-27 14:06:46 -0600

    Capitalization fix?

--- PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
@@ -1,6 +1,6 @@
 Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
     Connect-JCOnline -JumpCloudApiKey:($TestOrgAPIKey) -force | Out-Null
-    Add-JCUserGroupMember -GroupName $PesterParams.UserGroupName -username $PesterParams.Username
+    Add-JCUserGroupMember -GroupName $PesterParams.UserGroupName -Username $PesterParams.Username
     It 'Gets a User Groups membership by Groupname' {
         $UserGroupMembers = Get-JCUserGroupMember -GroupName $PesterParams.UserGroupName
         $UserGroupMembers.id.Count | Should -BeGreaterThan 0
@@ -10,5 +10,5 @@ Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
         $UserGroupMembers = Get-JCUserGroupMember -ByID $PesterParams.UserGroupID
         $UserGroupMembers.to.id.Count | Should -BeGreaterThan 0
     }
-    Remove-JCUserGroupMember -GroupName $PesterParams.UserGroupName -username $PesterParams.Username
+    Remove-JCUserGroupMember -GroupName $PesterParams.UserGroupName -Username $PesterParams.Username
 }
