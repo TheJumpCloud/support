4b407d5979b6bc1027290f07c386833c744e999b
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-27 13:56:07 -0600

    UserGroup test fix

--- PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
@@ -1,5 +1,6 @@
 Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
     Connect-JCOnline -JumpCloudApiKey:($TestOrgAPIKey) -force | Out-Null
+    Add-JCUserGroupMember -GroupName $PesterParams.UserGroupName -username $PesterParams.Username
     It 'Gets a User Groups membership by Groupname' {
         $UserGroupMembers = Get-JCUserGroupMember -GroupName $PesterParams.UserGroupName
         $UserGroupMembers.id.Count | Should -BeGreaterThan 0
@@ -9,5 +10,5 @@ Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
         $UserGroupMembers = Get-JCUserGroupMember -ByID $PesterParams.UserGroupID
         $UserGroupMembers.to.id.Count | Should -BeGreaterThan 0
     }
-
+    Remove-JCUserGroupMember -GroupName $PesterParams.UserGroupName -username $PesterParams.Username
 }
