183d3651618cf6d2941f9dda3c4d3932b7976dfd
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-10 15:47:40 -0600

    add if not exist

--- PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Remove-JCRadiusServer.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Remove-JCRadiusServer.Tests.ps1
@@ -1,6 +1,10 @@
 Describe -Tag:('JCRadiusServer') 'Remove-JCRadiusServer Tests' {
     BeforeAll {
         $RadiusServerTemplate = Get-JCRadiusServer -Name:($PesterParams_RadiusServerName); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
+        If (-not $RadiusServerTemplate)
+        {
+            $RadiusServerTemplate = New-JCRadiusServer @PesterParams_NewRadiusServer
+        }
     }
     Context 'Remove-JCRadiusServer' {
         It ('Should remove a specific radius server.') {
