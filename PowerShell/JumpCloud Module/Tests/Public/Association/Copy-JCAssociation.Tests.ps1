c893cc3ac36a82e2a4c6ccae43b5332709a982da
Joe Workman <joe.workman@jumpcloud.com>
2020-06-10 15:54:21 -0600

    Associations tests potentially working

--- PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
@@ -1,9 +1,11 @@
-Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
+BeforeAll {
     $Users = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
     $SourceUser = $Users[0]
     $TargetUser = $Users[1]
     # Remove all associations from target user
     $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
+}
+Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
     Context ('Copy the associations of a user to another user ById.') {
         It("Associations should be the same.") {
             # Copy-JCAssociation -Type:('user') -Id:($SourceUser._id) -TargetId:($TargetUser._id) -Force
@@ -11,7 +13,7 @@ Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
         }
     }
     # Remove all associations from target user
-    $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
+    # $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
     Context ('Copy the associations of a user to another user ByName.') {
         It("Associations should be the same.") {
             # Copy-JCAssociation -Type:('user') -Name:($SourceUser.username) -TargetName:($TargetUser.username) -Force
