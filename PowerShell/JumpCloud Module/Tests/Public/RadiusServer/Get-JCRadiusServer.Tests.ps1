7520c4170a5c090ee16773290188922f395e2fae
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-10 13:40:27 -0600

    remove tests

--- PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Get-JCRadiusServer.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Get-JCRadiusServer.Tests.ps1
@@ -62,15 +62,15 @@ Describe -Tag:('JCRadiusServer') 'Get-JCRadiusServer Tests' {
             $RadiusServer | Should -Not -BeNullOrEmpty
             $RadiusServer.name | Should -Be $RadiusServerTemplate.name
         }
-        It ('Should return a specific radius server ByValue (ById).') {
-            $RadiusServer = Get-JCRadiusServer -SearchBy:('ById') -SearchByValue:($RadiusServerTemplate.id); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
-            $RadiusServer | Should -Not -BeNullOrEmpty
-            $RadiusServer.id | Should -Be $RadiusServerTemplate.id
-        }
-        It ('Should return a specific radius server ByValue (ByName).') {
-            $RadiusServer = Get-JCRadiusServer -SearchBy:('ByName') -SearchByValue:($RadiusServerTemplate.name); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
-            $RadiusServer | Should -Not -BeNullOrEmpty
-            $RadiusServer.name | Should -Be $RadiusServerTemplate.name
-        }
+        # It ('Should return a specific radius server ByValue (ById).') {
+        #     $RadiusServer = Get-JCRadiusServer -SearchBy:('ById') -SearchByValue:($RadiusServerTemplate.id); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
+        #     $RadiusServer | Should -Not -BeNullOrEmpty
+        #     $RadiusServer.id | Should -Be $RadiusServerTemplate.id
+        # }
+        # It ('Should return a specific radius server ByValue (ByName).') {
+        #     $RadiusServer = Get-JCRadiusServer -SearchBy:('ByName') -SearchByValue:($RadiusServerTemplate.name); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
+        #     $RadiusServer | Should -Not -BeNullOrEmpty
+        #     $RadiusServer.name | Should -Be $RadiusServerTemplate.name
+        # }
     }
 }
