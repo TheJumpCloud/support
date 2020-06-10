500d92fb2a06616e0ff79a5266afb0e6fde9a206
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-10 14:21:12 -0600

    break out radius server [skip ci]

--- PowerShell/JumpCloud Module/Tests/Public/RadiusServer/New-JCRadiusServer.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/RadiusServer/New-JCRadiusServer.Tests.ps1
@@ -1,1 +1,12 @@
-# See Get-JCRadiusServer.Tests.ps1
+Describe -Tag:('JCRadiusServer') 'New-JCRadiusServer Tests' {
+    BeforeAll {
+        $RadiusServerTemplate = Get-JCRadiusServer -Name:($PesterParams_RadiusServerName); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
+    }
+    Context 'New-JCRadiusServer' {
+        It ('Should create a new radius server.') {
+            $RadiusServer = $RadiusServerTemplate | New-JCRadiusServer # -Name:('') -networkSourceIp:('') -sharedSecret:('') -Force ;
+            $RadiusServer | Should -Not -BeNullOrEmpty
+            $RadiusServer.name | Should -Be $RadiusServerTemplate.name
+        }
+    }
+}
--- PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Remove-JCRadiusServer.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/RadiusServer/Remove-JCRadiusServer.Tests.ps1
@@ -1,1 +1,12 @@
-# See Get-JCRadiusServer.Tests.ps1
+Describe -Tag:('JCRadiusServer') 'Remove-JCRadiusServer Tests' {
+    BeforeAll {
+        $RadiusServerTemplate = Get-JCRadiusServer -Name:($PesterParams_RadiusServerName); # -Fields:('') -Filter:('') -Limit:(1) -Skip:(1) -Paginate:($true) -Force;
+    }
+    Context 'Remove-JCRadiusServer' {
+        It ('Should remove a specific radius server.') {
+            $RadiusServer = Remove-JCRadiusServer -Id:($RadiusServerTemplate.id) -Force;
+            $RadiusServer | Should -Not -BeNullOrEmpty
+            $RadiusServer.name | Should -Be $RadiusServerTemplate.name
+        }
+    }
+}
