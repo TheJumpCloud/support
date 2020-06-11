058b9eba870cdc6bf8a85392ef6e5b69f57c12cb
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-11 09:34:12 -0600

    update syntax

--- PowerShell/JumpCloud Module/Tests/Public/Groups/SystemGroups/New-JCSystemGroup.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/SystemGroups/New-JCSystemGroup.tests.ps1
@@ -1,9 +1,9 @@
 Describe -Tag:('JCSystemGroup') 'New-JCSystemGroup 1.0' {
     BeforeAll {
         Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
-        If (Get-JCGroup -Name:($PesterParams_SystemGroupName))
+        If (Get-JCGroup -Type:('System') | Where-Object { $_.name -eq $PesterParams_SystemGroupName })
         {
-            Remove-JCSystemGroup -GroupName:($PesterParams_SystemGroupName)
+            Remove-JCSystemGroup -GroupName:($PesterParams_SystemGroupName) -force
         }
     }
     It "Creates a new system group" {
