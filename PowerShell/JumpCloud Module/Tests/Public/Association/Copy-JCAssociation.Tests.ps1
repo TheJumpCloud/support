8ccc7eed748234d3f36e93a647ce54ddc3921acb
Joe Workman <joe.workman@jumpcloud.com>
2020-06-15 10:05:24 -0600

    copy associtaion tests

--- PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
@@ -1,23 +1,42 @@
 BeforeAll {
-    $Users = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
-    $SourceUser = $Users[0]
-    $TargetUser = $Users[1]
-    # Remove all associations from target user
-    $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
 }
 Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
-    Context ('Copy the associations of a user to another user ById.') {
-        It("Associations should be the same.") {
-            # Copy-JCAssociation -Type:('user') -Id:($SourceUser._id) -TargetId:($TargetUser._id) -Force
-            # ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
-        }
-    }
-    # Remove all associations from target user
-    # $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
-    Context ('Copy the associations of a user to another user ByName.') {
-        It("Associations should be the same.") {
-            # Copy-JCAssociation -Type:('user') -Name:($SourceUser.username) -TargetName:($TargetUser.username) -Force
-            # ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
+    Context ('some cool tests Im thinking about writing') {
+        It '<testDescription>' -TestCases @(
+            @{  testDescription = 'user case'
+                Users           = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
+                QType            = "username"
+                SourceTarget    = "Name"
+                DestTarget      = "TargetName"
+            }
+            @{  testDescription = 'id case'
+                Users           = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
+                QType            = "_id"
+                SourceTarget    = "Id"
+                DestTarget      = "TargetId"
+
+            }
+        ) {
+            param (
+                [string] $SourceTarget,
+                [string] $DestTarget,
+                [string] $QType
+            )
+            # define first testing users
+            $SourceUser = $Users[0]
+            $TargetUser = $Users[1]
+
+            # query for username TODO: combine with iex?
+            if ($QType -eq "username") {
+                Copy-JCAssociation -Type:('user') -Name:($SourceUser.$QType) -TargetName:($TargetUser.$QType) -Force
+            }
+            # query by id
+            if ($QType -eq "_id") {
+                Copy-JCAssociation -Type:('user') -Id:($SourceUser.$QType) -TargetId:($TargetUser.$QType) -Force
+            }
+            # compare results
+            ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
+            $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
         }
     }
 }
