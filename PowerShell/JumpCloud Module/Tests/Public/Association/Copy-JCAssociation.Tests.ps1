3c7b80519b8704995ee18cd7b5006287215b85c9
Joe Workman <joe.workman@jumpcloud.com>
2020-06-15 10:56:27 -0600

    Fixed copy associations

--- PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Association/Copy-JCAssociation.Tests.ps1
@@ -5,12 +5,14 @@ Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
         It '<testDescription>' -TestCases @(
             @{  testDescription = 'user case'
                 Users           = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
+                UserGroup       = (Get-JCObject -Type:('user_group') -limit:(1))[0]
                 QType            = "username"
                 SourceTarget    = "Name"
                 DestTarget      = "TargetName"
             }
             @{  testDescription = 'id case'
                 Users           = Get-JCObject -Type:('user') -Fields:('_id', 'username') -limit:(2)
+                UserGroup       = (Get-JCObject -Type:('user_group') -limit:(1))[0]
                 QType            = "_id"
                 SourceTarget    = "Id"
                 DestTarget      = "TargetId"
@@ -25,17 +27,17 @@ Describe -Tag:('JCAssociation') "Copy-JCAssociation Tests" {
             # define first testing users
             $SourceUser = $Users[0]
             $TargetUser = $Users[1]
-
-            # query for username TODO: combine with iex?
-            if ($QType -eq "username") {
-                Copy-JCAssociation -Type:('user') -Name:($SourceUser.$QType) -TargetName:($TargetUser.$QType) -Force
+            
+            # write-host($UserGroup.id)
+            If (-not (Get-JCAssociation -Type:('user') -Id:($SourceUser._id) -TargetType:('user_group') | Where-Object { $_.TargetId -eq $UserGroup.id })){
+                Add-JCAssociation -Type:('user') -Id:($SourceUser._id) -TargetType:('user_group') -TargetId:($UserGroup.id) -Force
             }
-            # query by id
-            if ($QType -eq "_id") {
-                Copy-JCAssociation -Type:('user') -Id:($SourceUser.$QType) -TargetId:($TargetUser.$QType) -Force
-            }
+            # build query
+            $Command = "Copy-JCAssociation -Type:('user') -$($SourceTarget):('$($SourceUser.$QType)') -$($DestTarget):('$($TargetUser.$QType)') -Force"
+            invoke-expression -command $Command
             # compare results
             ($SourceUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json ) | Should -Be ($TargetUser | Get-JCAssociation | Select-Object -Property:('associationType', 'type', 'targetId', 'targetType', 'compiledAttributes') | ConvertTo-Json )
+            $SourceUser | Get-JCAssociation | Remove-JCAssociation -force
             $TargetUser | Get-JCAssociation | Remove-JCAssociation -force
         }
     }
