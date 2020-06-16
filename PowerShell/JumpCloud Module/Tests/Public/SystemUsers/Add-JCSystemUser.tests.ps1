749c0b7856dedfc1decc6fd49e94895be5cbad7a
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-16 13:20:47 -0600

    add remove steps [skip ci]

--- PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Add-JCSystemUser.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Add-JCSystemUser.tests.ps1
@@ -24,65 +24,42 @@ Describe -Tag:('JCSystemUser') 'Add-JCSystemUser 1.0' {
 Describe -Tag:('JCSystemUser') 'Add-JCSystemUser 1.1.0' {
 
     It "Adds a JumpCloud User to a JumpCloud System with admin `$False using username" {
-
+        $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $FalseUser = Add-JCSystemUser -Username $PesterParams_User1.username -SystemID $PesterParams_SystemLinux._id -Administrator $False
-
         $FalseUser.Administrator | Should -Be $False
-
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $FalseUser.Username | Select-Object Administrator
-
         $GetUser.Administrator | Should -Be $False
-
     }
 
     It "Adds a JumpCloud User to a JumpCloud System with admin $False using username" {
-
+        $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $FalseUser = Add-JCSystemUser -Username $PesterParams_User1.username -SystemID $PesterParams_SystemLinux._id -Administrator $False
-
         $FalseUser.Administrator | Should -Be $False
-
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $FalseUser.Username | Select-Object Administrator
-
         $GetUser.Administrator | Should -Be $False
-
     }
 
     It "Adds a JumpCloud User to a JumpCloud System with admin `$False using UserID" {
-
+        $UserRemove = Remove-JCSystemUser -UserID $PesterParams_User1.id -SystemID $PesterParams_SystemLinux._id -force
         $FalseUser = Add-JCSystemUser -UserID $PesterParams_User1._id -SystemID $PesterParams_SystemLinux._id -Administrator $False
-
         $FalseUser.Administrator | Should -Be $False
-
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $PesterParams_User1.Username | Select-Object Administrator
-
         $GetUser.Administrator | Should -Be $False
-
     }
 
     It "Adds a JumpCloud User to a JumpCloud System with admin `$True using UserID" {
-
         $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $TrueUser = Add-JCSystemUser -UserID $PesterParams_User1._id -SystemID $PesterParams_SystemLinux._id -Administrator $True
-
         $TrueUser.Administrator | Should -Be $True
-
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $PesterParams_User1.Username | Select-Object Administrator
-
         $GetUser.Administrator | Should -Be $True
-
     }
 
     It "Adds a JumpCloud User to a JumpCloud System with admin $True using username" {
         $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $TrueUser = Add-JCSystemUser -Username $PesterParams_User1.username -SystemID $PesterParams_SystemLinux._id -Administrator $True
-
         $TrueUser.Administrator | Should -Be $True
-
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $TrueUser.Username | Select-Object Administrator
-
         $GetUser.Administrator | Should -Be $True
-
     }
-
-
 }
