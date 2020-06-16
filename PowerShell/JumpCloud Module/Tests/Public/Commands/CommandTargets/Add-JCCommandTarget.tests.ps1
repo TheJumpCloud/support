5bbea42bff5673c459a4222609c94cc1767a0a45
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-16 12:25:45 -0600

    update beforeall

--- PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Add-JCCommandTarget.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Add-JCCommandTarget.tests.ps1
@@ -1,5 +1,15 @@
 Describe -Tag:('JCCommandTarget') 'Add-JCCommandTarget 1.3' {
-    BeforeAll { Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null }
+    BeforeAll {
+        Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
+        If (-not (Get-JCCommand -CommandID:($PesterParams_Command1.Id)))
+        {
+            $PesterParams_Command1 = New-JCCommand @PesterParams_NewCommand1
+        }
+        If (-not (Get-JCGroup -Type:('System') | Where-Object { $_.name -eq $PesterParams_SystemGroup.Name }))
+        {
+            $PesterParams_SystemGroup = New-JCSystemGroup @PesterParams_NewSystemGroup
+        }
+    }
     It "Adds a single system to a JumpCloud command" {
 
         $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams_Command1.id -SystemID $PesterParams_SystemLinux._id
