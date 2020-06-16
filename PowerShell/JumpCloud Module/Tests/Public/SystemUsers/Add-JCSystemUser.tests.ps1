e8736c741fd70f906bc71e3948a0219cd415eda8
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-16 13:48:47 -0600

    update parameter [skip ci]

--- PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Add-JCSystemUser.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Add-JCSystemUser.tests.ps1
@@ -1,5 +1,7 @@
 Describe -Tag:('JCSystemUser') 'Add-JCSystemUser 1.0' {
-    BeforeAll { Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null }
+    BeforeAll {
+        Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
+    }
     It "Adds a single user to a single system by Username and SystemID" {
         $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $UserAdd = Add-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id
@@ -40,7 +42,7 @@ Describe -Tag:('JCSystemUser') 'Add-JCSystemUser 1.1.0' {
     }
 
     It "Adds a JumpCloud User to a JumpCloud System with admin `$False using UserID" {
-        $UserRemove = Remove-JCSystemUser -UserID $PesterParams_User1.id -SystemID $PesterParams_SystemLinux._id -force
+        $UserRemove = Remove-JCSystemUser -Username $PesterParams_User1.Username -SystemID $PesterParams_SystemLinux._id -force
         $FalseUser = Add-JCSystemUser -UserID $PesterParams_User1._id -SystemID $PesterParams_SystemLinux._id -Administrator $False
         $FalseUser.Administrator | Should -Be $False
         $GetUser = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id | Where-Object Username -EQ $PesterParams_User1.Username | Select-Object Administrator
