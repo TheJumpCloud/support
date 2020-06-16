0e32bc7e9a07735a5ea57f696d7df3d7d87eaedc
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-16 09:44:30 -0600

    update target name [skip ci]

--- PowerShell/JumpCloud Module/Tests/Public/Policies/PolicyTargets/Get-JCPolicyTargetSystem.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Policies/PolicyTargets/Get-JCPolicyTargetSystem.tests.ps1
@@ -2,9 +2,9 @@ Describe -Tag:('JCPolicyTargetSystem') 'Get-JCPolicyTargetSystem 1.10' {
     BeforeAll {
         Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
         $PesterParams_MultiplePolicy | ForEach-Object {
-            If (-not (Get-JCAssociation -Type:('policy') -Id:($_.Id) -TargetType:('system') -IncludeNames | Where-Object { $_.TargetName -eq $PesterParams_SystemNameWindows }))
+            If (-not (Get-JCAssociation -Type:('policy') -Id:($_.Id) -TargetType:('system') -IncludeNames | Where-Object { $_.TargetName -eq $PesterParams_SystemLinux.displayName }))
             {
-                Add-JCAssociation -Type:('policy') -Id:($_.Id) -TargetType:('system') -TargetName:($PesterParams_SystemNameWindows) -Force
+                Add-JCAssociation -Type:('policy') -Id:($_.Id) -TargetType:('system') -TargetName:($PesterParams_SystemLinux.displayName) -Force
             }
         }
     }
