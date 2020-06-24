6b6567ba52031955e69726b7017f2e540b2110ae
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-24 12:46:27 -0600

    remove extra tests [skip ci]

--- PowerShell/JumpCloud Module/Tests/Public/Systems/Get-JCSystemInsights.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Systems/Get-JCSystemInsights.Tests.ps1
@@ -7,30 +7,30 @@ Describe -Tag:('JCSystemInsights') "Get-JCSystemInsights Tests" {
     Function Get-JCSystemInsightsTestCases($System)
     {
         # Retrieve objects to test with
-        $TableNames = $PesterParams_SystemInsightsTables | Where-Object { $_ -notin ('disk_info', 'bitlocker_info', 'uptime', 'sip_config', 'alf', 'shared_resources', 'user_ssh_keys', 'user_groups', 'sharing_preferences', 'scheduled_tasks') } # HACK Temp workaround because these tables don't take strings as filters
+        $TableNames = $PesterParams_SystemInsightsTables | Where-Object { $_ -notin ('disk_info', 'bitlocker_info', 'uptime', 'sip_config', 'alf', 'shared_resources', 'user_ssh_keys', 'user_groups', 'sharing_preferences', 'scheduled_tasks', 'alf_exceptions') } # HACK Temp workaround because these tables don't take strings as filters
         $SystemInsightsTestCases = @()
         $TableNames | ForEach-Object {
             $TableName = $_
             $SystemInsightsTestCases += @{
-                testDescription = 'Test a specific table across all systems where error is NullOrEmpty.'
+                testDescription = "Test table '$TableName' across all systems where error is NullOrEmpty."
                 Command         = "Get-JCSystemInsights -Table:('$TableName');"
             }
             $SystemInsightsTestCases += @{
-                testDescription = 'Test a specific table across specified systems ById where error is NullOrEmpty.'
+                testDescription = "Test table '$TableName' across specified systems ById where error is NullOrEmpty."
                 Command         = "Get-JCSystemInsights -Table:('$TableName') -Id:('$(($System._id) -join "','")');"
             }
+            # $SystemInsightsTestCases += @{
+            #     testDescription = "Test table '$TableName' across specified systems ByValue Id where error is NullOrEmpty."
+            #     Command         = "Get-JCSystemInsights -Table:('$TableName') -SearchBy:('ById') -SearchByValue:('$(($System._id) -join "','")');"
+            # }
             $SystemInsightsTestCases += @{
-                testDescription = 'Test a specific table across specified systems ByValue Id where error is NullOrEmpty.'
-                Command         = "Get-JCSystemInsights -Table:('$TableName') -SearchBy:('ById') -SearchByValue:('$(($System._id) -join "','")');"
-            }
-            $SystemInsightsTestCases += @{
-                testDescription = 'Test a specific table across specified systems ByName where error is NullOrEmpty.'
+                testDescription = "Test table '$TableName' across specified systems ByName where error is NullOrEmpty."
                 Command         = "Get-JCSystemInsights -Table:('$TableName') -Name:('$(($System.displayName) -join "','")');"
             }
-            $SystemInsightsTestCases += @{
-                testDescription = 'Test a specific table across specified systems ByValue Name where error is NullOrEmpty.'
-                Command         = "Get-JCSystemInsights -Table:('$TableName') -SearchBy:('ByName') -SearchByValue:('$(($System.displayName) -join "','")');"
-            }
+            # $SystemInsightsTestCases += @{
+            #     testDescription = "Test table '$TableName' across specified systems ByValue Name where error is NullOrEmpty."
+            #     Command         = "Get-JCSystemInsights -Table:('$TableName') -SearchBy:('ByName') -SearchByValue:('$(($System.displayName) -join "','")');"
+            # }
         }
         Return $SystemInsightsTestCases
     }
