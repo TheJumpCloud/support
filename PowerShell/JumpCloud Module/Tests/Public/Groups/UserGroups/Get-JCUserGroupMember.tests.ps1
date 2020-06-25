1c566ace05ada10de45651876f73c28b4baa2231
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-25 08:46:53 -0600

    add setup to beforeall

--- PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
@@ -1,9 +1,13 @@
 Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
     BeforeAll {
         Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
+        If (-not (Get-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name)))
+        {
+            Add-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name) -Username:($PesterParams_User1.username)
+        }
     }
     It 'Gets a User Groups membership by Groupname' {
-        $UserGroupMembers = Get-JCUserGroupMember -GroupName $PesterParams_UserGroup.Name
+        $UserGroupMembers = Get-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name)
         $UserGroupMembers.UserID.Count | Should -BeGreaterThan 0
     }
     It 'Gets a User Groups membership -ByID' {
