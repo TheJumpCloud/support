c63d042e7004ecf412c8eca5fcdbaa0f55b7fd62
Joe Workman <joe.workman@jumpcloud.com>
2020-06-30 15:53:44 -0600

    update module test

--- PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
@@ -1,14 +1,15 @@
 Describe -Tag:('JCModule') 'Test for Update-JCModule' {
     It ('Where the local version number has been updated to match the PowerShell gallery version number') {
         Install-Module -Name:('JumpCloud') -RequiredVersion:('1.0.0') -Scope:('CurrentUser') -Force
-        $InitialModule = Get-Module -Name:('JumpCloud') -All | Where-Object { $_.Version -eq '1.0.0' }
+        $InitialModule = Get-InstalledModule -Name:('JumpCloud') -AllVersions | Where-Object { $_.Version -eq '1.0.0' }
         $PowerShellGalleryModule = Find-Module -Name:('JumpCloud') -ErrorAction:('Ignore')
-        $LocalModulePre = Get-Module -Name:('JumpCloud')
+        $LocalModulePre = Get-InstalledModule -Name:('JumpCloud')
         Write-Host ('PowerShellGallery Version: ' + $PowerShellGalleryModule.Version)
         Write-Host ('Local Version Before: ' + $LocalModulePre.Version)
         Update-JCModule -SkipUninstallOld -Force
         $InitialModule | Remove-Module
-        $LocalModulePost = Get-Module -Name:('JumpCloud') -All | Where-Object { $_.Version -eq $PowerShellGalleryModule.Version }
+        $InitialModule | Uninstall-Module -Force
+        $LocalModulePost = Get-InstalledModule -Name:('JumpCloud') -AllVersions | Where-Object { $_.Version -eq $PowerShellGalleryModule.Version }
         If ($LocalModulePost)
         {
             Write-Host ('Local Version After: ' + $LocalModulePost.Version)
