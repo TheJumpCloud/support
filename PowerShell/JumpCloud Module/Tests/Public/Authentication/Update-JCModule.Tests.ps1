452c718d9554413e7fc07a9c8f0e9acefa52f34d
Joe Workman <joe.workman@jumpcloud.com>
2020-07-01 15:11:53 -0600

    revert back to orig jcmodule tests

--- PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Authentication/Update-JCModule.Tests.ps1
@@ -1,27 +1,25 @@
 Describe -Tag:('JCModule') 'Test for Update-JCModule' {
     It ('Where the local version number has been updated to match the PowerShell gallery version number') {
         Install-Module -Name:('JumpCloud') -RequiredVersion:('1.0.0') -Scope:('CurrentUser') -Force
-        $InitialModule = Get-InstalledModule -Name:('JumpCloud') -AllVersions | Where-Object { $_.Version -eq '1.0.0' }
+        $InitialModule = Get-Module -Name:('JumpCloud') -All | Where-Object { $_.Version -eq '1.0.0' }
         $PowerShellGalleryModule = Find-Module -Name:('JumpCloud') -ErrorAction:('Ignore')
-        $LocalModulePre = Get-InstalledModule -Name:('JumpCloud') -AllVersions  | Where-Object { $_.Version -ne $PowerShellGalleryModule.Version }
+        $LocalModulePre = Get-Module -Name:('JumpCloud')
         Write-Host ('PowerShellGallery Version: ' + $PowerShellGalleryModule.Version)
         Write-Host ('Local Version Before: ' + $LocalModulePre.Version)
-        Write-Host('Testing Module Update Function on Version: ' + (Get-Command -Name "Update-JCModule").Version)
         Update-JCModule -SkipUninstallOld -Force
         $InitialModule | Remove-Module
-        $InitialModule | Uninstall-Module -Force
-        $LocalModulePost = Get-InstalledModule -Name:('JumpCloud') -AllVersions | Where-Object { $_.Version -eq $PowerShellGalleryModule.Version }
-        If ($LocalModulePost)
-        {
+        $LocalModulePost = Get-Module -Name:('JumpCloud') -All | Where-Object { $_.Version -eq $PowerShellGalleryModule.Version }
+        If ($LocalModulePost) {
             Write-Host ('Local Version After: ' + $LocalModulePost.Version)
             $LocalModulePost | Remove-Module
         }
-        Else
-        {
+        Else {
             Write-Error ('Unable to find latest version of the JumpCloud PowerShell module installed on local machine.')
         }
-        $InitialModule.Version | Should -Not -Be $PowerShellGalleryModule.Version
         $LocalModulePost.Version | Should -Be $PowerShellGalleryModule.Version
         $LocalModulePost | Should -Not -BeNullOrEmpty
+    }
+    AfterAll{
+        Import-Module -Name:("$PesterParams_ModuleManifestPath/$PesterParams_ModuleManifestName") -Force
     }
 }
