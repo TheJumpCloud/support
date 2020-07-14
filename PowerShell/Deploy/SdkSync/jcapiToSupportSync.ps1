059d3e956a31106404492e106340fe6c00e853f6
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-07-14 09:58:41 -0600

    add script analyzer

--- PowerShell/Deploy/SdkSync/jcapiToSupportSync.ps1
+++ PowerShell/Deploy/SdkSync/jcapiToSupportSync.ps1
@@ -1,3 +1,4 @@
+Install-Module -Name:('PSScriptAnalyzer')
 $ApprovedFunctions = [Ordered]@{
     'JumpCloud.SDK.DirectoryInsights' = @(
         [PSCustomObject]@{
@@ -18,9 +19,6 @@ $Divider = '|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|'
 $FunctionTemplate = "{0}`nFunction {1}`n{{`n$($IndentChar){2}`n$($IndentChar)Param(`n{3}`n$($IndentChar))`n$($IndentChar)Begin`n$($IndentChar){{`n{4}`n$($IndentChar)}}`n$($IndentChar)Process`n$($IndentChar){{`n{5}`n$($IndentChar)}}`n$($IndentChar)End`n$($IndentChar){{`n{6}`n$($IndentChar)}}`n}}"
 $ScriptAnalyzerResults = @()
 $JumpCloudModulePath = (Get-Item -Path:($PSScriptRoot)).Parent.Parent.FullName + '/JumpCloud Module'
-#####################################################################
-. ((Get-Item -Path:($PSScriptRoot)).Parent.FullName + '/Setup-Dependencies.ps1')
-#####################################################################
 $Modules = Get-Module -Name:($Psd1.RequiredModules | Where-Object { $_ -in $ApprovedFunctions.Keys })
 ForEach ($Module In $Modules)
 {
