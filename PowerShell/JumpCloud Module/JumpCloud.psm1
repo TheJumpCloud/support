c1d2be3e611e7f30aeb9ae522e40130406373e5e
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-08-05 13:15:34 -0600

    move logic into script

--- PowerShell/JumpCloud Module/JumpCloud.psm1
+++ PowerShell/JumpCloud Module/JumpCloud.psm1
@@ -15,43 +15,6 @@ Foreach ($Import in @($Public + $Private))
 # Set default values for function parameters
 $PSDefaultParameterValues['Invoke-RestMethod:ContentType'] = 'application/json; charset=utf-8'
 $PSDefaultParameterValues['Invoke-WebRequest:ContentType'] = 'application/json; charset=utf-8'
-# Populate values for function parameters
-$SystemInsightsPrefix = 'Get-JcSdkSystemInsight'
-$SystemInsightsTables = @{}
-$Commands = Get-Command -Module:('JumpCloud.SDK.V2') -Name:("$SystemInsightsPrefix*")
-ForEach ($Command In $Commands)
-{
-    $Help = Get-Help -Name:($Command)
-    $SystemInsightsTables.Add($Command.Name.Replace($SystemInsightsPrefix, ''), $Help.Description.Text + ' ' + $Help.parameters.parameter.Where( { $_.Name -eq 'filter' }).Description.Text + ' EX: {field}:{operator}:{searchValue}' )
-}
-Register-ArgumentCompleter -CommandName Get-JCSystemInsightsElliott -ParameterName Table -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-    $FilterFilter = $fakeBoundParameter.Filter
-    $SystemInsightsTables.Keys | Where-Object { $_ -like "${wordToComplete}*" } | Where-Object {
-        $SystemInsightsTables.$_ -like "${FilterFilter}*"
-    } | ForEach-Object {
-        New-Object System.Management.Automation.CompletionResult (
-            $_,
-            $_,
-            'ParameterValue',
-            $_
-        )
-    }
-}
-Register-ArgumentCompleter -CommandName Get-JCSystemInsightsElliott -ParameterName Filter -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-    $TypeFilter = $fakeBoundParameter.Table
-    $SystemInsightsTables.Keys | Where-Object { $_ -like "${TypeFilter}*" } | ForEach-Object { $SystemInsightsTables.$_ |
-        Where-Object { $_ -like "${wordToComplete}*" } } |
-    Sort-Object -Unique | ForEach-Object {
-        New-Object System.Management.Automation.CompletionResult (
-            $_,
-            $_,
-            'ParameterValue',
-            $_
-        )
-    }
-}
 # https://docs.microsoft.com/en-us/dotnet/api/system.net.servicepointmanager?view=netcore-3.1
 # Required
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls, [System.Net.SecurityProtocolType]::Tls11, [System.Net.SecurityProtocolType]::Tls12
--- PowerShell/JumpCloud Module/Public/Systems/Get-JCSystemInsightsElliott.ps1
+++ PowerShell/JumpCloud Module/Public/Systems/Get-JCSystemInsightsElliott.ps1
@@ -1,3 +1,40 @@
+# Populate values for function parameters
+$SystemInsightsPrefix = 'Get-JcSdkSystemInsight'
+$SystemInsightsTables = @{}
+$Commands = Get-Command -Module:('JumpCloud.SDK.V2') -Name:("$SystemInsightsPrefix*")
+ForEach ($Command In $Commands)
+{
+    $Help = Get-Help -Name:($Command)
+    $SystemInsightsTables.Add($Command.Name.Replace($SystemInsightsPrefix, ''), $Help.Description.Text + ' ' + $Help.parameters.parameter.Where( { $_.Name -eq 'filter' }).Description.Text + ' EX: {field}:{operator}:{searchValue}' )
+}
+Register-ArgumentCompleter -CommandName Get-JCSystemInsightsElliott -ParameterName Table -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $FilterFilter = $fakeBoundParameter.Filter
+    $SystemInsightsTables.Keys | Where-Object { $_ -like "${wordToComplete}*" } | Where-Object {
+        $SystemInsightsTables.$_ -like "${FilterFilter}*"
+    } | ForEach-Object {
+        New-Object System.Management.Automation.CompletionResult (
+            $_,
+            $_,
+            'ParameterValue',
+            $_
+        )
+    }
+}
+Register-ArgumentCompleter -CommandName Get-JCSystemInsightsElliott -ParameterName Filter -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $TypeFilter = $fakeBoundParameter.Table
+    $SystemInsightsTables.Keys | Where-Object { $_ -like "${TypeFilter}*" } | ForEach-Object { $SystemInsightsTables.$_ |
+        Where-Object { $_ -like "${wordToComplete}*" } } |
+    Sort-Object -Unique | ForEach-Object {
+        New-Object System.Management.Automation.CompletionResult (
+            $_,
+            $_,
+            'ParameterValue',
+            $_
+        )
+    }
+}
 Function Get-JCSystemInsightsElliott
 {
     [CmdletBinding(DefaultParameterSetName = 'List', PositionalBinding = $false)]
