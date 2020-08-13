c437aabaf934726db95b8cce356e0735e3f74fe9
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-08-13 14:19:33 -0600

    maybe this will work

--- PowerShell/Deploy/Build-WikiPages.ps1
+++ PowerShell/Deploy/Build-WikiPages.ps1
@@ -1,44 +1,40 @@
 . ($PSScriptRoot + '/' + 'Get-Config.ps1')
 ###########################################################################
-$CurrentLocation = Get-Location
-Set-Location -Path:($StagingDirectory)
 Invoke-GitClone -Repo:($GitSourceRepoWiki)
-
-### Add step check out support wiki
-# $PathToSupportWikiRepo = ''
-# $SupportRepoDocs = $PSScriptRoot + '/Docs'
-# $SupportWiki = $PathToSupportWikiRepo + '/support.wiki'
-# $Docs = Get-ChildItem -Path:($SupportRepoDocs + '/*.md') -Recurse
-# ForEach ($Doc In $Docs)
-# {
-#     $DocName = $Doc.Name
-#     $DocFullName = $Doc.FullName
-#     $SupportWikiDocFullName = $SupportWiki + '/' + $DocName
-#     $DocContent = Get-Content -Path:($DocFullName)
-#     If (Test-Path -Path:($SupportWikiDocFullName))
-#     {
-#         $SupportWikiDocContent = Get-Content -Path:($SupportWikiDocFullName)
-#         $Diffs = Compare-Object -ReferenceObject:($DocContent) -DifferenceObject:($SupportWikiDocContent)
-#         If ($Diffs)
-#         {
-#             Write-Warning -Message:('Diffs found in: ' + $DocName)
-#             # are you sure you want to continue?
-#         }
-#     }
-#     Else
-#     {
-#         Write-Warning -Message:('Creating new file: ' + $DocName)
-#     }
-#     $NewDocContent = If (($DocContent | Select-Object -First 1) -eq '---')
-#     {
-#         $DocContent | Select-Object -Skip:(7)
-#     }
-#     Else
-#     {
-#         $DocContent
-#     }
-#     Set-Content -Path:($SupportWikiDocFullName) -Value:($NewDocContent) -Force
-# }
-# ### Add step check in changes to support wiki
-
-Set-Location -Path:($CurrentLocation)
+$SupportRepoDocs = "$FolderPath_Module/Docs"
+$SupportWiki = "$ScriptRoot/support.wiki"
+If (!(Test-Path -Path:($SupportWiki))) { New-Item -Path:($SupportWiki) -ItemType:('Directory') }
+Set-Location -Path:($SupportWiki)
+$Docs = Get-ChildItem -Path:($SupportRepoDocs + '/*.md') -Recurse
+ForEach ($Doc In $Docs)
+{
+    $DocName = $Doc.Name
+    $DocFullName = $Doc.FullName
+    $SupportWikiDocFullName = $SupportWiki + '/' + $DocName
+    $DocContent = Get-Content -Path:($DocFullName)
+    If (Test-Path -Path:($SupportWikiDocFullName))
+    {
+        $SupportWikiDocContent = Get-Content -Path:($SupportWikiDocFullName)
+        $Diffs = Compare-Object -ReferenceObject:($DocContent) -DifferenceObject:($SupportWikiDocContent)
+        If ($Diffs)
+        {
+            Write-Warning -Message:('Diffs found in: ' + $DocName)
+            # are you sure you want to continue?
+        }
+    }
+    Else
+    {
+        Write-Warning -Message:('Creating new file: ' + $DocName)
+    }
+    $NewDocContent = If (($DocContent | Select-Object -First 1) -eq '---')
+    {
+        $DocContent | Select-Object -Skip:(7)
+    }
+    Else
+    {
+        $DocContent
+    }
+    Set-Content -Path:($SupportWikiDocFullName) -Value:($NewDocContent) -Force
+}
+# Check in changes to support wiki
+Invoke-GitCommit -BranchName:($GitSourceRepoWiki)
