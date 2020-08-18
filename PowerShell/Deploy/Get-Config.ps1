174a029f5be715011e15402c3647de71e68b03fb
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-08-18 08:04:42 -0600

    [skip ci] move things around

--- PowerShell/Deploy/Get-Config.ps1
+++ PowerShell/Deploy/Get-Config.ps1
@@ -1,30 +1,29 @@
+$ModuleName = $env:MODULENAME
+$ModuleFolderName = $env:MODULEFOLDERNAME
+$DEPLOYFOLDER = $env:DEPLOYFOLDER
+$GitSourceBranch = $env:BUILD_SOURCEBRANCHNAME
+$GitSourceRepo = $env:BUILD_REPOSITORY_URI
+$StagingDirectory = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+$GitSourceRepoWiki = $GitSourceRepo + '.wiki'
+$RELEASETYPE = $env:RELEASETYPE
+$XAPIKEY_PESTER = $env:XAPIKEY_PESTER
+$XAPIKEY_MTP = $env:XAPIKEY_MTP
+$NUGETAPIKEY = $env:NUGETAPIKEY
+$SYSTEM_ACCESSTOKEN = $env:SYSTEM_ACCESSTOKEN
 # Log statuses
 Write-Host ('[status]Platform: ' + [environment]::OSVersion.Platform)
 Write-Host ('[status]PowerShell Version: ' + ($PSVersionTable.PSVersion -join '.'))
 Write-Host ('[status]Host: ' + (Get-Host).Name)
 Write-Host ('[status]Loaded config: ' + $MyInvocation.MyCommand.Path)
 # Set variables from Azure Pipelines
-$ModuleName = $env:MODULENAME
-$ModuleFolderName = $env:MODULEFOLDERNAME
-$GitSourceBranch = $env:BUILD_SOURCEBRANCHNAME
-$GitSourceRepo = $env:BUILD_REPOSITORY_URI
-$StagingDirectory = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-$GitSourceRepoWiki = $GitSourceRepo + '.wiki'
-$ScriptRoot = Switch ($env:DEPLOYFOLDER) { $true { $env:DEPLOYFOLDER } Default { $PSScriptRoot } }
+$ScriptRoot = Switch ($DEPLOYFOLDER) { $true { $DEPLOYFOLDER } Default { $PSScriptRoot } }
 $FolderPath_ModuleRootPath = (Get-Item -Path:($ScriptRoot)).Parent.FullName
-$RELEASETYPE = $env:RELEASETYPE
-$XAPIKEY_PESTER = $env:XAPIKEY_PESTER
-$XAPIKEY_MTP = $env:XAPIKEY_MTP
-$NUGETAPIKEY = $env:NUGETAPIKEY
-$SYSTEM_ACCESSTOKEN = $env:SYSTEM_ACCESSTOKEN
 $GitHubWikiUrl = 'https://github.com/TheJumpCloud/support/wiki/'
 $FilePath_ModuleBanner = $FolderPath_ModuleRootPath + '/ModuleBanner.md'
 $FilePath_ModuleChangelog = $FolderPath_ModuleRootPath + '/ModuleChangelog.md'
 # Define required files and folders variables
 $RequiredFiles = ('LICENSE', 'psm1', 'psd1')
 $RequiredFolders = ('Docs', 'Private', 'Public', 'Tests', 'en-US')
-# Get .psd1 contents
-$Psd1 = Import-PowerShellDataFile -Path:($FilePath_psd1)
 # Define folder path variables
 $FolderPath_Module = $FolderPath_ModuleRootPath + '/' + $ModuleFolderName
 $RequiredFolders | ForEach-Object {
@@ -39,6 +38,8 @@ $RequiredFiles | ForEach-Object {
     New-Variable -Name:('FileName_' + $_) -Value:($FileName) -Force;
     New-Variable -Name:('FilePath_' + $_) -Value:($FilePath) -Force;
 }
+# Get .psd1 contents
+$Psd1 = Import-PowerShellDataFile -Path:($FilePath_psd1)
 # Get module function names
 $Functions_Public = If (Test-Path -Path:($FolderPath_Public))
 {
