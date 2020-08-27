3c63e781c25830cab8fcea9704a32fff16776c3e
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-08-27 12:32:54 -0600

    [skip ci] wip parsing swagger spec

--- PowerShell/JumpCloud Module/Public/Association/Set-JcSdkAssociation.ps1
+++ PowerShell/JumpCloud Module/Public/Association/Set-JcSdkAssociation.ps1
@@ -1,148 +1,110 @@
-# # $Prefix = 'JcSdk'
-# # $AssociationKeyWords = @('Traverse') #'Association', 'Member', 'Membership',
-# # $AssociationCommands = $AssociationKeyWords | ForEach-Object {
-# #     $AssociationKeyWord = $_
-# #     Get-Command -Module:('JumpCloud.SDK.V2') -Noun:("*$AssociationKeyWord*") | Select-Object -Property:*, @{Name = 'JcType'; Expression = { $_.Noun -Replace ($AssociationKeyWord, '') -Replace ($Prefix, '') } }
-# # }
-# # $AssociationsGet = $AssociationCommands.Where( { $_.Verb -eq 'Get' })
-# # $AssociationsGet | ForEach-Object { $_.Name -replace ($_.JcType, '{0}') }
+Function Get-SwaggerItem($FilePath, $JsonPath)
+{
+    # Brake up parts of path to iterate through
+    If ($JsonPath -match '#')
+    {
+        $JsonPath = $JsonPath.Replace('#', '')
+    }
+    $JsonPathDotSource = $JsonPath.Split('/') | Where-Object { $_ }
+    # Get contents of swagger file
+    $SwaggerJson = Get-Content -Path:($FilePath) -Raw
+    $Swagger = $SwaggerJson | ConvertFrom-Json -Depth 99
+    $Definition = $Swagger
+    # Iterate through parts of the path and return the requested model
+    $JsonPathDotSource | ForEach-Object {
+        $Definition = $Definition.$_
+    }
+    Return $Definition
+}
+# Load swagger file
+$SwaggerFilePath = 'C:\Users\epanipinto\Documents\GitHub\jcapi-powershell\SwaggerSpecs\JumpCloud.SDK.V2.json'
+$OutputObject = @()
+$SwaggerJson = Get-Content -Path:($SwaggerFilePath) -Raw
+$Swagger = $SwaggerJson | ConvertFrom-Json -Depth 99
+# Parse swagger spec
+$Paths = $Swagger.paths
+$Paths.PSObject.Properties.Name | ForEach-Object {
+    $PathName = $_
+    $PathProperties = $Paths.$_
+    $PathProperties.PSObject.Properties.Name | ForEach-Object {
+        If ($_ -in ('delete', 'get', 'put', 'post', 'patch'))
+        {
+            $MethodName = $_
+            $MethodProperties = $PathProperties.$_
+            $operationId = $MethodProperties.operationId
+            $AssociationEndpoints = $MethodProperties | Where-Object { $_.operationId -like '*Association*' `
+                    -or $_.operationId -like '*Membership*' `
+                    -or $_.operationId -like '*MemberOf*' `
+                    -or $_.operationId -like '*Member*' `
+                    -or $_.operationId -like '*Traverse*' }
+            # $AssociationEndpoints = $MethodProperties | Where-Object { $_.operationId -like '*Set*Association*' }
+            If ($AssociationEndpoints)
+            {
+                If ($MethodName -eq 'get')
+                {
+                    # Extract "op" parameter
+                    $Targets = $AssociationEndpoints.parameters | Where-Object { $_.name -eq 'targets' }
+                    $OutputObject += [PSCustomObject]@{
+                        Path          = $PathName
+                        Method        = $MethodName
+                        operationId   = $operationId
+                        ParameterName = $Targets.name
+                        enum          = $Targets.items.enum
+                    }
+                }
+                ElseIf ($MethodName -eq 'post')
+                {
+                    $EndpointParameters = Get-SwaggerItem -FilePath:($SwaggerFilePath) -JsonPath:($AssociationEndpoints.parameters.schema.'$ref')
+                    # Extract "op" parameter
+                    $OutputObject += [PSCustomObject]@{
+                        Path          = $PathName
+                        Method        = $MethodName
+                        operationId   = $operationId
+                        ParameterName = $EndpointParameters.properties.PSObject.Properties.Name | Where-Object { $_ -eq 'op' }
+                        enum          = $EndpointParameters.properties.op.enum
+                    }
+                    # Extract "type" parameter
+                    $OutputObject += [PSCustomObject]@{
+                        Path          = $PathName
+                        Method        = $MethodName
+                        operationId   = $operationId
+                        ParameterName = $EndpointParameters.properties.PSObject.Properties.Name | Where-Object { $_ -eq 'type' }
+                        enum          = If ($EndpointParameters.properties.type.PSObject.Properties.Name | Where-Object { $_ -eq '$ref' })
+                        {
+                            (Get-SwaggerItem -FilePath:($SwaggerFilePath) -JsonPath:($EndpointParameters.properties.type.'$ref')).enum;
+                        }
+                        Else
+                        {
+                            $EndpointParameters.properties.type.enum;
+                        };
+                    }
+                }
+                Else
+                {
+                    Write-Error ("Unknown method: $MethodName")
+                }
+            }
+        }
+        ElseIf ($_ -in ('parameters'))
+        {
+            # Need to document?
+        }
+        Else
+        {
+            Write-Error "Unknown child path property:$($_)"
+        }
+    }
+}
 
-# # $V2Commands = Get-Command -Module:('JumpCloud.SDK.V2')
-# # $AssociationCommands = $V2Commands | Where-Object { $_.Name | Select-String -Pattern:('(^.*?JcSdk)(.*?)(Association|Membership|Member|Traverse)(.*?$)') }
-# # $AssociationCommands = $AssociationCommands | Select-Object -Property:*, @{Name = 'ParsedName'; Expression = { $_.Name | Select-String -Pattern:('(^.*?JcSdk)(.*?)(Association|Membership|Member|Traverse)(.*?$)') } }
-# # $AssociationCommands.ParsedName | ForEach-Object { # [0],[1],[26],[25]
-# #     $ParsedName = $_
-# #     If ($ParsedName.Matches.Groups[0].Value -like '*Association*')
-# #     {
-# #         [PSCustomObject]@{
-# #             Type    = $ParsedName.Matches.Groups[2].Value
-# #             Keyword = $ParsedName.Matches.Groups[3].Value
-# #         }
-# #     }
-# #     ElseIf ($ParsedName.Matches.Groups[0].Value -like '*MemberShip*')
-# #     {
-# #         [PSCustomObject]@{
-# #             Type    = $ParsedName.Matches.Groups[2].Value
-# #             Keyword = $ParsedName.Matches.Groups[3].Value
-# #         }
-# #     }
-# #     ElseIf ($ParsedName.Matches.Groups[0].Value -like '*Member*')
-# #     {
-# #         [PSCustomObject]@{
-# #             Type    = $ParsedName.Matches.Groups[2].Value
-# #             Keyword = $ParsedName.Matches.Groups[3].Value
-# #         }
-# #     }
-# #     ElseIf ($ParsedName.Matches.Groups[0].Value -like '*Traverse*')
-# #     {
-# #         [PSCustomObject]@{
-# #             Type       = $ParsedName.Matches.Groups[2].Value
-# #             Keyword    = $ParsedName.Matches.Groups[3].Value
-# #             TargetType = $ParsedName.Matches.Groups[4].Value
-# #         }
-# #     }
-# #     Else
-# #     {
-# #         Write-Error ("$ParsedName does not match any association key words.")
-# #     }
-# # }
+# Export CSV
+$AssociationCommands = $OutputObject | Select-Object -Property:*, @{Name = 'FunctionType'; Expression = { ($_.operationId | Select-String -Pattern:('(^.*?-)(.*?)(Association|Membership|Member|Traverse)(.*?$)')).Matches.Groups[2].Value } }
+$AssociationCommands | ForEach-Object { $_.enum = $_.enum -join ', ' }
+$AssociationCommands | Export-Csv -Path:('temp.csv') -Force
 
-
-# $V2Commands = Get-Command -Module:('JumpCloud.SDK.V2')
-# $AssociationCommands = $V2Commands | Where-Object { $_.Name | Select-String -Pattern:('(^.*?JcSdk)(.*?)(Association|Membership|Member|Traverse)(.*?$)') }
-# $AssociationCommands = $AssociationCommands | Select-Object -Property:*, @{Name = 'ParsedName'; Expression = { $_.Name | Select-String -Pattern:('(^.*?JcSdk)(.*?)(Association|Membership|Member|Traverse)(.*?$)') } }
-# $Traverse = $AssociationCommands.Where( { $_.Name -like '*Traverse*' })
-
-# $Type = 'User'
-# $TargetType = 'Application'
-# # Pull from "internal" module class?
-# $Body = '{"op":"' + $Action + '","type":"' + $SourceItemTypeNameSingular + '","id":"' + $SourceItemId + '","attributes":' + $AttributesValue + '}'
-# $Command = $Traverse | Where-Object { $_.ParsedName.Matches.Groups[2].Value -eq $Type -and $_.ParsedName.Matches.Groups[4].Value -eq $TargetType }
-# Write-Host ("$($Command.Name) -Body:('$($Body)')") -BackgroundColor Cyan -ForegroundColor Black
-
-
-
-# # $AssociationsGet = $AssociationCommands.Where( { $_.Verb -eq 'Get' })
-# # $AssociationsSet = $AssociationCommands.Where( { $_.Verb -eq 'Set' })
-# # $AssociationsGet | Select-Object Name, JcType | Group-Object JcType | Sort-Object Count
-# $AssociationCommands | Get-Command -Syntax
-# # $AssociationsGet | ForEach-Object { $_.Name -replace ($_.JcType, '{0}') }
-# # $Type = 'User'
-# # $Target = 'UserGroup'
-# # If ($Type -in $AssociationsSet.JcType )
-# # {
-# #     $AssociationsSetCommand = $AssociationsSet.Where( { ($_.JcType -eq $Type) })
-
-# #     # If (($Type -eq 'system' -and $Target -eq 'systemgroup') -or ($Type -eq 'user' -and $Target -eq 'usergroup'))
-# #     # {
-# #     #     $Uri_Associations_POST = $URL_Template_Associations_Members -f $TargetItemTypeNamePlural, $TargetItemId
-# #     #     $JsonBody = '{"op":"' + $Action + '","type":"' + $SourceItemTypeNameSingular + '","id":"' + $SourceItemId + '","attributes":' + $AttributesValue + '}'
-# #     # }
-# #     # ElseIf (($Type -eq 'systemgroup' -and $Target -eq 'system') -or ($Type -eq 'usergroup' -and $Target -eq 'user'))
-# #     # {
-# #     #     $Uri_Associations_POST = $URL_Template_Associations_Members -f $Type, $SourceItemId
-# #     #     $JsonBody = '{"op":"' + $Action + '","type":"' + $TargetItemTypeNameSingular + '","id":"' + $TargetItemId + '","attributes":' + $AttributesValue + '}'
-# #     # }
-# #     # Else
-# #     # {
-# #     #     $Uri_Associations_POST = $URL_Template_Associations_Targets -f $Type, $SourceItemId, $SourceItemTargetSingular
-# #     #     $JsonBody = '{"op":"' + $Action + '","type":"' + $TargetItemTypeNameSingular + '","id":"' + $TargetItemId + '","attributes":' + $AttributesValue + '}'
-# #     # }
-
-
-
-# # User, UserGroups, System, SystemGroups
-# # $URL_Template_Associations_MemberOf = 'Member'
-# # $URL_Template_Associations_Membership = 'Membership'
-
-# # $URL_Template_Associations_TargetType = 'Traverse'
-
-# # Activedirectories, Users
-# # $URL_Template_Associations_Targets = 'Association'
-
-# # Only for POST
-# # $URL_Template_Associations_Members = ''
-
-# # ('Association', , '', 'Traverse')
-# #     # # All the bindings, recursive , both direct and indirect
-# #     # $URL_Template_Associations_MemberOf = '/api/v2/{0}/{1}/memberof' # $SourcePlural, $SourceId
-# #     # $URL_Template_Associations_Membership = '/api/v2/{0}/{1}/membership' # $SourcePlural (systemgroups,usergroups), $SourceId
-# #     # $URL_Template_Associations_TargetType = '/api/v2/{0}/{1}/{2}' # $SourcePlural, $SourceId, $TargetPlural
-# #     # # Only direct bindings and don’t traverse through groups
-# #     # $URL_Template_Associations_Targets = '/api/v2/{0}/{1}/associations?targets={2}' # $SourcePlural, $SourceId, $TargetSingular
-# #     # $URL_Template_Associations_Members = '/api/v2/{0}/{1}/members' # $SourcePlural, $SourceId
-
-# #     # Build Url based upon source and target combinations
-# #     If (($Type -eq 'system' -and $Target -eq 'systemgroup') -or ($Type -eq 'user' -and $Target -eq 'usergroup'))
-# #     {
-# #         $URL_Template_Associations_MemberOf
-# #     }
-# #     ElseIf (($Type -eq 'systemgroup' -and $Target -eq 'system') -or ($Type -eq 'usergroup' -and $Target -eq 'user'))
-# #     {
-# #         $URL_Template_Associations_Membership
-# #     }
-# #     # ElseIf (($Type -eq 'activedirectory' -and $Target -eq 'user') -or ($Type -eq 'user' -and $Target -eq 'activedirectory'))
-# #     # {
-# #     #     $URL_Template_Associations_Targets
-# #     # }
-# #     Else
-# #     {
-# #         $URL_Template_Associations_TargetType
-# #     }
-
-
-
-
-
-
-
-
-# #     Write-Host ($Type)
-# #     # $CommandTemplate = "JumpCloud.SDK.V2\Get-JcSdkSystemInsight{0} @PSBoundParameters"
-# #     # Invoke-Expression -Command:($CommandTemplate -f $Table)
-
-# # }
-# # Else
-# # {
-# #     Write-Error ("$Type is not in $($AssociationsSet.JcType -join ', ')")
-# # }
+# # Build sample commands
+# $AssociationCommands | Where-Object { $_.Method -eq 'get' -and $_.ParameterName -eq 'targets' } | ForEach-Object {
+#     $Command = $_
+#     $_.enum | ForEach-Object {
+#         "Get-JCAssociation -Type:('$($Command.FunctionType)') -Id:('') -TargetType:('$($_)');"
+#     }
+# } | Sort-Object
