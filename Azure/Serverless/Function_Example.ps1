4468526a1c8eff69f8c635e0f30f62600d362056
Brad Stevens <35233498+btstevens@users.noreply.github.com>
2020-09-30 00:44:30 -0600

    Update Function_Example.ps1

--- Azure/Serverless/Function_Example.ps1
+++ Azure/Serverless/Function_Example.ps1
@@ -3,18 +3,21 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
 # Interact with query parameters or the body of the request.
-$command = $Request.Query.$command
+$command = $Request.Query.Name
 if (-not $command) {
     $command = $Request.Body.Command
 }
 
 connect-jconline -jumpcloudapikey $env:JumpCloudApiKey -force
 $result = Invoke-Expression $command
-$body = "This HTTP triggered function executed successfully. Pass a Powershell command in the query string or in the response header body."
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
 if ($command) {
-    $body = $result
+$body = $result
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
