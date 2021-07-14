456e4f439e3483b83a6cb4aa53d74ab1eccfed48
Joe Workman <joe.workman@jumpcloud.com>
2021-07-14 13:12:36 -0600

    code artifact repository

--- PowerShell/Deploy/Setup-Dependencies.ps1
+++ PowerShell/Deploy/Setup-Dependencies.ps1
@@ -1,5 +1,5 @@
 Param(
-    [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 0)][System.String[]]$DependentModules = ('PowerShellGet', 'PackageManagement', 'PSScriptAnalyzer', 'PlatyPS', 'Pester')
+    [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 0)][System.String[]]$DependentModules = ('PowerShellGet', 'PackageManagement', 'PSScriptAnalyzer', 'PlatyPS', 'Pester', 'AWS.Tools.Common', 'AWS.Tools.CodeArtifact')
     , [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 1)][System.String]$RequiredModulesRepo = 'PSGallery'
 )
 # Install NuGet
@@ -27,60 +27,65 @@ ForEach ($DependentModule In $DependentModules)
 ### TODO: Switch to CodeArtifact
 If ($RequiredModulesRepo -ne 'PSGallery')
 {
-    # Set default -Repository parameter value to be $RequiredModulesRepo
-    #Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'Repository' | ForEach-Object {
-    #    If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):Repository" -and $_.Value -eq $RequiredModulesRepo })
-    #    {
-    #        $global:PSDefaultParameterValues["$($_.Name):Repository"] = $RequiredModulesRepo
-    #    }
-    #}
+    $env:SYSTEM_ACCESSTOKEN = Get-CAAuthorizationToken -Domain jumpcloud-artifacts -Region us-east-1    # Set default -Repository parameter value to be $RequiredModulesRepo
+    Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'Repository' | ForEach-Object {
+       If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):Repository" -and $_.Value -eq $RequiredModulesRepo })
+       {
+           $global:PSDefaultParameterValues["$($_.Name):Repository"] = $RequiredModulesRepo
+       }
+    }
     # Set default -AllowPrerelease parameter value to be $True
-    #Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'AllowPrerelease' | ForEach-Object {
-    #    If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):AllowPrerelease" -and $_.Value -eq $true })
-    #    {
-    #        $global:PSDefaultParameterValues["$($_.Name):AllowPrerelease"] = $true
-    #    }
-    #}
-    #
-    #If (-not [System.String]::IsNullOrEmpty($env:SYSTEM_ACCESSTOKEN))
-    #{
-    #    $global:RepositoryCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:SYSTEM_ACCESSTOKEN, ($env:SYSTEM_ACCESSTOKEN | ConvertTo-SecureString -AsPlainText -Force)
-    #    # Set default -Credential parameter value to be $RepositoryCredentials
-    #    Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'Credential' | ForEach-Object {
-    #        If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):Credential" -and $_.Value -eq $RepositoryCredentials })
-    #        {
-    #            $global:PSDefaultParameterValues["$($_.Name):Credential"] = $RepositoryCredentials
-    #        }
-    #    }
-    #}
-    #Else
-    #{
-    #    Write-Warning ('No SYSTEM_ACCESSTOKEN has been provided')
-    #}
+    Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'AllowPrerelease' | ForEach-Object {
+       If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):AllowPrerelease" -and $_.Value -eq $true })
+       {
+           $global:PSDefaultParameterValues["$($_.Name):AllowPrerelease"] = $true
+       }
+    }
+    If (-not [System.String]::IsNullOrEmpty($env:SYSTEM_ACCESSTOKEN))
+    {
+       $global:RepositoryCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:SYSTEM_ACCESSTOKEN, ($env:SYSTEM_ACCESSTOKEN | ConvertTo-SecureString -AsPlainText -Force)
+       # Set default -Credential parameter value to be $RepositoryCredentials
+       Get-Command -Module:('PowerShellGet', 'PackageManagement') -ParameterName 'Credential' | ForEach-Object {
+           If ( -not $global:PSDefaultParameterValues.GetEnumerator() | Where-Object { $_.Key -eq "$($_.Name):Credential" -and $_.Value -eq $RepositoryCredentials })
+           {
+               $global:PSDefaultParameterValues["$($_.Name):Credential"] = $RepositoryCredentials
+           }
+       }
+    }
+    Else
+    {
+       Write-Warning ('No SYSTEM_ACCESSTOKEN has been provided')
+    }
     # Register PSRepository
-    #If (-not (Get-PackageSource -Name:($RequiredModulesRepo) -ErrorAction SilentlyContinue))
-    #{
-    #    Write-Host("[status]Register-PackageSource Setup '$RequiredModulesRepo'")
+    If (-not (Get-PackageSource -Name:($RequiredModulesRepo) -ErrorAction SilentlyContinue))
+    {
+       Write-Host("[status]Register-PackageSource Setup '$RequiredModulesRepo'")
     #    Register-PackageSource -Trusted -ProviderName:("PowerShellGet") -Name:($RequiredModulesRepo) -Credential:($RepositoryCredentials) -Location:("https://pkgs.dev.azure.com/$(($RequiredModulesRepo.Split('-'))[0])/_packaging/$($(($RequiredModulesRepo.Split('-'))[1]))/nuget/v2/")
-    #}
+        Register-PSResourceRepository -Name CodeArtifact -URL "https://jumpcloud-artifacts-868503801984.d.codeartifact.us-east-1.amazonaws.com/nuget/jumpcloud-nuget-modules/v3/index.json" -Trusted 
+    }
 }
 If (-not [System.String]::IsNullOrEmpty($Psd1))
 {
     # Install required modules
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     ForEach ($RequiredModule In $Psd1.RequiredModules)
     {
         # Check to see if the module is installed
         If ([System.String]::IsNullOrEmpty((Get-InstalledModule | Where-Object { $_.Name -eq $RequiredModule })))
         {
             Write-Host("[status]Installing module: '$RequiredModule' from '$RequiredModulesRepo'")
-            Install-Module -Force -Name:($RequiredModule) -Scope:('CurrentUser') #-Repository:('PSGallery') -Credential:($RepositoryCredentials) -AllowPrerelease
-        }
-        # Get-Module -Refresh -ListAvailable
-        If ([System.String]::IsNullOrEmpty((Get-Module | Where-Object { $_.Name -eq $RequiredModule })))
-        {
-            Write-Host("[status]Importing module: '$RequiredModule'")
-            Import-Module -Name:($RequiredModule) -Force -Global
+            If ($RequiredModulesRepo -ne 'PSGallery'){
+                install-PSResource -Name:($RequiredModule) -Repository:($RequiredModulesRepo) -Credential $RepositoryCredentials -Prerelease -Scope:('CurrentUser')
+            }
+            else{
+                Install-Module -Force -Name:($RequiredModule) -Scope:('CurrentUser') -Repository:($RequiredModulesRepo) -Credential:($RepositoryCredentials) -AllowPrerelease
+            }
+            # Get-Module -Refresh -ListAvailable
+            If ([System.String]::IsNullOrEmpty((Get-Module | Where-Object { $_.Name -eq $RequiredModule })))
+            {
+                Write-Host("[status]Importing module: '$RequiredModule'")
+                Import-Module -Name:($RequiredModule) -Force -Global
+            }
         }
     }
     # Load current module
