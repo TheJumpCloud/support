52498c7d402a7abed3e1ed54b4b2cccc1d1dde91
TheJumpCloud <TheJumpCloud>
2021-07-21 16:31:51 +0000

    Updating PowerShell Module;[skip ci]

--- PowerShell/JumpCloud Module/Public/DirectoryInsights/Get-JCEvent.ps1
+++ PowerShell/JumpCloud Module/Public/DirectoryInsights/Get-JCEvent.ps1
@@ -63,78 +63,78 @@ https://github.com/TheJumpCloud/jcapi-powershell/tree/master/SDKs/PowerShell/Jum
 #>
 Function Get-JCEvent
 {
-  [OutputType([JumpCloud.SDK.DirectoryInsights.Models.IPost200ApplicationJsonItemsItem], [System.String])]
-  [CmdletBinding(DefaultParameterSetName = 'GetExpanded', PositionalBinding = $false, SupportsShouldProcess, ConfirmImpact = 'Medium')]
-  Param(
-    [Parameter(ParameterSetName = 'GetExpanded', Mandatory)]
+    [OutputType([JumpCloud.SDK.DirectoryInsights.Models.IPost200ApplicationJsonItemsItem], [System.String])]
+    [CmdletBinding(DefaultParameterSetName='GetExpanded', PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+    [Parameter(ParameterSetName='GetExpanded', Mandatory)]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.String[]]
     # service name to query.
     # Known services: systems,radius,sso,directory,ldap,all
     ${Service},
 
-    [Parameter(ParameterSetName = 'GetExpanded', Mandatory)]
+    [Parameter(ParameterSetName='GetExpanded', Mandatory)]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.DateTime]
     # query start time, UTC in RFC3339 format
     ${StartTime},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.DateTime]
     # optional query end time, UTC in RFC3339 format
     ${EndTime},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.String[]]
     # optional list of fields to return from query
     ${Fields},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.String[]]
     # Specific query to search after, see x-* response headers for next values
     ${SearchAfter},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
-    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes = ([JumpCloud.SDK.DirectoryInsights.Models.ITermConjunction]))]
+    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes=([JumpCloud.SDK.DirectoryInsights.Models.ITermConjunction]))]
     [System.Collections.Hashtable]
     # TermConjunction represents a conjunction (and/or)NOTE: the validator limits what the operator can be, not the objectfor future-proof-nessand a list of sub-values
     ${SearchTermAnd},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
-    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes = ([JumpCloud.SDK.DirectoryInsights.Models.ITermConjunction]))]
+    [JumpCloud.SDK.DirectoryInsights.Runtime.Info(PossibleTypes=([JumpCloud.SDK.DirectoryInsights.Models.ITermConjunction]))]
     [System.Collections.Hashtable]
     # TermConjunction represents a conjunction (and/or)NOTE: the validator limits what the operator can be, not the objectfor future-proof-nessand a list of sub-values
     ${SearchTermOr},
 
-    [Parameter(ParameterSetName = 'GetExpanded')]
+    [Parameter(ParameterSetName='GetExpanded')]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [System.String]
     # ASC or DESC order for timestamp
     ${Sort},
 
-    [Parameter(ParameterSetName = 'Get', Mandatory, ValueFromPipeline)]
+    [Parameter(ParameterSetName='Get', Mandatory, ValueFromPipeline)]
     [JumpCloud.SDK.DirectoryInsights.Category('Body')]
     [JumpCloud.SDK.DirectoryInsights.Models.IEventQuery]
     # EventQuery is the users' command to search our auth logs
     # To construct, see NOTES section for BODY properties and create a hash table.
     ${Body}
-  )
-  Begin
-  {
-    Connect-JCOnline -force | Out-Null
-    $Results = @()
-  }
-  Process
-  {
-    $Results = JumpCloud.SDK.DirectoryInsights\Get-JcSdkEvent @PSBoundParameters
-  }
-  End
-  {
-    Return $Results
-  }
+    )
+    Begin
+    {
+        Connect-JCOnline -force | Out-Null
+        $Results = @()
+    }
+    Process
+    {
+        $Results = JumpCloud.SDK.DirectoryInsights\Get-JcSdkEvent @PSBoundParameters
+    }
+    End
+    {
+        Return $Results
+    }
 }
