f5e76c42fc5e1ab3ae245fde20885cd8b87ed5a4
Joe Workman <joe.workman@jumpcloud.com>
2021-12-01 11:37:40 -0700

    cleanup

--- PowerShell/Deploy/Tests/PSScriptAnalyzer.Tests.ps1
+++ PowerShell/Deploy/Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,23 +1,7 @@
 . ((Get-Item -Path:($PSScriptRoot)).Parent.FullName + '/' + 'Get-Config.ps1')
 ###########################################################################
-# # Describe "PSScriptAnalyzer Tests" {
-# #     BeforeAll {
-# #         # $DebugPreference = 'Continue'
-# #         # $VerbosePreference = 'Continue'
-# #         $ErrorActionPreference = 'Stop'
-# #     }
-# #     AfterAll {
-# #         # $DebugPreference = 'SilentlyContinue'
-# #         # $VerbosePreference = 'SilentlyContinue'
-# #         $ErrorActionPreference = 'Continue'
-# #     }
-# #     Context 'PSScriptAnalyzer Tests' {
-# #         It 'Passes Invoke-PSScriptAnalyzer' {
 Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
 $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse
-# #     $ScriptAnalyzerResults | Should BeNullOrEmpty
-# #     $? | Should Be $true
-# # }
 If ($ScriptAnalyzerResults)
 {
     $ScriptAnalyzerResults
@@ -27,5 +11,3 @@ Else
 {
     Write-Host ('[success]ScriptAnalyzer returned no results')
 }
-# #     }
-# # }
--- PowerShell/Deploy/Tests/PSScriptInfo.Tests.ps1
+++ PowerShell/Deploy/Tests/PSScriptInfo.Tests.ps1
@@ -1,22 +1,6 @@
 . ((Get-Item -Path:($PSScriptRoot)).Parent.FullName + '/' + 'Get-Config.ps1')
 ###########################################################################
-# # Describe "ScriptInfo Tests" {
-# #     BeforeAll {
-# #         # $DebugPreference = 'Continue'
-# #         # $VerbosePreference = 'Continue'
-# #         $ErrorActionPreference = 'Stop'
-# #     }
-# #     AfterAll {
-# #         # $DebugPreference = 'SilentlyContinue'
-# #         # $VerbosePreference = 'SilentlyContinue'
-# #         $ErrorActionPreference = 'Continue'
-# #     }
-# #     Context 'ScriptInfo Tests' {
-# #         It 'Passes Test-ScriptFileInfo' {
 $ScriptFileInfoResults = Get-ChildItem -Path:($FolderPath_Public) -Recurse -File | ForEach-Object { Test-ScriptFileInfo -Path:($_.FullName) -ErrorAction:('Ignore') }
-# #     $ScriptFileInfoResults | Should BeNullOrEmpty
-# #     $? | Should Be $true
-# # }
 If ($ScriptFileInfoResults)
 {
     $ScriptFileInfoResults
@@ -26,43 +10,3 @@ Else
 {
     Write-Host ('[success]ScriptFileInfo returned no results')
 }
-# #     }
-# # }
-
-# Test-ScriptFileInfo -Path:('')
-
-# New-ScriptFileInfo -Path:('') `
-#     -Description:('') `
-#     -Version:('') `
-#     -Guid:('') `
-#     -Author:('') `
-#     -CompanyName:('') `
-#     -Copyright:('') `
-#     -RequiredModules:('') `
-#     -ExternalModuleDependencies:('') `
-#     -RequiredScripts:('') `
-#     -ExternalScriptDependencies:('') `
-#     -Tags:('') `
-#     -ProjectUri:('') `
-#     -LicenseUri:('') `
-#     -IconUri:('') `
-#     -ReleaseNotes:('') `
-#     -PrivateData:('')
-
-# Update-ScriptFileInfo  -Path:('') `
-#     -Description:('') `
-#     -Version:('') `
-#     -Guid:('') `
-#     -Author:('') `
-#     -CompanyName:('') `
-#     -Copyright:('') `
-#     -RequiredModules:('') `
-#     -ExternalModuleDependencies:('') `
-#     -RequiredScripts:('') `
-#     -ExternalScriptDependencies:('') `
-#     -Tags:('') `
-#     -ProjectUri:('') `
-#     -LicenseUri:('') `
-#     -IconUri:('') `
-#     -ReleaseNotes:('') `
-#     -PrivateData:('')
