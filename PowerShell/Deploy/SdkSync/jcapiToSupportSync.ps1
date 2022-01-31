27b672f10c4720d354a343a5f0db6806bdeff538
Joe Workman <joe.workman@jumpcloud.com>
2022-01-31 11:42:36 -0700

    jcmodule + sdk generation

--- PowerShell/Deploy/SdkSync/jcapiToSupportSync.ps1
+++ PowerShell/Deploy/SdkSync/jcapiToSupportSync.ps1
@@ -75,7 +75,7 @@ $MSCopyrightHeader = "`n<#`n.Synopsis`n"
 $Divider = "`n[SPLIT]`n<#`n.Synopsis`n"
 $FunctionTemplate = "{0}`nFunction {1}`n{{`n$($IndentChar){2}`n$($IndentChar)Param(`n{3}`n$($IndentChar))`n$($IndentChar)Begin`n$($IndentChar){{`n{4}`n$($IndentChar)}}`n$($IndentChar)Process`n$($IndentChar){{`n{5}`n$($IndentChar)}}`n$($IndentChar)End`n$($IndentChar){{`n{6}`n$($IndentChar)}}`n}}"
 $ScriptAnalyzerResults = @()
-$JumpCloudModulePath = "$PSScriptRoot/../../JumpCloud Module"
+$JumpCloudModulePath = "$PSScriptRoot/../JumpCloud Module"
 Get-Module -Refresh -ListAvailable -All | Out-Null
 $Modules = Get-Module -Name:($Psd1.RequiredModules | Where-Object { $_ -in $ApprovedFunctions.Keys })
 If (-not [System.String]::IsNullOrEmpty($Modules))
