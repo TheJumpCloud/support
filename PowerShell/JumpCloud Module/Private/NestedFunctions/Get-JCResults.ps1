f7244e7be0485feb7855e013a087d8dac30e0f90
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-05-20 13:52:01 -0500

    parallel implementation in private function

--- PowerShell/JumpCloud Module/Private/NestedFunctions/Get-JCResults.ps1
+++ PowerShell/JumpCloud Module/Private/NestedFunctions/Get-JCResults.ps1
@@ -5,7 +5,8 @@ Function Get-JCResults
         [Parameter(Mandatory = $true, HelpMessage = 'URL of Endpoint')][ValidateNotNullOrEmpty()]$URL,
         [Parameter(Mandatory = $true, HelpMessage = 'Method of WebRequest')][ValidateNotNullOrEmpty()]$method,
         [Parameter(Mandatory = $true, HelpMessage = 'Limit of WebRequest')][ValidateNotNullOrEmpty()]$limit,
-        [Parameter(Mandatory = $false, HelpMessage = 'Body of WebRequest, if required')]$body
+        [Parameter(Mandatory = $false, HelpMessage = 'Body of WebRequest, if required')]$body,
+        [Parameter(Mandatory = $false, HelpMessage = 'Boolean: True to run in parallel, False to run in sequential')][bool]$parallel = $false
     )
     begin {
         $hdrs = @{
@@ -17,7 +18,14 @@ Function Get-JCResults
         {
             $hdrs.Add('x-org-id', "$($JCOrgID)")
         }
-        $resultsArray = @()
+        if (($PSVersionTable.PSVersion.Major -ge 7) -and ($parallel -eq $true)) {
+            Write-Debug "Parallel set to True, PSVersion greater than 7"
+            $resultsArray = [System.Collections.Concurrent.ConcurrentBag[object]]::new()
+        }
+        else {
+            Write-Debug "Running in Sequential"
+            $resultsArray = @()
+        }
         $totalCount = 1
         $limit = [int]$limit
         $skip = 0
@@ -31,19 +39,43 @@ Function Get-JCResults
         Write-Debug "total count: $totalCount"
         $passCounter = [math]::ceiling($totalCount/$limit)
         Write-Debug "number of passes: $passCounter"
-        $resultsArray += $response.Content | ConvertFrom-Json
-        
-        for($i = 1; $i -lt $passCounter; $i++) {
-            $skip += $limit
-            $limitURL = $URL + "?limit=$limit&skip=$skip"
-            if ($body){
-                $response = Invoke-WebRequest -Method $method -Body $body -Uri $limitURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
+
+        # Running Function in Parallel
+        if (($PSVersionTable.PSVersion.Major -ge 7) -and ($parallel -eq $true)) {
+            $content = $response.Content
+            $resultsArray.Add($content)
+            if ($passCounter -gt 1) {
+                1..$passCounter | ForEach-Object -Parallel {
+                    $resultsArray = $using:resultsArray
+                    $skip = $_ * $using:limit
+                    $limitURL = $using:URL + "?limit=$using:limit&skip=$skip"
+                    if ($using:body){
+                        $response = Invoke-WebRequest -Method $using:method -Body $using:body -Uri $limitURL -Headers $using:hdrs -UserAgent:(Get-JCUserAgent)
+                    }
+                    else {
+                        $response = Invoke-WebRequest -Method $using:method -Uri $limitURL -Headers $using:hdrs -UserAgent:(Get-JCUserAgent)
+                    }
+                    $content = $response.Content
+                    $resultsArray.Add($content)
+                }
             }
-            else {
-                $response = Invoke-WebRequest -Method $method -Uri $limitURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
-            }
+            $resultsArray = $resultsArray | ConvertFrom-Json
+        }
+        # Running Function in Sequential
+        else {
             $resultsArray += $response.Content | ConvertFrom-Json
-            Write-Debug ("Pass: $i Amount: " + ($response.Content | ConvertFrom-Json).Count)
+            for($i = 1; $i -lt $passCounter; $i++) {
+                $skip += $limit
+                $limitURL = $URL + "?limit=$limit&skip=$skip"
+                if ($body){
+                    $response = Invoke-WebRequest -Method $method -Body $body -Uri $limitURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
+                }
+                else {
+                    $response = Invoke-WebRequest -Method $method -Uri $limitURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
+                }
+                $resultsArray += $response.Content | ConvertFrom-Json
+                Write-Debug ("Pass: $i Amount: " + ($response.Content | ConvertFrom-Json).Count)
+            }
         }
     }
     end {
