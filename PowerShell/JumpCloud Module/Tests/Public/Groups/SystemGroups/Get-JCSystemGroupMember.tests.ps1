4a60adbb32fae65d638ec6b207febc915f10b464
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-05-27 15:48:49 -0500

    usergroup/systemgroup parallel tests

--- PowerShell/JumpCloud Module/Tests/Public/Groups/SystemGroups/Get-JCSystemGroupMember.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/SystemGroups/Get-JCSystemGroupMember.tests.ps1
@@ -19,3 +19,24 @@ Describe -Tag:('JCSystemGroupMember') 'Get-JCSystemGroupMember 1.0' {
     }
 
 }
+Describe -Tag:('JCSystemGroupMember') 'Get-JCSystemGroupMember Parallel' {
+    BeforeAll { Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null }
+    It 'Gets a System Groups membership by Groupname' {
+
+        $MultiSystemGroupAdd = Get-JCSystem | Select-Object -Last 2 | Add-JCSystemGroupMember -GroupName $PesterParams_SystemGroup.Name
+
+        $SystemGroupMembers = Get-JCSystemGroupMember -GroupName $PesterParams_SystemGroup.Name -parallel $true
+        $SystemGroupMembers.SystemID.Count | Should -BeGreaterThan 0
+    }
+
+    It 'Gets a System Groups membership -ByID' {
+        $SystemGroupMembers = Get-JCSystemGroupMember -ByID $PesterParams_SystemGroup.Id -parallel $true
+        $SystemGroupMembers.SystemID.Count | Should -BeGreaterThan 0
+    }
+
+    It 'Gets all System Group members using Get-JCGroup -type system and the pipeline' {
+        $AllSystemGroupmembers = Get-JCGroup -Type System | Get-JCSystemGroupMember -parallel $true
+        $AllSystemGroupmembers.GroupName.Count | Should -BeGreaterThan 1
+    }
+
+}
--- PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/UserGroups/Get-JCUserGroupMember.tests.ps1
@@ -15,3 +15,20 @@ Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember 1.0' {
         $UserGroupMembers.UserID.Count | Should -BeGreaterThan 0
     }
 }
+Describe -Tag:('JCUserGroupMember') 'Get-JCUserGroupMember Parallel' {
+    BeforeAll {
+        Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
+        If (-not (Get-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name)))
+        {
+            Add-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name) -Username:($PesterParams_User1.username)
+        }
+    }
+    It 'Gets a User Groups membership by Groupname' {
+        $UserGroupMembers = Get-JCUserGroupMember -GroupName:($PesterParams_UserGroup.Name) -parallel $true
+        $UserGroupMembers.UserID.Count | Should -BeGreaterThan 0
+    }
+    It 'Gets a User Groups membership -ByID' {
+        $UserGroupMembers = Get-JCUserGroupMember -ByID $PesterParams_UserGroup.Id -parallel $true
+        $UserGroupMembers.UserID.Count | Should -BeGreaterThan 0
+    }
+}
