61ee21a26c3c4ce29dbb5f1a8ed9d4cff891de65
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-06-07 15:36:07 -0500

    parallel tests

---
+++ PowerShell/JumpCloud Module/Tests/Private/NestedFunctions/Get-JCResults.Tests.ps1
@@ -0,0 +1,111 @@
+Describe -Tag:('Parallel') "Get-JCResults Parallel" {
+    It "Returns all users in parallel" {
+        
+        $URL = "{0}/api/search/systemusers" -f $JCUrlBasePath
+        $Search = @{
+            filter = @(
+                @{}
+            )
+            fields = ""
+            limit = 1
+            skip = 0
+        }
+        $SearchJSON = $Search | ConvertTo-Json -Compress -Depth 4
+
+        $ParallelUsers = Get-JCResults -Url $URL -method "POST" -body $SearchJSON -limit 1 -parallel $true
+        $SerialUsers = Get-JCUser
+        
+        $ParallelUsers.Count | Should -Be $SerialUsers.Count
+
+        $SortedParallelUsers = $ParallelUsers.username | Sort-Object
+        $SortedSerialUsers = $SerialUsers.username | Sort-Object
+
+        $SortedParallelUsers | Should -Be $SortedSerialUsers
+
+    }
+
+    It "Returns all systems in parallel" {
+        
+        $URL = "$JCUrlBasePath/api/search/systems"
+        $Search = @{
+            filter = @(
+                @{}
+            )
+            fields = ""
+            limit = 1
+            skip = 0
+        }
+        $SearchJSON = $Search | ConvertTo-Json -Compress -Depth 4
+
+        $ParallelSystems = Get-JCResults -Url $URL -method "POST" -body $SearchJSON -limit 1 -parallel $true
+        $SerialSystems = Get-JCSystem
+        
+        $ParallelSystems.Count | Should -Be $SerialSystems.Count
+
+        $SortedParallelSystems = $ParallelSystems.displayName | Sort-Object
+        $SortedSerialSystems = $SerialSystems.displayName | Sort-Object
+
+        $SortedParallelSystems | Should -Be $SortedSerialSystems
+
+    }
+
+    It "Returns all SystemGroupMembers in parallel" {
+        $limitURL = "{0}/api/v2/Systemgroups/{1}/members" -f $JCUrlBasePath, $PesterParams_SystemGroup.Id
+        $rawResults = Get-JCResults -Url $limitURL -method "GET" -limit 1 -parallel $true
+
+        $ParallelSystemGroupMembers = [PSCustomObject]@{}
+        foreach ($uid in $rawResults)
+            {
+                $System = Get-JCSystem -id:($uid.to.id)
+
+                $FomattedResult = [pscustomobject]@{
+
+                    'GroupName' = $PesterParams_SystemGroup.name
+                    'System'    = $System.displayName
+                    'SystemID'  = $uid.to.id
+                }
+
+                $ParallelSystemGroupMembers.Add($FomattedResult)
+            }
+
+
+        $SerialSystemGroupMembers = Get-JCSystemGroupMember -ByID $PesterParams_SystemGroup.Id
+
+        $ParallelSystemGroupMembers.Count | Should -Be $SerialSystemGroupMembers.Count
+
+        $SortedParallelMembers = $ParallelSystemGroupMembers | Sort-Object -Property SystemID
+        $SortedSerialMembers = $SerialSystemGroupMembers | Sort-Object -Property SystemID
+
+        $SortedParallelMembers | Should -Be $SortedSerialMembers
+    }
+
+    It "Returns all SystemGroupMembers in parallel" {
+        $limitURL = "{0}/api/v2/Usergroups/{1}/members" -f $JCUrlBasePath, $PesterParams_UserGroup.Id
+        $rawResults = Get-JCResults -Url $limitURL -method "GET" -limit 1 -parallel $true
+
+        $ParallelUserGroupMembers = [PSCustomObject]@{}
+        foreach ($uid in $rawResults)
+            {
+                $User = Get-JCUser -id:($uid.to.id)
+
+                $FomattedResult = [pscustomobject]@{
+
+                    'GroupName' = $PesterParams_UserGroup.name
+                    'Username'    = $User.username
+                    'UserID'  = $uid.to.id
+                }
+
+                $ParallelUserGroupMembers.Add($FomattedResult)
+            }
+
+
+        $SerialUserGroupMembers = Get-JCUserGroupMember -ByID $PesterParams_UserGroup.Id
+
+        $ParallelUserGroupMembers.Count | Should -Be $SerialUserGroupMembers.Count
+
+        $SortedParallelMembers = $ParallelUserGroupMembers | Sort-Object -Property Username
+        $SortedSerialMembers = $SerialUserGroupMembers | Sort-Object -Property Username
+
+        $SortedParallelMembers | Should -Be $SortedSerialMembers
+    }
+}
--- PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Get-JCSystemUser.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Get-JCSystemUser.tests.ps1
@@ -5,11 +5,4 @@ Describe -Tag:('JCSystemUser') 'Get-JCSystemUser 1.0' {
         $SystemUsers = Get-JCSystemUser -SystemID  $PesterParams_SystemLinux._id
         $SystemUsers.username.Count | Should -BeGreaterThan 1
     }
-
-    It "Gets JumpCloud system users for a system using SystemID in parallel" {
-
-        $SystemUsers = Get-JCSystemUser -SystemID  $PesterParams_SystemLinux._id -Parallel $true
-        $SystemUsers.username.Count | Should -BeGreaterThan 1
-    }
-
 }
