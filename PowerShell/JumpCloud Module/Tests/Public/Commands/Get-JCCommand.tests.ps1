5d0e17f0fe3ef5afe1fcf1c33df72e9ee5298d9a
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-06-20 16:59:09 -0500

    command search tests

--- PowerShell/JumpCloud Module/Tests/Public/Commands/Get-JCCommand.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Commands/Get-JCCommand.tests.ps1
@@ -39,6 +39,47 @@ Describe -Tag:('JCCommand') 'Get-JCCommand 1.0' {
         $Triggers = Get-JCCommand | Where-Object trigger -ne ''
         $Triggers._id.Count | Should -BeGreaterThan 1
     }
+}
 
-
+Describe -Tag('JCCommand') 'Get-JCCommand Search' {
+    BeforeAll { 
+        Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
+        $PesterParams_Command1 = Get-JCCommand -CommandID:($PesterParams_Command1.Id) 
+    }
+    It "Searches a JumpCloud command by name" {
+        $Command = Get-JCCommand -name $PesterParams_Command1.name
+        $Command.name | Should -Be $PesterParams_Command1.name
+    }
+    It "Searches a JumpCloud command by command" {
+        $Command = Get-JCCommand -command $PesterParams_Command1.command
+        $Command.command | Should -Be $PesterParams_Command1.command
+    }
+    It "Searches a JumpCloud command by commandType" {
+        $Command = Get-JCCommand -commandType $PesterParams_Command1.commandType
+        $Command.commandType | Should -Be $PesterParams_Command1.commandType
+    }
+    It "Searches a JumpCloud command by launchType" {
+        $Command = Get-JCCommand -launchType $PesterParams_Command1.launchType
+        $Command.launchType | Should -Be $PesterParams_Command1.launchType
+    }
+    It "Searches a JumpCloud command by listensTo" {
+        $Command = Get-JCCommand -listensTo $PesterParams_Command1.listensTo
+        $Command.listensTo | Should -Be $PesterParams_Command1.listensTo
+    }
+    It "Searches a JumpCloud command by schedule" {
+        $Command = Get-JCCommand -schedule $PesterParams_Command1.schedule
+        $Command.schedule | Should -Be $PesterParams_Command1.schedule
+    }
+    It "Searches a JumpCloud command by trigger" {
+        $Command = Get-JCCommand -trigger $PesterParams_Command1.trigger
+        $Command.trigger | Should -Be $PesterParams_Command1.trigger
+    }
+    It "Searches a JumpCloud command by scheduleRepeatType" {
+        $Command = Get-JCCommand -scheduleRepeatType $PesterParams_Command1.scheduleRepeatType
+        $Command.scheduleRepeatType | Should -Be $PesterParams_Command1.scheduleRepeatType
+    }
+    It "Searches a JumpCloud command by organization" {
+        $Command = Get-JCCommand -organization $PesterParams_Command1.organization
+        $Command.organization | Should -Be $PesterParams_Command1.organization
+    }
 }
