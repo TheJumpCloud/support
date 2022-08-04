4eed5cfaf28bba48398b6757a91e4412f4bc70b7
Joe Workman <joe.workman@jumpcloud.com>
2022-08-04 15:32:36 -0600

    test for null values in settings file

--- PowerShell/JumpCloud Module/Tests/Private/settings/New-JCSettingsFile.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Private/settings/New-JCSettingsFile.Tests.ps1
@@ -11,4 +11,16 @@ Describe -Tag "JCSettingsFile" -Name "New JCSettings Tests" {
         # Test that new config file is actually modified
         $lastModifiedDate | Should -BeGreaterThan $previousModifiedDate
     }
+    it "settings file should contain properties with values, copy and write settings" {
+        $newConfig = Get-JCSettingsFile -Raw
+        foreach ($parameter in $newConfig.psobject.Properties){
+            foreach ($subParameter in $parameter.Value.psobject.Properties) {
+                # Write-host "$($parameter.Name).$($subParameter.name)"
+                # Each subProperty should contain a value, copy and write property
+                $newConfig.$($parameter.Name).$($subParameter.name).value | Should -Not -BeNullOrEmpty
+                $newConfig.$($parameter.Name).$($subParameter.name).copy | Should -Not -BeNullOrEmpty
+                $newConfig.$($parameter.Name).$($subParameter.name).write | Should -Not -BeNullOrEmpty
+            }
+        }
+    }
 }
