2b3ba780a833a80ac0fc87aec501bfff9c3af789
Joe Workman <joe.workman@jumpcloud.com>
2022-08-12 09:27:30 -0600

    import priv functions in update jcmodule & revert settings

--- PowerShell/JumpCloud Module/Public/Authentication/Update-JCModule.ps1
+++ PowerShell/JumpCloud Module/Public/Authentication/Update-JCModule.ps1
@@ -358,31 +358,21 @@ Function Update-JCModule {
                         # Copy saved settings to new config.json
                         if (-Not ($savedJCSettings)::IsNullOrEmpty) {
                             # If not null or empty
-                            # Get module config for new module version
+                            # Get Private functions:
                             $ModuleRoot = (Get-Item -Path:($PSScriptRoot)).Parent.Parent.FullName
-                            $configFilePath = join-path -path $ModuleRoot -childpath 'Config.json'
-                            write-host $configFilePath
-                            $config = Get-Content -Path $configFilePath | ConvertFrom-Json
-                            foreach ($newSetting in $config.psobject.properties) {
-                                foreach ($copiedSetting in $savedJCSettings.psobject.properties) {
-                                    if ($newSetting.name -eq $copiedSetting.name) {
-                                        # If the new property is in the copied settings property list:
-                                        foreach ($newProperty in $newSetting.value.psobject.properties) {
-                                            foreach ($copiedProperty in $copiedSetting.value.psobject.properties) {
-                                                # If the property names match & the new property is eligible to be copied, copy it
-                                                if ( ($newProperty.name -eq $copiedProperty.name) -And ($newProperty.Value.copy -eq $true)) {
-                                                    # If the values are different, copy the values
-                                                    if ( $newProperty.value.value -ne $copiedProperty.value.value) {
-                                                        $config.$($newsetting.name).$($newProperty.name).value = $savedJCSettings.$($copiedSetting.name).$($copiedProperty.name).value
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                            $SettingsFunctionsDir = join-path -path $ModuleRoot -childpath 'private/settings'
+                            $SettingsFunctionsDir
+                            $Private = @( Get-ChildItem -Path $SettingsFunctionsDir -Recurse)
+                            Foreach ($Import in @($Private)) {
+                                Try {
+                                    . $Import.FullName
+                                    Write-Host "Imported: $($Import.Name)"
+                                } Catch {
+                                    Write-Error -Message "Failed to import function $($Import.FullName): $_"
                                 }
                             }
-
-                            $config | ConvertTo-Json | Out-File -path $configFilePath
+                            Update-JCSettingsFile -settings $savedJCSettings
+                            $global:JCConfig = Get-JCSettingsFile
                         }
                         # Confirm to user module update has been successful
                         Write-Host ('STATUS:') -BackgroundColor:($JCColorConfig.BackgroundColor) -ForegroundColor:($JCColorConfig.ForegroundColor_Header)
