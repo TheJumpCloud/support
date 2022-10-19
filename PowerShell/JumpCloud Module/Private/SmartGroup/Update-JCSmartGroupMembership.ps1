ddae7fbed6ade3a1780b5819be3cec0e79c64de4
Joe Workman <joe.workman@jumpcloud.com>
2022-10-19 12:03:18 -0600

    Get filters

--- PowerShell/JumpCloud Module/Private/SmartGroup/Update-JCSmartGroupMembership.ps1
+++ PowerShell/JumpCloud Module/Private/SmartGroup/Update-JCSmartGroupMembership.ps1
@@ -11,28 +11,49 @@ function Update-JCSmartGroupMembership {
         $ID
     )
     begin {
-        $SmartGroupDetails = Get-JCSmartGroup -GroupType $GroupType -ID $ID
+        $SmartGroupDetails = Get-JCSmartGroup2 -GroupType $GroupType -ID $ID
 
         # Build Attribute string:
-        $string = ''
-        $And = $SmartGroupDetails.Attributes.And.PSobject.Properties.Name
-        $or = $SmartGroupDetails.Attributes.Or.PSobject.Properties.Name
-        $ret = @($and) + @($or)
-        foreach ($item in $ret) {
-            $string += \'"$item"\'
+        $filterBuilder = @()
+        $ands = $SmartGroupDetails.Attributes.And.PSobject.Properties.Name
+
+        foreach ($And in $ands) {
+            $filterBuilder += "$($and):" + $SmartGroupDetails.Attributes.And."$and"
         }
-        $ret = $ret.replace(" ", "")
+        # $string = ''
+        # $And = $SmartGroupDetails.Attributes.And.PSobject.Properties.Name
+        # $or = $SmartGroupDetails.Attributes.Or.PSobject.Properties.Name
+        # $ret = @($and) + @($or)
+        # foreach ($item in $ret) {
+        #     $string += "'$item'"
+        # }
+        # $ret = $ret.replace(" ", "")
         #TODO: Turn the attributes into a list and pass to next function
-        $systems = Get-DynamicHash -Object $GroupType -returnProperties $ret
+        # $filter @{
+        #     "osFamily:$eq:darwin"
+        # }
+        $Search = @{
+            filter = @{
+                and = $filterBuilder
+            }
+        }
+        $systems = Search-JcSdkSystem -Body:($Search)
+        $systems.count
         # TODO:, figure out how to call this once if we are updating all groups
     }
     process {
         # For the group specified, go fetch system group membership
+        #TODO: update the groups
         switch ($GroupType) {
             'System' {
                 $existingMembers = Get-JcSdkSystemGroupMembership -GroupId $ID
-                "existing users"
-                $existingMembers
+                foreach ($system in $systems) {
+                    # If Existing Member not in group, add
+                    if ($system.id -notin $existingMembers.Id) {
+                        Set-JcSdkSystemGroupMember -GroupId $ID -Op add -Id $system.id
+                    }
+                }
+                #TODO: remove system members if they do not match criteria
             }
             'User' {
 
