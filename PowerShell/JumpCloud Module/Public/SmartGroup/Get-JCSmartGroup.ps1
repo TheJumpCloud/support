531c0fd5b2d692edfda9e8e33bbd61df7a744b28
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-10-20 08:56:12 -0600

    include -all param

--- PowerShell/JumpCloud Module/Public/SmartGroup/Get-JCSmartGroup.ps1
+++ PowerShell/JumpCloud Module/Public/SmartGroup/Get-JCSmartGroup.ps1
@@ -1,14 +1,15 @@
 function Get-JCSmartGroup () {
     [CmdletBinding(DefaultParameterSetName = 'ByGroup')]
     param (
-        [Parameter(Mandatory, ParameterSetName = 'ByGroup', HelpMessage = 'Group Type you want to return')]
+        [Parameter(ParameterSetName = 'ByGroup', HelpMessage = 'Group Type you want to return')]
         [ValidateSet('User', 'System')]
         [String]$GroupType,
         [Parameter(HelpMessage = 'The ID of the JumpCloud Group you want to return.')]
         [String]$ByID,
         [Parameter(HelpMessage = 'The name of the JumpCloud Group you want to return.')]
-        [String]$GroupName
-
+        [String]$GroupName,
+        [Parameter(ParameterSetName = 'All', HelpMessage = 'Return all JumpCloud Smart Groups')]
+        [switch]$All
     )
 
     begin {
@@ -28,30 +29,34 @@ function Get-JCSmartGroup () {
 
     }
     process {
-
-        if ($PSCmdlet.ParameterSetName -eq 'ByGroup') {
-            #Write-Host "$($config.SmartGroups."$($GroupType)Groups".PSObject.Properties)"
-            if ($ById) {
-                $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
-                    if ($_.Value.ID -eq $ById) {
+        switch ($PSCmdlet.ParameterSetName) {
+            ByGroup {
+                #Write-Host "$($config.SmartGroups."$($GroupType)Groups".PSObject.Properties)"
+                if ($ById) {
+                    $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
+                        if ($_.Value.ID -eq $ById) {
+                            $resultsArray.Add($_.Value)
+                        }
+                    }
+                } elseif ($GroupName) {
+                    $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
+                        if ($_.Value.Name -eq $GroupName) {
+                            $resultsArray.Add($_.Value)
+                        }
+                    }
+                } else {
+                    $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
                         $resultsArray.Add($_.Value)
                     }
                 }
-            } elseif ($GroupName) {
-                $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
-                    if ($_.Value.Name -eq $GroupName) {
-                        $resultsArray.Add($_.Value)
-                    }
-                }
-            } else {
-                $config.SmartGroups."$($GroupType)Groups".PSObject.Properties | foreach {
-                    $resultsArray.Add($_.Value)
-                }
-
             }
-
+            All {
+                $UserGroups = Get-JCSmartGroup -GroupType User
+                $SystemGroups = Get-JCSmartGroup -GroupType System
+                $resultsArray.Add($UserGroups)
+                $resultsArray.Add($SystemGroups)
+            }
         }
-
     } end {
         return $resultsArray
     }
