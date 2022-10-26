4fd3b8cb8d5c3cc90f9b46ebef4aeefdcf6daf98
Joe Workman <joe.workman@jumpcloud.com>
2022-10-26 14:51:09 -0600

    tests and Type param to specify how to generate

--- PowerShell/JumpCloud Module/Public/Utilities/CSV_Import/New-JCImportTemplate.ps1
+++ PowerShell/JumpCloud Module/Public/Utilities/CSV_Import/New-JCImportTemplate.ps1
@@ -5,73 +5,87 @@ Function New-JCImportTemplate() {
     (
         [Parameter(
             ParameterSetName = 'force',
-            HelpMessage = 'Parameter to force populate CSV with all headers when creating an update template ')]
+            HelpMessage = 'Parameter to force populate CSV with all headers when creating an update template. When selected this option will forcefully replace existing files in the current working directory. i.e. If you ',
+            Mandatory = $false)]
         [Switch]
-        $force
+        $Force,
+        [Parameter(ParameterSetName = 'force',
+            HelpMessage = 'Type of CSV to Create. Update or Import are valid options.',
+            Mandatory = $true)]
+        [ValidateSet('Import', 'Update')]
+        $Type
     )
 
     begin {
-        $Banner = @"
-       __                          ______ __                   __
-      / /__  __ ____ ___   ____   / ____// /____   __  __ ____/ /
- __  / // / / // __  __ \ / __ \ / /    / // __ \ / / / // __  /
+        $date = Get-Date -Format MM-dd-yyyy
+
+        if ($PSCmdlet.ParameterSetName -eq 'force') {
+            $ExportLocation = $PWD
+        } else {
+
+            $Banner = @"
+        __                          ______ __                   __
+        / /__  __ ____ ___   ____   / ____// /____   __  __ ____/ /
+    __  / // / / // __  __ \ / __ \ / /    / // __ \ / / / // __  /
 / /_/ // /_/ // / / / / // /_/ // /___ / // /_/ // /_/ // /_/ /
 \____/ \____//_/ /_/ /_// ____/ \____//_/ \____/ \____/ \____/
-                       /_/
+                        /_/
                                     CSV User Import Template
 "@
 
-        $date = Get-Date -Format MM-dd-yyyy
+            $Heading2 = 'The CSV file will be created within the directory:'
+
+            If (!(Get-PSCallStack | Where-Object { $_.Command -match 'Pester' })) {
+                Clear-Host
+            }
+
+            Write-Host $Banner -ForegroundColor Green
+            Write-Host "`n$Heading2`n"
+            Write-Host " $PWD" -ForegroundColor Yellow
+            Write-Host ""
 
 
-        $Heading2 = 'The CSV file will be created within the directory:'
+            while ($ConfirmFile -ne 'Y' -and $ConfirmFile -ne 'N') {
+                $ConfirmFile = Read-Host  "Enter Y to confirm or N to change output location" #Confirm .csv file location creation
+            }
 
-        If (!(Get-PSCallStack | Where-Object { $_.Command -match 'Pester' })) {
-            Clear-Host
-        }
+            if ($ConfirmFile -eq 'Y') {
 
-        Write-Host $Banner -ForegroundColor Green
-        Write-Host "`n$Heading2`n"
-        Write-Host " $PWD" -ForegroundColor Yellow
-        Write-Host ""
+                $ExportLocation = $PWD
+            }
 
-
-        while ($ConfirmFile -ne 'Y' -and $ConfirmFile -ne 'N') {
-            $ConfirmFile = Read-Host  "Enter Y to confirm or N to change output location" #Confirm .csv file location creation
-        }
-
-        if ($ConfirmFile -eq 'Y') {
-
-            $ExportLocation = $PWD
-        }
-
-        elseif ($ConfirmFile -eq 'N') {
-            $ExportLocation = Read-Host "Enter the full path to the folder you wish to create the import file in"
-
-            while (-not(Test-Path -Path $ExportLocation -PathType Container)) {
-                Write-Host -BackgroundColor Yellow -ForegroundColor Red "The location $ExportLocation does not exist. Try another"
+            elseif ($ConfirmFile -eq 'N') {
                 $ExportLocation = Read-Host "Enter the full path to the folder you wish to create the import file in"
 
+                while (-not(Test-Path -Path $ExportLocation -PathType Container)) {
+                    Write-Host -BackgroundColor Yellow -ForegroundColor Red "The location $ExportLocation does not exist. Try another"
+                    $ExportLocation = Read-Host "Enter the full path to the folder you wish to create the import file in"
+
+                }
+                Write-Host ""
+                Write-Host -BackgroundColor Green -ForegroundColor Black "The CSV file will be created within the $ExportLocation directory"
+                Pause
+
             }
-            Write-Host ""
-            Write-Host -BackgroundColor Green -ForegroundColor Black "The CSV file will be created within the $ExportLocation directory"
-            Pause
-
         }
-
     }
 
     process {
+        if ($type -eq 'Import') {
+            $ConfirmUpdateVsNew = 'N'
+        } elseif ($type -eq 'Update') {
+            $ConfirmUpdateVsNew = 'U'
+        } Else {
+            Write-Host "`nDo you want to create an import CSV template for creating new users or for updating existing users?"
+            Write-Host 'Enter "N" for to create a template for ' -NoNewline
+            Write-Host -ForegroundColor Yellow 'new users'
+            Write-Host 'Enter "U" for creating a template for ' -NoNewline
+            Write-Host -ForegroundColor Yellow "updating existing users"
 
-        Write-Host "`nDo you want to create an import CSV template for creating new users or for updating existing users?"
-        Write-Host 'Enter "N" for to create a template for ' -NoNewline
-        Write-Host -ForegroundColor Yellow 'new users'
-        Write-Host 'Enter "U" for creating a template for ' -NoNewline
-        Write-Host -ForegroundColor Yellow "updating existing users"
 
-
-        while ($ConfirmUpdateVsNew -ne 'N' -and $ConfirmUpdateVsNew -ne 'U') {
-            $ConfirmUpdateVsNew = Read-Host  "Enter N for 'new users' or U for 'updating users'"
+            while ($ConfirmUpdateVsNew -ne 'N' -and $ConfirmUpdateVsNew -ne 'U') {
+                $ConfirmUpdateVsNew = Read-Host  "Enter N for 'new users' or U for 'updating users'"
+            }
         }
 
         if (($ConfirmUpdateVsNew -eq 'U' -and $PSCmdlet.ParameterSetName -eq 'force') -or ($ConfirmUpdateVsNew -eq 'N' -and $PSCmdlet.ParameterSetName -eq 'force')) {
@@ -423,37 +437,54 @@ Function New-JCImportTemplate() {
 
     end {
         $ExportPath = Test-Path ("$ExportLocation/$FileName")
-        if (!$ExportPath ) {
+        if ($PSCmdlet.ParameterSetName -eq 'force') {
+            if (!$ExportPath ) {
+                Write-Host ""
+                $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
+                Write-Host 'Creating file '  -NoNewline
+                Write-Host $FileName -ForegroundColor Yellow -NoNewline
+                Write-Host ' in the location' -NoNewline
+                Write-Host " $ExportLocation" -ForegroundColor Yellow
+            } else {
+                Write-Warning "The file $fileName already exists, overwriting..."
+                $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
+                Write-Host 'Creating file '  -NoNewline
+                Write-Host $FileName -ForegroundColor Yellow -NoNewline
+                Write-Host ' in the location' -NoNewline
+                Write-Host " $ExportLocation" -ForegroundColor Yellow
+            }
+        } Else {
+            if (!$ExportPath ) {
+                Write-Host ""
+                $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
+                Write-Host 'Creating file'  -NoNewline
+                Write-Host " $fileName" -ForegroundColor Yellow -NoNewline
+                Write-Host ' in the location' -NoNewline
+                Write-Host " $ExportLocation" -ForegroundColor Yellow
+            } else {
+                Write-Host ""
+                Write-Warning "The file $fileName already exists do you want to overwrite it?" -WarningAction Inquire
+                Write-Host ""
+                $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
+                Write-Host 'Creating file '  -NoNewline
+                Write-Host $FileName -ForegroundColor Yellow -NoNewline
+                Write-Host ' in the location' -NoNewline
+                Write-Host " $ExportLocation" -ForegroundColor Yellow
+            }
             Write-Host ""
-            $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
-            Write-Host 'Creating file'  -NoNewline
-            Write-Host " $fileName" -ForegroundColor Yellow -NoNewline
-            Write-Host ' in the location' -NoNewline
-            Write-Host " $ExportLocation" -ForegroundColor Yellow
-        } else {
-            Write-Host ""
-            Write-Warning "The file $fileName already exists do you want to overwrite it?" -WarningAction Inquire
-            Write-Host ""
-            $CSVheader | Export-Csv -path "$ExportLocation/$FileName" -NoTypeInformation
-            Write-Host 'Creating file '  -NoNewline
-            Write-Host $FileName -ForegroundColor Yellow -NoNewline
-            Write-Host ' in the location' -NoNewline
-            Write-Host " $ExportLocation" -ForegroundColor Yellow
-        }
+            Write-Host "Do you want to open the file" -NoNewLine
+            Write-Host " $FileName`?" -ForegroundColor Yellow
 
-        Write-Host ""
-        Write-Host "Do you want to open the file" -NoNewLine
-        Write-Host " $FileName`?" -ForegroundColor Yellow
+            while ($Open -ne 'Y' -and $Open -ne 'N') {
+                $Open = Read-Host  "Enter Y for Yes or N for No"
+            }
 
-        while ($Open -ne 'Y' -and $Open -ne 'N') {
-            $Open = Read-Host  "Enter Y for Yes or N for No"
-        }
+            if ($Open -eq 'Y') {
+                Invoke-Item -path "$ExportLocation/$FileName"
 
-        if ($Open -eq 'Y') {
-            Invoke-Item -path "$ExportLocation/$FileName"
-
-        }
-        if ($Open -eq 'N') {
+            }
+            if ($Open -eq 'N') {
+            }
         }
     }
 }
