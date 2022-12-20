6af6406480e437c142adc47338ac80bdf75d092e
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-12-20 16:26:18 -0700

    Get-OpenSSLVersion

--- scripts/automation/Radius/Config.ps1
+++ scripts/automation/Radius/Config.ps1
@@ -28,7 +28,7 @@ $Subj = [PSCustomObject]@{
 $CertType = "EmailSAN"
 
 # When we import this config, this function will run and validate the openSSL binary location
-function Get-OpenSSLVerion {
+function Get-OpenSSLVersion {
     [CmdletBinding()]
     param (
         [Parameter()]
@@ -36,24 +36,30 @@ function Get-OpenSSLVerion {
         $opensslBinary
     )
     begin {
-        $version = openssl version
-        $dictionary = @{
-            # LibreSSL = [version]"2.8.3"
-            OpenSSL = [version]"3.0.0"
+        try {
+            $version = openssl version
+        } catch {
+            Write-Host "OpenSSL was not found in PATH... attempting lookup via file path"
+            $OSPlatform = [System.Environment]::OSVersion.Platform
+            if ($OSPlatform -match "Win") {
+                $version = Invoke-Expression "C:\'Program Files'\OpenSSL-Win64\bin\openssl.exe version"
+            }
         }
+        $OpenSSLVersion = [version]"3.0.0"
         # Determine Libre or Open SSL:
-        if ($version -match "LibreSSL|OpenSSL") {
-            $Type = $Matches[0]
+        if ($version -match "LibreSSL") {
+            Throw "LibreSSL does not meet the requirements of this application, please install OpenSSL v3.0.0 or later"
+        } else {
             [version]$Version = (Select-String -InputObject $version -Pattern "([0-9]+)\.([0-9]+)\.([0-9]+)").matches.value
         }
     }
     process {
-        if ($version -lt $dictionary[$Type]) {
-            Throw "The installed version of OpenSSL: $Type $Version, does not meet the requirements of this application, please install a later version of at least $Type $Version"
+        if ($version -lt $OpenSSLVersion) {
+            Throw "The installed version of OpenSSL: OpenSSL $Version, does not meet the requirements of this application, please install a later version of at least $Type $Version"
             exit 1
         } else {
-            Write-Host "$Type $Version is installed and meets required version for this application"
+            Write-Host "OpenSSL $Version is installed and meets required version for this application"
         }
     }
 }
-Get-OpenSSLVerion -opensslBinary $opensslBinary
+Get-OpenSSLVersion -opensslBinary $opensslBinary
