a22f81931128fcea59b16bf99da6e1c18f28d851
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2023-01-06 11:55:09 -0700

    Create Monitor-Commands.ps1

---
+++ scripts/automation/Radius/Monitor-Commands.ps1
@@ -0,0 +1,57 @@
+# Import Global Config:
+. "$psscriptroot/config.ps1"
+Connect-JCOnline $JCAPIKEY -Force
+
+# Tracking Variables for results
+$SuccessfulCommandRuns = @()
+$FailedCommandRuns = @()
+
+# Create CommandResults dir
+if (Test-Path "$PSScriptRoot/CommandResults") {
+    Write-Host "[status] Command Results Directory Exists"
+} else {
+    Write-Host "[status] Creating Command Results Directory"
+    [void](New-Item -ItemType Directory -Path "$PSScriptRoot/CommandResults")
+}
+
+# Get all Commands with the RadiusCertInstall trigger
+$RadiusCommands = Get-JCCommand | Where-Object trigger -Like 'RadiusCertInstall'
+$CommandCount = $RadiusCommands.Count
+
+# Get all Command Results for the RadiusCommands
+$CommandResults = Get-JCCommandResult -Detailed | Where-Object { $_.name -like "RadiusCert-Install*" }
+$ResultCount = $CommandResults.Count
+
+# Check results
+$SuccessfulCommandRuns = $CommandResults | Select-Object -ExcludeProperty command | Where-Object { $_.exitCode -eq "0" }
+$FailedCommandRuns = $CommandResults | Select-Object -ExcludeProperty command | Where-Object { $_.exitCode -eq "1" -or $_.exitCode -eq "4" }
+
+# Send Results to CSV
+$SuccessfulCommandRuns | Export-Csv -Path "$PSScriptRoot/CommandResults/SuccessfulCommands.csv" -NoTypeInformation
+$FailedCommandRuns | Export-Csv -Path "$PSScriptRoot/CommandResults/FailedCommands.csv" -NoTypeInformation
+
+while ($ResultCount -lt $CommandCount) {
+    # Gather Result information
+    $CommandResults = Get-JCCommandResult | Where-Object { $_.name -like "RadiusCert-Install*" }
+    $ResultCount = $CommandResults.Count
+
+    # Check results
+    $SuccessfulCommandRuns = $CommandResults | Select-Object -ExcludeProperty command | Where-Object { $_.exitCode -eq "0" }
+    $FailedCommandRuns = $CommandResults | Select-Object -ExcludeProperty command | Where-Object { $_.exitCode -eq "1" -or $_.exitCode -eq "4" }
+
+    # Send Results to CSV
+    $SuccessfulCommandRuns | Export-Csv -Path "$PSScriptRoot/CommandResults/SuccessfulCommands.csv" -NoTypeInformation
+    $FailedCommandRuns | Export-Csv -Path "$PSScriptRoot/CommandResults/FailedCommands.csv" -NoTypeInformation
+
+    # Track % Completed
+    $Completed = ($ResultCount / $CommandCount) * 100
+
+    # Progress Bar
+    Write-Progress -Activity "Checking Command Results..." -Status "Progress:" -PercentComplete $Completed
+
+    # Sleep 5 seconds
+    Start-Sleep -Seconds 5
+}
+
+Write-Host "[status] All commands have been executed"
+Write-Host "[status] $($SuccessfulCommandRuns.Count) successful command executions and $($FailedCommandRuns.Count) failures"
