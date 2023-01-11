35013d7ad2d7df99bcc21915012532526947c7d4
ttatarevicjc <tea.tatarevic@jumpcloud.com>
2023-01-11 14:04:05 -0500

    Adding RAA install and uninstall scripts to the scripts directory so they can be referenced in the KB

---
+++ scripts/macos/UninstallRemoteAssist.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+function get_app_pid() {
+    local -r APP_NAME=$1
+    ps -eo pid,comm | awk "/${APP_NAME}$/ {print \$1}"
+}
+function remove_app_by_name() {
+    local -r APP_NAME=$1
+    if PID=$(get_app_pid "${APP_NAME}") && [[ -n "${PID}" ]]; then
+        kill -s TERM "${PID}"
+        sleep 1
+    fi
+    if PID=$(get_app_pid "${APP_NAME}") && [[ -n "${PID}" ]]; then
+        kill -s KILL "${PID}"
+    fi
+
+    sudo rm -Rdf "/Applications/${APP_NAME}.app"
+}
+# Clean up installs having legacy name
+remove_app_by_name "Jumpcloud Assist App"
+# Clean up installs
+remove_app_by_name "JumpCloud Remote Assist"
---
+++ scripts/windows/UninstallRemoteAssist.ps1
@@ -0,0 +1,27 @@
+$legacyUninstallerPath="C:\Program Files\JumpCloud\Jumpcloud Assist App\Uninstall Jumpcloud Assist App.exe"
+$uninstallerPath="C:\Program Files\JumpCloud Remote Assist\Uninstall JumpCloud Remote Assist.exe"
+
+if ( Test-Path $legacyUninstallerPath ) {
+    Write-Host "Uninstalling legacy JumpCloud Remote Assist at " $legacyUninstallerPath
+    try {
+        $uninstallerProcess = Start-Process -FilePath $legacyUninstallerPath -Wait -PassThru -ArgumentList "/S"
+    }
+    catch {
+        Write-Error "Unable to uninstall legacy JumpCloud Remote Assist"
+        Write-Error $_
+        exit 1
+    }
+    Write-Host "Legacy JumpCloud Remote Assist uninstaller completed with exit code $($uninstallerProcess.ExitCode)"
+}
+if ( Test-Path $uninstallerPath ) {
+    Write-Host "Uninstalling JumpCloud Remote Assist at " $uninstallerPath
+    try {
+        $uninstallerProcess = Start-Process -FilePath $uninstallerPath -Wait -PassThru -ArgumentList "/S"
+    }
+    catch {
+        Write-Error "Unable to uninstall JumpCloud Remote Assist"
+        Write-Error $_
+        exit 1
+    }
+    Write-Host "JumpCloud Remote Assist uninstaller completed with exit code $($uninstallerProcess.ExitCode)"
+}
