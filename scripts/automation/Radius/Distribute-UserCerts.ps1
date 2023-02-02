e7c468e2bfaa588ec09a943b7eb3b26daf07222e
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2023-02-02 10:41:47 -0700

    move functions to functions file

--- scripts/automation/Radius/Distribute-UserCerts.ps1
+++ scripts/automation/Radius/Distribute-UserCerts.ps1
@@ -2,74 +2,66 @@
 . "$psscriptroot/config.ps1"
 Connect-JCOnline $JCAPIKEY -force
 
-# Functions
-function New-JCCommandFile {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)][System.IO.FileInfo]$certFilePath,
-        [Parameter(Mandatory = $true)][String]$FileName,
-        [Parameter(Mandatory = $true)][String]$FileDestination
-    )
-    begin {
-        $headers = @{
-            "x-api-key" = $JCAPIKEY
-            "x-org-id"  = $JCORGID
+################################################################################
+# Do not modify below
+################################################################################
+
+# Import the functions
+Import-Module "$psscriptroot/RadiusCertFunctions.ps1" -Force
+
+# Import the users.json file and convert to PSObject
+$userArray = Get-Content -Raw -Path "$PSScriptRoot/users.json" | ConvertFrom-Json -Depth 6
+# Check to see if previous commands exist
+$Command = Get-JCCommand | Where-Object { $_.Name -like 'RadiusCert-Install*' }
+
+if ($Command.Count -ge 1) {
+    $confirmation = Read-Host "[status] Previous RadiusCert commands detected, would you like to delete existing commands to proceed? [y/n]"
+    while ($confirmation -ne 'n') {
+        if ($confirmation -eq 'y') {
+            Write-Host "[status] Removing $($Command.Count) commands"
+            # Delete previous commands
+            $Command | Remove-JCCommand -force | Out-Null
+            # Clean up users.json array
+            $userArray | ForEach-Object { $_.commandAssociations = @() }
+            break
         }
-        $body = @{
-            content     = [convert]::ToBase64String((Get-Content -Path $certFilePath -AsByteStream))
-            name        = $FileName
-            destination = $FileDestination
-        }
-
     }
-    process {
-        $CommandFile = Invoke-RestMethod -Uri 'https://console.jumpcloud.com/api/files' -Method POST -Headers $headers -Body $body
-    }
-    end {
-        return $CommandFile._id
-    }
-}
-# End Functions
-
-# Get hashtables for users and systems
-$UserHash = Get-JCUser -returnProperties username
-$SystemHash = Get-JCSystem -returnProperties displayName, os
-
-# Get all users in the defined user group
-$JCUSERS = Get-JCUserGroupMember -ByID $JCUSERGROUP
-
-# Find user to system associations
-$SystemUserAssociations = @()
-$JCUSERS | ForEach-Object {
-    $SystemUserAssociations += (Get-JCAssociation -Type user -Id $_.UserID -TargetType system | Select-Object @{N = 'UserID'; E = { $_.id } }, @{N = 'SystemID'; E = { $_.targetId } })
+    Write-Host "[status] Proceeding with execution"
 }
 
-# Create commands for each association
-foreach ($association in $SystemUserAssociations) {
-    # Gather user and system information
-    $SystemInfo = $SystemHash | Where-Object { $association.SystemID -eq $_._id }
-    $UserInfo = $UserHash | Where-Object { $association.UserID -eq $_._id }
-
+# Create commands for each user
+foreach ($user in $userArray) {
     # Get certificate and zip to upload to Commands
-    $userPfx = "$psscriptroot/UserCerts/$($UserInfo.username)-client-signed.pfx"
-    $userPfxZip = "$psscriptroot/UserCerts/$($UserInfo.username)-client-signed.zip"
+    $userPfx = "$psscriptroot/UserCerts/$($user.userName)-client-signed.pfx"
+    $userPfxZip = "$psscriptroot/UserCerts/$($user.userName)-client-signed.zip"
 
     Compress-Archive -Path $userPfx -DestinationPath $userPfxZip -CompressionLevel NoCompression -Force
     # Find OS of System
-    if ($SystemInfo.os -eq 'Mac OS X') {
+    if ($user.systemAssociations.osFamily -contains 'Mac OS X') {
+        # Get the macOS system ids
+        $systemIds = $user.systemAssociations | Where-Object { $_.osFamily -eq 'Mac OS X' } | Select-Object systemId
+
+        # Check to see if previous commands exist
+        $Command = Get-JCCommand -name "RadiusCert-Install:$($user.userName):MacOSX"
+
+        if ($Command.Count -ge 1) {
+            $confirmation = Write-Host "[status] RadiusCert-Install:$($user.userName):MacOSX command already exists, skipping..."
+            continue
+        }
+
         # Create new Command and upload the signed pfx
         try {
             $CommandBody = @{
-                Name              = "RadiusCert-Install:$($UserInfo.username):$($SystemInfo.displayName)"
+                Name              = "RadiusCert-Install:$($user.userName):MacOSX"
                 Command           = @"
 set -e
-unzip -o /tmp/$($UserInfo.username)-client-signed.zip -d /tmp
+unzip -o /tmp/$($user.userName)-client-signed.zip -d /tmp
 currentUser=`$(/usr/bin/stat -f%Su /dev/console)
 currentUserUID=`$(id -u "`$currentUser")
-if [[ `$currentUser ==  $($UserInfo.username) ]]; then
-    /bin/launchctl asuser "`$currentUserUID" sudo -iu "`$currentUser" /usr/bin/security import /tmp/$($UserInfo.username)-client-signed.pfx -k /Users/$($UserInfo.username)/Library/Keychains/login.keychain -P $JCUSERCERTPASS
+if [[ `$currentUser ==  $($user.userName) ]]; then
+    /bin/launchctl asuser "`$currentUserUID" sudo -iu "`$currentUser" /usr/bin/security import /tmp/$($user.userName)-client-signed.pfx -k /Users/$($user.userName)/Library/Keychains/login.keychain -P $JCUSERCERTPASS
 else
-    echo "Current logged in user, `$currentUser, does not match expected certificate user. Please ensure $($UserInfo.username) is signed in and retry"
+    echo "Current logged in user, `$currentUser, does not match expected certificate user. Please ensure $($user.userName) is signed in and retry"
     exit 4
 fi
 
@@ -80,22 +72,46 @@ fi
                 commandType       = "mac"
                 timeout           = 600
                 TimeToLiveSeconds = 864000
-                files             = (New-JCCommandFile -certFilePath $userPfxZip -FileName "$($UserInfo.username)-client-signed.zip" -FileDestination "/tmp/$($UserInfo.username)-client-signed.zip")
+                files             = (New-JCCommandFile -certFilePath $userPfxZip -FileName "$($user.userName)-client-signed.zip" -FileDestination "/tmp/$($user.userName)-client-signed.zip")
             }
             $NewCommand = New-JcSdkCommand @CommandBody
 
             # Find newly created command and add system as target
             # TODO: Condition for duplicate commands
-            $Command = Get-JCCommand -name "RadiusCert-Install:$($UserInfo.username):$($SystemInfo.displayName)"
-            Set-JcSdkCommandAssociation -CommandId:("$($Command._id)") -Op 'add' -Type:('system') -Id:("$($association.SystemID)") | Out-Null
+            $Command = Get-JCCommand -name "RadiusCert-Install:$($user.userName):MacOSX"
+            $systemIds | ForEach-Object { Set-JcSdkCommandAssociation -CommandId:("$($Command._id)") -Op 'add' -Type:('system') -Id:("$($_.systemId)") | Out-Null }
         } catch {
             throw $_
         }
-        Write-Host "[status] Successfully created $($Command.name): User - $($UserInfo.Username); System - $($SystemInfo.displayName)"
-    } elseif ($SystemInfo.os -eq 'Windows') {
+
+        $CommandTable = [PSCustomObject]@{
+            commandId            = $command._id
+            commandName          = $command.name
+            commandPreviouslyRun = $false
+            commandQueued        = $false
+            systems              = $systemIds
+        }
+
+        $user.commandAssociations += $CommandTable
+
+        Write-Host "[status] Successfully created $($Command.name): User - $($user.userName); OS - Mac OS X"
+    }
+    if ($user.systemAssociations.osFamily -contains 'Windows') {
+        # Get the Windows system ids
+        $systemIds = $user.systemAssociations | Where-Object { $_.osFamily -eq 'Windows' } | Select-Object systemId
+
+        # Check to see if previous commands exist
+        $Command = Get-JCCommand -name "RadiusCert-Install:$($user.userName):Windows"
+
+        if ($Command.Count -ge 1) {
+            $confirmation = Write-Host "[status] RadiusCert-Install:$($user.userName):Windows command already exists, skipping..."
+            continue
+        }
+
+        # Create new Command and upload the signed pfx
         try {
             $CommandBody = @{
-                Name              = "RadiusCert-Install:$($UserInfo.username):$($SystemInfo.displayName)"
+                Name              = "RadiusCert-Install:$($user.userName):Windows"
                 Command           = @"
 `$ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -104,7 +120,7 @@ If ("Nuget" -notin `$PkgProvider.Name){
     Install-PackageProvider -Name NuGet -Force
 }
 `$CurrentUser = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-if (`$CurrentUser -eq "$($UserInfo.Username)") {
+if (`$CurrentUser -eq "$($user.userName)") {
     if (-not(Get-InstalledModule -Name RunAsUser -errorAction "SilentlyContinue")) {
         Write-Host "RunAsUser Module not installed, Installing..."
         Install-Module RunAsUser -Force
@@ -113,16 +129,16 @@ if (`$CurrentUser -eq "$($UserInfo.Username)") {
         Write-Host "RunAsUser Module installed, importing into session..."
         Import-Module RunAsUser -Force
     }
-    Expand-Archive -LiteralPath C:\Windows\Temp\$($UserInfo.username)-client-signed.zip -DestinationPath C:\Windows\Temp -Force
+    Expand-Archive -LiteralPath C:\Windows\Temp\$($user.userName)-client-signed.zip -DestinationPath C:\Windows\Temp -Force
     `$password = ConvertTo-SecureString -String $JCUSERCERTPASS -AsPlainText -Force
     `$ScriptBlock = { `$password = ConvertTo-SecureString -String "secret1234!" -AsPlainText -Force
-     Import-PfxCertificate -Password `$password -FilePath "C:\Windows\Temp\$($UserInfo.username)-client-signed.pfx" -CertStoreLocation Cert:\CurrentUser\My
+     Import-PfxCertificate -Password `$password -FilePath "C:\Windows\Temp\$($user.userName)-client-signed.pfx" -CertStoreLocation Cert:\CurrentUser\My
 }
-     Write-Host "Importing Pfx Certificate for $($UserInfo.username)"
+     Write-Host "Importing Pfx Certificate for $($user.userName)"
     `$JSON = Invoke-AsCurrentUser -ScriptBlock `$ScriptBlock -CaptureOutput
     `$JSON
 } else {
-    Write-Host "Current logged in user, `$CurrentUser, does not match expected certificate user. Please ensure $($UserInfo.Username) is signed in and retry."
+    Write-Host "Current logged in user, `$CurrentUser, does not match expected certificate user. Please ensure $($user.userName) is signed in and retry."
     exit 4
 }
 "@
@@ -132,19 +148,27 @@ if (`$CurrentUser -eq "$($UserInfo.Username)") {
                 shell             = "powershell"
                 timeout           = 600
                 TimeToLiveSeconds = 864000
-                files             = (New-JCCommandFile -certFilePath $userPfxZip -FileName "$($UserInfo.username)-client-signed.zip" -FileDestination "C:\Windows\Temp\$($UserInfo.username)-client-signed.zip")
+                files             = (New-JCCommandFile -certFilePath $userPfxZip -FileName "$($user.userName)-client-signed.zip" -FileDestination "C:\Windows\Temp\$($user.userName)-client-signed.zip")
             }
             $NewCommand = New-JcSdkCommand @CommandBody
 
             # Find newly created command and add system as target
-            $Command = Get-JCCommand -name "RadiusCert-Install:$($UserInfo.username):$($SystemInfo.displayName)"
-            Set-JcSdkCommandAssociation -CommandId:("$($Command._id)") -Op 'add' -Type:('system') -Id:("$($association.SystemID)") | Out-Null
+            $Command = Get-JCCommand -name "RadiusCert-Install:$($user.userName):Windows"
+            $systemIds | ForEach-Object { Set-JcSdkCommandAssociation -CommandId:("$($Command._id)") -Op 'add' -Type:('system') -Id:("$($_.systemId)") | Out-Null }
         } catch {
             throw $_
         }
-        Write-Host "[status] Successfully created $($Command.name): User - $($UserInfo.Username); System - $($SystemInfo.displayName)"
-    } else {
-        continue
+
+        $CommandTable = [PSCustomObject]@{
+            commandId            = $command._id
+            commandName          = $command.name
+            commandPreviouslyRun = $false
+            commandQueued        = $false
+            systems              = $systemIds
+        }
+
+        $user.commandAssociations += $CommandTable
+        Write-Host "[status] Successfully created $($Command.name): User - $($user.userName); OS - Windows"
     }
 }
 
@@ -157,42 +181,20 @@ $RadiusCommands = Get-JCCommand | Where-Object trigger -Like 'RadiusCertInstall'
 
 while ($confirmation -ne 'y') {
     if ($confirmation -eq 'n') {
-        Write-Host "[status] To invoke the commands at a later time, run the following script: $PSScriptRoot/Monitor-Commands.ps1"
+        $UserArray | ConvertTo-Json -Depth 6 | Out-File "$psscriptroot\users.json"
+        Write-Host "[status] To invoke the commands at a later time, run the following script: $PSScriptRoot/Monitor-CertDeployment.ps1"
         Write-Host "[status] Exiting..."
-
-        foreach ($command in $RadiusCommands) {
-            $CommandTable = [PSCustomObject]@{
-                commandId            = $command._id
-                commandName          = $command.name
-                commandPreviouslyRun = $false
-                commandQueued        = $false
-                resultTimestamp      = ""
-                result               = ""
-                exitCode             = ""
-            }
-            $CommandArray += $CommandTable
-        }
-        $CommandArray | ConvertTo-Json | Out-File "$psscriptroot\commands.json"
         exit
     }
 }
+
 [void](Invoke-JCCommand -trigger 'RadiusCertInstall')
 Write-Host "[status] Commands Invoked"
 
-foreach ($command in $RadiusCommands) {
-    $CommandTable = [PSCustomObject]@{
-        commandId            = $command._id
-        commandName          = $command.name
-        commandPreviouslyRun = $true
-        commandQueued        = $false
-        resultTimestamp      = ""
-        result               = ""
-        exitCode             = ""
-    }
-    $CommandArray += $CommandTable
-}
+# Set commandPreviouslyRun property to true
+$userArray.commandAssociations | ForEach-Object { $_.commandPreviouslyRun = $true }
 
-$CommandArray | ConvertTo-Json | Out-File "$psscriptroot\commands.json"
+$UserArray | ConvertTo-Json -Depth 6 | Out-File "$psscriptroot\users.json"
 
 Write-Host "[status] Run the Monitor-Commands.ps1 script to track command results and output results"
 Write-Host "[status] Exiting..."
--- scripts/automation/Radius/Monitor-CertDeployment.ps1
+++ scripts/automation/Radius/Monitor-CertDeployment.ps1
@@ -2,269 +2,33 @@
 . "$psscriptroot/config.ps1"
 Connect-JCOnline $JCAPIKEY -force
 
-# Begin Functions
-function Invoke-CommandRun {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [System.String]
-        $commandID
-    )
-    begin {
-        if ($commandID.length -ne 24) {
-            throw "Supplied CommandID is not of the correct length"
-        }
-    }
-    process {
-        $headers = @{
-            'x-api-key'    = $Env:JCApiKey
-            'x-org-id'     = $Env:JCOrgId
-            "content-type" = "application/json"
-        }
-        $body = @{
-            _id = $commandID
-        } | ConvertTo-Json
-        $response = Invoke-RestMethod -Uri 'https://console.jumpcloud.com/api/runCommand' -Method POST -Headers $headers -ContentType 'application/json' -Body $body
-    }
-    end {
-        if (!$response.queueIds) {
-            Throw "Command with ID: $commandID could not be triggered"
-        }
-
-    }
-
-}
-
-function Clear-JCQueuedCommand {
-    param (
-        [System.String]
-        $workflowId
-    )
-    process {
-        $headers = @{
-            'x-api-key' = $Env:JCApiKey
-            'x-org-id'  = $Env:JCOrgId
-        }
-        $response = Invoke-RestMethod -Uri "https://console.jumpcloud.com/api/v2/commandqueue/$workflowId" -Method DELETE -Headers $headers
-    }
-    end {
-        return $response
-    }
-}
-
-function Get-JCQueuedCommands {
-    begin {
-        $headers = @{
-            "x-api-key" = $Env:JCApiKey
-            "x-org-id"  = $Env:JCOrgId
-
-        }
-        $limit = [int]100
-        $skip = [int]0
-        $resultsArray = @()
-    }
-    process {
-        while (($resultsArray.results).Count -ge $skip) {
-            $response = Invoke-RestMethod -Uri "https://console.jumpcloud.com/api/v2/queuedcommand/workflows?limit=$limit&skip=$skip" -Method GET -Headers $headers
-            $skip += $limit
-            $resultsArray += $response.results
-        }
-    }
-    end {
-        return $resultsArray
-    }
-}
-
-
-Function Update-CommandsObject {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [System.String]
-        $jsonFile
-    )
-    begin {
-        $commandsObject = Get-Content -Raw -Path $jsonFile | ConvertFrom-Json
-        $QueuedCommandRuns = Get-JCQueuedCommands
-    }
-    process {
-        # Update the Command Result status for commands no longer in queue
-        foreach ($command in $commandsObject | Where-Object { $_.commandID -notin $QueuedCommandRuns.command }) {
-            $progress = $commandsObject.commandId.indexOf($command.commandId)
-            $Completed = ($progress / $commandsObject.count) * 100
-            Write-Progress -Activity "Checking Radius Command Results" -Status "Progress:" -PercentComplete $Completed
-            # update command results
-            $CommandResults = Get-JCCommandResult -CommandID $command.commandId
-            if ($commandResults) {
-                # write-host "$($commandResults.count) command results found for $($command.commandID)"
-                $lastCommandResult = $CommandResults | Sort-Object -Property responseTime | Select-Object -Last 1
-                # $lastCommandResultTimestamp.responseTime
-                $lastCommandResultTimestampUTC = Get-Date $lastCommandResult.responseTime -Format o -AsUTC
-                $command.commandPreviouslyRun = $true
-                $command.resultTimeStamp = $lastCommandResultTimestampUTC
-                $command.result = $lastCommandResult.output
-                $command.exitCode = $lastCommandResult.exitCode
-            }
-
-            if ($lastCommandResult.exitCode -eq 0) {
-                # Cleanup old failed results
-                $commandResults | Where-Object { $_._id -notin $lastCommandResult._id } | Remove-JCCommandResult -force | Out-Null
-            } else {
-                # Cleanup old results, save most recent
-                $commandResults | Sort-Object responseTime -Descending | Select-Object -Skip 1 | Remove-JCCommandResult -force | Out-Null
-            }
-        }
-        # Then update the command queue status for every object
-        foreach ($command in $commandsObject) {
-            # update current command queued status
-            if ($command.commandId -in $QueuedCommandRuns.command) {
-                $command.commandQueued = $true
-            } else {
-                $command.commandQueued = $false
-            }
-        }
-    }
-    end {
-        $commandsObject | ConvertTo-Json | Out-File $jsonFile
-        return $commandsObject
-    }
-}
-
-Function Invoke-CommandsRetry {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [System.string]
-        $jsonFile
-    )
-    begin {
-        $RetryCommands = @()
-        $commandsObject = Get-Content -Raw -Path $jsonFile | ConvertFrom-Json
-
-        $FailedCommands = $commandsObject | Where-Object { ($_.commandQueued -eq $false) -And ($_.exitCode -ne 0) }
-        Write-Host "$($failedCommands.count) commands will be re-run"
-    }
-    process {
-        # Prompt to rerun commands that have failed or expired
-        $FailedCommands | ForEach-Object {
-            try {
-                Invoke-CommandRun -commandID $_.commandId
-                # set command to queued
-                $_.commandQueued = $true
-                $RetryCommands += $_.commandId
-            } catch {
-                Write-Error "$($_.commandId) could not be invoked"
-            }
-        }
-    }
-    end {
-        # write out/ update jsonFile
-        $commandsObject | ConvertTo-Json | Out-File $jsonFile
-        return $RetryCommands
-    }
-}
-
-Function Get-CommandObjectTable {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [PSCustomObject]$retryCommands,
-        [Parameter()]
-        [string]$jsonFile,
-        [Parameter(ParameterSetName = 'Overview')]
-        [switch]$Overview,
-        [Parameter(ParameterSetName = 'Failed')]
-        [switch]$Failed
-    )
-    process {
-        switch ($PSCmdlet.ParameterSetName) {
-            Overview {
-                Write-Host "`n[Radius Cert Deployment Status - Overview]"
-                $commandsObject = Get-Content -Raw -Path $jsonFile | ConvertFrom-Json
-                foreach ($command in $commandsObject) {
-                    $character = if (($command.exitCode -eq 0)) {
-                        "$([char]0x1b)[92mOK"
-                    } elseif ($command.commandId -in $retryCommands) {
-                        "$([char]0x1b)[34mCOMMAND RE-SCHEDULED"
-                    } elseif (($command.exitCode -ne 0) -and ($command.commandQueued -eq $true)) {
-                        "$([char]0x1b)[93mPENDING"
-                    } elseif (($command.commandPreviouslyRun -eq $false) -and ($command.commandQueued -eq $false)) {
-                        "$([char]0x1b)[91mNOT SCHEDULED"
-                    } elseif (($command.exitCode -eq "") -and ($command.commandQueued -eq $false)) {
-                        "$([char]0x1b)[91mNOT SCHEDULED"
-                    } else {
-                        "$([char]0x1b)[91mFAILED"
-                    }
-                    $command | Add-Member -Name "Status" -Type NoteProperty -Value $character
-                }
-                $commandsObject | ForEach-Object { [PSCustomObject]$_ } | Format-Table commandName, @{N = "commandQueued"; E = { $_.commandQueued }; align = 'center' }, resultTimestamp, @{N = "exitCode"; E = { $_.exitCode }; align = 'center' }, Status -AutoSize
-            }
-            Failed {
-                Write-Host "`n[Radius Cert Deployment Status - Failed]"
-                $commandsObject = Get-Content -Raw -Path $jsonFile | ConvertFrom-Json
-                $failedCommands = $commandsObject | Where-Object { $_.commandId -in $retryCommands }
-                foreach ($command in $failedCommands) {
-                    $character = if (($command.exitCode -eq 0)) {
-                        "$([char]0x1b)[92mOK"
-                    } elseif ($command.commandId -in $retryCommands) {
-                        "$([char]0x1b)[34mCOMMAND RE-SCHEDULED"
-                    } elseif (($command.exitCode -ne 0) -and ($command.commandQueued -eq $true)) {
-                        "$([char]0x1b)[93mPENDING"
-                    } elseif (($command.commandPreviouslyRun -eq "") -and ($command.commandQueued -eq $false)) {
-                        "$([char]0x1b)[91mNOT SCHEDULED"
-                    } elseif (($command.exitCode -eq "") -and ($command.commandQueued -eq $false)) {
-                        "$([char]0x1b)[91mNOT SCHEDULED"
-                    } else {
-                        "$([char]0x1b)[91mFAILED"
-                    }
-                    $command | Add-Member -Name "Status" -Type NoteProperty -Value $character
-                }
-                $failedCommands | ForEach-Object { [PSCustomObject]$_ } | Format-Table commandName, @{N = "commandQueued"; E = { $_.commandQueued }; align = 'center' }, resultTimestamp, @{N = "exitCode"; E = { $_.exitCode }; align = 'center' }, Status -AutoSize
-            }
-        }
-    }
-}
-# End Functions
+################################################################################
+# Do not modify below
+################################################################################
+# Import the functions
+Import-Module "$psscriptroot/RadiusCertFunctions.ps1" -Force
 # Define jsonData file
-$jsonFile = "$PSScriptRoot/commands.json"
+$jsonFile = "$PSScriptRoot/users.json"
 
-# First get latest command results & set $jsonFile with any updates
-$CommandResults = Update-CommandsObject -jsonFile $jsonFile
-
-# Tracking Variables for results
-$SuccessfulCommandRuns = @()
-$AvailableCommands = @()
-
-# Check results
-$SuccessfulCommandRuns = $CommandResults | Where-Object { $_.exitCode -eq "0" }
-$AvailableCommands = $CommandResults | Where-Object { ($_.exitCode -ne 0) -And ($_.commandQueued -eq $false) }
-
-Get-CommandObjectTable -Overview -jsonFile $jsonFile
-
-# Prompt to rerun commands that have failed or expired (if possible)
-If ($AvailableCommands) {
-    $confirmation = Read-Host "Would you like to rerun failed/unrun commands and/or expired queued commands? [y/n]"
-    while ($confirmation -ne 'y') {
-        if ($confirmation -eq 'n') {
-            break
+# Show user selection
+do {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection) {
+        '1' {
+            Get-CommandObjectTable -Overview -jsonFile $jsonFile
+        } '2' {
+            Get-CommandObjectTable -Detailed -jsonFile $jsonFile
+        } '3' {
+            $retryCommands = Invoke-CommandsRetry -jsonFile $jsonFile
         }
     }
-    if ($confirmation -eq 'y') {
-        # Retry commands
-        $retryCommands = Invoke-CommandsRetry -jsonFile $jsonFile
-        Get-CommandObjectTable -jsonFile $jsonFile -retryCommands $retryCommands -Failed
-    }
-}
+    Pause
+} until ($selection.ToUpper() -eq 'Q')
 
 ################################################################################
 # If needed you can clear out your command queue with the following commands.
 # Copy and Paste these into a powershell terminal window to clear all queued
 # commands in your org.
-# $headers = @{
-#     "x-api-key" = $Env:JCApiKey
-#     "x-org-id"  = $Env:JCOrgId
-# }
-# $queuedCommands = Get-CommandQueue
-# foreach ($queue in $queuedCommands.id) {
-#     $response = Invoke-RestMethod -Uri "https://console.jumpcloud.com/api/v2/commandqueue/$($queue)" -Method DELETE -Headers $headers
-# }
+# Import-Module scripts/automation/Radius/RadiusCertFunctions.ps1 -force
+# Get-JCQueuedCommands | Foreach-Object { Clear-JCQueuedCommand -workflowId $_.id }
