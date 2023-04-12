c5e17db455fd2b2adcb6b7ef19bcda41a4422d3b
Joe Workman <joe.workman@jumpcloud.com>
2023-04-12 16:06:33 -0600

    validate registry setting for type

--- PowerShell/JumpCloud Module/Private/Policies/New-CustomRegistryTableRow.ps1
+++ PowerShell/JumpCloud Module/Private/Policies/New-CustomRegistryTableRow.ps1
@@ -15,16 +15,44 @@ function New-CustomRegistryTableRow {
         $customValueName
     )
     begin {
-        $validRegTypes = @('DWORD', 'expandString', 'multiString', 'QWORD', 'String')
+        $validRegTypes = @(
+            [pscustomobject]@{
+                row   = 0;
+                name  = 'DWORD';
+                value = 'DWORD'
+            },
+            [pscustomobject]@{
+                row   = 1;
+                name  = 'EXPAND_SZ';
+                value = 'expandString'
+            },
+            [pscustomobject]@{
+                row   = 2;
+                name  = 'MULTI_SZ';
+                value = 'multiString'
+            },
+            [pscustomobject]@{
+                row   = 3;
+                name  = 'QWORD';
+                value = 'QWORD';
+            },
+            [pscustomobject]@{
+                row   = 4;
+                name  = 'SZ';
+                value = 'String'
+            }
+        )
     }
     process {
         if (-Not $customData) {
             $customData = (Read-Host "Please enter the registry data value")
         }
         if (-Not $customRegType) {
+            $validRegTypes | Format-Table -Property Row, Name | Out-Host
             Do {
-                $customRegType = (Read-Host "Please enter a registry type ('DWORD', 'expandString', 'multiString', 'QWORD', 'String') value")
-            } While ($customRegType -notin $validRegTypes)
+                $customRegType = (Read-Host "Please enter a row number for a coresponding registry type (0 - 4)")
+                $customRegTypeValue = $validRegTypes[$customRegType].value
+            } While ($validRegTypes.Row -notcontains $customRegType)
         }
         if (-Not $customLocation) {
             $customLocation = (Read-Host "Please enter the registry key location value")
@@ -37,7 +65,7 @@ function New-CustomRegistryTableRow {
     end {
         $tableRow = [PSCustomObject]@{
             customData      = $customData
-            customRegType   = $customRegType
+            customRegType   = $customRegTypeValue
             customLocation  = $customLocation
             customValueName = $customValueName
         }
