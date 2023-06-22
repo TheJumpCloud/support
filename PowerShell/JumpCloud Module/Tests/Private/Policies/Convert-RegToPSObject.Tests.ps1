3c2182d33889c977cb8424d1cf272d32b2c05936
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2023-06-22 12:42:13 -0600

    fiix registry file tests

--- PowerShell/JumpCloud Module/Tests/Private/Policies/Convert-RegToPSObject.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Private/Policies/Convert-RegToPSObject.Tests.ps1
@@ -1,10 +1,6 @@
-BeforeAll {
-    $PesterRegistryFilePath = "$PSScriptRoot/Reg_File/PesterRegFile.reg"
-}
 Describe -Tag:('JCPolicy') 'Registry File Tests' {
-
     Context 'Test Reg File Conversion' {
-        $regFile = Convert-RegToPSObject -regFilePath $PesterRegistryFilePath
+        $regFile = Convert-RegToPSObject -regFilePath $PesterParams_RegistryFilePath
         It 'Convert-RegToPSObject should return object with values' {
             foreach ($regKey in $regFile) {
                 $regKey.customLocation | Should -Not -BeNullOrEmpty
