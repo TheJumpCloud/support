78b87531d9b3c72421afd0838a861553535a1407
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2023-06-30 12:16:14 -0600

    new test to validate regfile values

--- PowerShell/JumpCloud Module/Tests/Private/Policies/Convert-RegToPSObject.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Private/Policies/Convert-RegToPSObject.Tests.ps1
@@ -9,5 +9,32 @@ Describe -Tag:('JCPolicy') 'Registry File Tests' {
                 $regKey.customData | Should -Not -BeNullOrEmpty
             }
         }
+        It 'Convert-RegToPSObject validate correct values' {
+            foreach ($regKey in $regFile) {
+                $regKey.customLocation | Should -Be "SOFTWARE\Policies\Microsoft\Power\PowerSettings"
+                switch ($regKey.customRegType) {
+                    DWORD {
+                        $regKey.customValueName | Should -Be "DWORDValue"
+                        $regKey.customData | Should -Be 0
+                    }
+                    QWORD {
+                        $regKey.customValueName | Should -Be "QWORDValue"
+                        $regKey.customData | Should -Be 16
+                    }
+                    multiString {
+                        $regKey.customValueName | Should -Be "MULTISZValue"
+                        $regKey.customData | Should -Be "Test1\0Test2\0\0"
+                    }
+                    expandString {
+                        $regKey.customValueName | Should -Be "EXPANDSZValue"
+                        $regKey.customData | Should -Be "Test1"
+                    }
+                    String {
+                        $regKey.customValueName | Should -Be "ActivePowerScheme"
+                        $regKey.customData | Should -Be "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
+                    }
+                }
+            }
+        }
     }
 }
