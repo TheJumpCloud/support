b984c38a767882fc81cc1ed27a046be9cb364ac8
Joe Workman <joe.workman@jumpcloud.com>
2023-09-12 07:48:42 -0600

    Remove JCCommandTarget cleanup

--- PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Remove-JCCommandTarget.tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Commands/CommandTargets/Remove-JCCommandTarget.tests.ps1
@@ -1,28 +1,70 @@
-Describe -Tag:('JCCommandTarget') 'Add-JCCommandTarget 1.3' {
-    BeforeAll {
-        Connect-JCOnline -JumpCloudApiKey:($PesterParams_ApiKey) -force | Out-Null
-        $PesterParams_Command1 = Get-JCCommand -CommandID:($PesterParams_Command1.Id)
-        If (-not $PesterParams_Command1) {
-            $PesterParams_Command1 = New-JCCommand @PesterParams_NewCommand1
+Describe -Tag:('JCCommandTarget') 'Add-JCCommandTarget' {
+    Context 'SystemGroup to Command tests' {
+
+        BeforeEach {
+            $RandomString1 = ( -join (( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 8 | ForEach-Object { [char]$_ }))
+            $commandDef = @{
+                name        = "pester-JCCMDTarget_$RandomString1"
+                trigger     = 'JCCMDTarget'
+                commandType = 'linux'
+                command     = 'test'
+                launchType  = 'trigger'
+                timeout     = 120
+            }
+            $systemGroupDef = @{
+                GroupName = "PesterTest_SystemGroup_$RandomString1"
+            }
+            $cmd = New-JCCommand @commandDef
+            Write-Host "commandName: $($cmd.name)"
+
+            $systemGroup = New-JCSystemGroup @systemGroupDef
+            Write-Host "systemGroupName: $($systemGroup.name)"
+
         }
-        $PesterParams_SystemGroup = Get-JCGroup -Type:('System') | Where-Object { $_.name -eq $PesterParams_SystemGroup.Name }
-        If (-not $PesterParams_SystemGroup) {
-            $PesterParams_SystemGroup = New-JCSystemGroup @PesterParams_NewSystemGroup
+
+        It "Removes a single system group to a JumpCloud command by GroupName" {
+            $TargetAdd = Add-JCCommandTarget -CommandID $cmd.id -GroupName $systemGroup.Name
+            $TargetRemove = Remove-JCCommandTarget -CommandID $cmd.id -GroupName $systemGroup.Name
+            $TargetRemove.Status | Should -Be 'Removed'
+        }
+        It "Removes a single system group to a JumpCloud command by GroupID" {
+            $TargetAdd = Add-JCCommandTarget -CommandID $cmd.id -GroupID $systemGroup.Id
+            $TargetRemove = Remove-JCCommandTarget -CommandID $cmd.id -GroupID $systemGroup.id
+            $TargetRemove.Status | Should -Be 'Removed'
+        }
+        AfterEach {
+            Write-Host "removing commandName: $($cmd.name)"
+            Remove-JCCommand -CommandID $cmd._id -force
+            Write-Host "removing systemGroupName: $($systemGroup.name)"
+            Remove-JcSdkSystemGroup -Id $systemGroup.id
         }
     }
-    It "Removes a single system to a JumpCloud command" {
-        $TargetAdd = Add-JCCommandTarget -CommandID $PesterParams_Command1.id -SystemID $PesterParams_SystemLinux._id
-        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams_Command1.id -SystemID $PesterParams_SystemLinux._id
-        $TargetRemove.Status | Should -Be 'Removed'
-    }
-    It "Removes a single system group to a JumpCloud command by GroupName" {
-        $TargetAdd = Add-JCCommandTarget -CommandID $PesterParams_Command1.id -GroupName $PesterParams_SystemGroup.Name
-        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams_Command1.id -GroupName $PesterParams_SystemGroup.Name
-        $TargetRemove.Status | Should -Be 'Removed'
-    }
-    It "Removes a single system group to a JumpCloud command by GroupID" {
-        $TargetAdd = Add-JCCommandTarget -CommandID $PesterParams_Command1.id -GroupID $PesterParams_SystemGroup.Id
-        $TargetRemove = Remove-JCCommandTarget -CommandID $PesterParams_Command1.id -GroupName $PesterParams_SystemGroup.Name
-        $TargetRemove.Status | Should -Be 'Removed'
+    Context "System to Command tests" {
+        BeforeEach {
+            $RandomString1 = ( -join (( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 8 | ForEach-Object { [char]$_ }))
+            $commandDef = @{
+                name        = "pester-JCCMDTarget_$RandomString1"
+                trigger     = 'JCCMDTarget'
+                commandType = 'linux'
+                command     = 'test'
+                launchType  = 'trigger'
+                timeout     = 120
+            }
+            $systemGroupDef = @{
+                GroupName = "PesterTest_SystemGroup_$RandomString1"
+            }
+            $cmd = New-JCCommand @commandDef
+            Write-Host "commandName: $($cmd.name)"
+
+        }
+        It "Removes a single system to a JumpCloud command" {
+            $TargetAdd = Add-JCCommandTarget -CommandID $cmd.id -SystemID $PesterParams_SystemLinux._id
+            $TargetRemove = Remove-JCCommandTarget -CommandID $cmd.id -SystemID $PesterParams_SystemLinux._id
+            $TargetRemove.Status | Should -Be 'Removed'
+        }
+        AfterEach {
+            Write-Host "removing commandName: $($cmd.name)"
+            Remove-JCCommand -CommandID $cmd._id -force
+        }
     }
 }
