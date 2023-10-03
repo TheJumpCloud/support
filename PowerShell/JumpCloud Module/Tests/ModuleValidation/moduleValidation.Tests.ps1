f4026816dbe5338b7b3b592be2a9b6e38ff85985
Joe Workman <joe.workman@jumpcloud.com>
2023-10-03 16:20:58 -0600

    re-commit tests file

---
+++ PowerShell/JumpCloud Module/Tests/ModuleValidation/moduleValidation.Tests.ps1
@@ -0,0 +1,51 @@
+Describe -Tag:('ModuleValidation') 'Module Manifest Tests' {
+    It ('Passes Test-ModuleManifest') {
+        $module = Test-ModuleManifest -Path:("$FilePath_psd1")
+        # validate module
+        $module.RootModule | Should -Be "JumpCloud.psm1"
+        $module.Author | Should -Be "JumpCloud Solutions Architect Team"
+        $module.CompanyName | Should -Be "JumpCloud"
+        $module.Copyright | Should -Be "(c) JumpCloud. All rights reserved."
+        $module.Description | Should -Be "PowerShell functions to manage a JumpCloud Directory-as-a-Service"
+
+        # validate required Modules
+        $RequiredModules = @('JumpCloud.SDK.DirectoryInsights', 'JumpCloud.SDK.V1', 'JumpCloud.SDK.V2')
+        $RequiredModules | ForEach-Object {
+            $module.RequiredModules.Name | should -Contain $_
+        }
+
+        $ExportedFuncs = $module.ExportedFunctions.keys | Group-Object
+        foreach ($func in $ExportedFuncs) {
+            $func.count | should -BeExactly 1
+        }
+        # Validate module version
+        $latestModule = Find-Module -Name JumpCloud
+        $module.Version | should -BeGreaterThan $latestModule.version
+        # GHA Env Variables
+        if (-Not $env:OVERRIDE_VERSION) {
+            switch ($env:RELEASE_TYPE) {
+                'major' {
+                    $module.Version.Major | Should -Be (([version]$latestModule.Version).Major + 1)
+                }
+                'minor' {
+                    $module.Version.Minor | Should -Be (([version]$latestModule.Version).Minor + 1)
+                }
+                'patch' {
+                    $module.Version.Build | Should -Be (([version]$latestModule.Version).Build + 1)
+                }
+            }
+        }
+    }
+    It 'When Update-ModuleManifest is run, no changes in the current branch are behind' {
+        Update-ModuleManifest -Path:($FilePath_psd1)
+        $diff = git diff -- $FilePath_psd1
+        if ($diff) {
+            write-warning "diff found in file: $FilePath_psd1 when we expected none to exist; have you run `Update-ModuleManifest -Path /src/PowerShell/JumpCloud Module/JumpCloud.psd1` and committed the resulting changes?"
+        }
+        $diff | Should -BeNullOrEmpty
+        $moduleContent = Get-COntent -Path ("$FilePath_psd1")
+        $stringMatch = Select-String -InputObject $moduleContent -Pattern "# Generated on: ([\d]+\/[\d]+\/[\d]+)"
+        $PSD1_date = $stringMatch.matches.groups[1].value
+        ([datetime]$PSD1_date) | Should -Be ([datetime]( Get-Date -Format "M/d/yyyy" ))
+    }
+}
