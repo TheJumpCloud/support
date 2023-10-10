923f1030f93476d37d7c58517b527e9cd15843a6
Ken Maranion <ken.maranion@jumpcloud.com>
2023-10-10 13:33:06 -0700

    test

--- .github/scripts/tests/test_build_commands_gallery.py
+++ .github/scripts/tests/test_build_commands_gallery.py
@@ -1,6 +1,7 @@
 from ..build_commands_gallery import parse_commands_to_json, set_links, validate_commands_galleryMD
 import subprocess
 import os
+import git
 
 def test_script_functions():
     try:
@@ -12,14 +13,16 @@ def test_script_functions():
 
 #subprocess.check_output(['git', 'diff', '--name-only', currentBranch + '..' + master])
 def test_diff():
-    try:
-        base_ref = os.environ.get('GITHUB_BASE_REF')
+    # try:
+    #     base_ref = os.environ.get('GITHUB_BASE_REF')
 
-        print(base_ref)
-        diff_output = subprocess.check_output(['git', 'diff', base_ref], universal_newlines=True, stderr=subprocess.STDOUT)
+    #     print(base_ref)
+    #     diff_output = subprocess.check_output(['git', 'diff', base_ref], universal_newlines=True, stderr=subprocess.STDOUT)
 
-    except subprocess.CalledProcessError as e:
-        print("Exception on process, rc=", e.returncode, "output=", e.output)
+    # except subprocess.CalledProcessError as e:
+    #     print("Exception on process, rc=", e.returncode, "output=", e.output)
+    repo = git.Repo('.')
+    diff = repo.git.diff('master')
     # Check if the expected lines are present in the output
     expected_lines = [
         "diff --git a/PowerShell/JumpCloud Commands Gallery/commands.json b/PowerShell/JumpCloud Commands Gallery/commands.json",
@@ -27,4 +30,4 @@ def test_diff():
     ]
 
     for line in expected_lines:
-        assert line in diff_output
+        assert line in diff
