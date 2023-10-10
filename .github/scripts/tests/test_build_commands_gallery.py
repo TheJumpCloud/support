1302a4c26207bc4764173d57acfb2d0179a1bd8a
Ken Maranion <ken.maranion@jumpcloud.com>
2023-10-10 11:49:23 -0700

    test

--- .github/scripts/tests/test_build_commands_gallery.py
+++ .github/scripts/tests/test_build_commands_gallery.py
@@ -1,22 +1,9 @@
-import json
-import os
-# import git
-import re
-#from git
-from tempfile import TemporaryDirectory
-#Import the functions from the script
 from ..build_commands_gallery import parse_commands_to_json, set_links, validate_commands_galleryMD
 import subprocess
-#import pytest
 
 
 
-# Pull master then do pytest difference
 def test_script_functions():
-    # You can use temporary directories and files for testing if needed.
-    # For simplicity, we'll just use the current directory for this example.
-
-    # Capture the standard output to check for exceptions.
     try:
         parse_commands_to_json()
         set_links()
@@ -27,14 +14,8 @@ def test_script_functions():
 #subprocess.check_output(['git', 'diff', '--name-only', currentBranch + '..' + master])
 def test_diff():
 
-    # git_diff_output = subprocess.check_output(['git', 'diff', 'master'], universal_newlines=True)
-    # grep_output = subprocess.check_output(['grep', 'b/PowerShell/JumpCloud Commands Gallery/commands.json'], input=git_diff_output, universal_newlines=True)
-    # # Print the grep output
-    # print(grep_output)
-    # # Do assertions on the grep_output has to have b/PowerShell/JumpCloud Commands Gallery/commands.json
-    # assert grep_output == 'b/PowerShell/JumpCloud Commands Gallery/commands.json\n'
-    # Run the git diff command
-    git_diff_output = subprocess.check_output(['git', 'diff', 'master'], universal_newlines=True)
+    github_base_ref = os.environ.get("GITHUB_BASE_REF")
+    git_diff_output = subprocess.check_output(['git', 'diff', github_base_ref], universal_newlines=True)
 
     # Check if the expected lines are present in the output
     expected_lines = [
