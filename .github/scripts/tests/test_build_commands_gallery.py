e36b368ca1a5bf5cd2799cc517062675c493ba8a
Ken Maranion <ken.maranion@jumpcloud.com>
2023-10-10 14:21:10 -0700

    test

--- .github/scripts/tests/test_build_commands_gallery.py
+++ .github/scripts/tests/test_build_commands_gallery.py
@@ -2,7 +2,7 @@ from ..build_commands_gallery import parse_commands_to_json, set_links, validate
 import subprocess
 import os
 import git
-
+from os.path import dirname
 def test_script_functions():
     try:
         parse_commands_to_json()
@@ -13,18 +13,22 @@ def test_script_functions():
 
 #subprocess.check_output(['git', 'diff', '--name-only', currentBranch + '..' + master])
 def test_diff():
-    try:
-        base_ref = os.environ.get('github.base.ref')
-        head_ref = os.environ.get('github.head_ref')
+    # try:
+    #     base_ref = os.environ.get('github.base.ref')
+    #     head_ref = os.environ.get('github.head_ref')
 
-        print(base_ref)
-        print(head_ref)
-        diff = subprocess.check_output(['git', 'diff', 'master'], universal_newlines=True, stderr=subprocess.STDOUT)
+    #     print(base_ref)
+    #     print(head_ref)
+    #     diff = subprocess.check_output(['git', 'diff', 'origin/master', 'sa-3606-Update-CommandsJSON-CI'], universal_newlines=True, stderr=subprocess.STDOUT)
 
-    except subprocess.CalledProcessError as e:
-        print("Exception on process, rc=", e.returncode, "output=", e.output)
-    # repo = git.Repo('./')
-    # diff = repo.git.diff('${{ github.base.ref }}, ${{ github.head_ref }}')
+    # except subprocess.CalledProcessError as e:
+    #     print("Exception on process, rc=", e.returncode, "output=", e.output)
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+    # rootPath is root of the directory
+    rootPath = dirname(dirname(dirname(scriptPath)))
+    print(rootPath)
+    repo = git.Repo(rootPath)
+    diff = repo.git.diff()
     #Check if the expected lines are present in the output
     expected_lines = [
         "diff --git a/PowerShell/JumpCloud Commands Gallery/commands.json b/PowerShell/JumpCloud Commands Gallery/commands.json",
