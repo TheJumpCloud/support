2f3351e227fcee70fd1c258da9902a2f1c827ec8
Joe Workman <joe.workman@jumpcloud.com>
2023-10-11 12:38:44 -0600

    test without import

--- .github/scripts/tests/test_build_commands_gallery.py
+++ .github/scripts/tests/test_build_commands_gallery.py
@@ -1,38 +1,32 @@
-from ..build_commands_gallery import parse_commands_to_json, set_links, validate_commands_galleryMD
+
+import subprocess
 import os
 import git
 from os.path import dirname
 def test_script_functions():
+    # get script path
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+    # get parent directory
+    scriptRootPath = dirname(scriptPath)
+    # get the 'build_commands_gallery' file
+    build_file =os.path.join(scriptRootPath, "build_commands_gallery.py")
     try:
-        parse_commands_to_json()
-        set_links()
-        validate_commands_galleryMD()
-    except Exception as e:
-        assert False, f"An exception occurred: {e}"
+        result = subprocess.run(['python3', build_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        assert result.returncode == 0, f"Script exited with an error code: {result.returncode}"
+    except FileNotFoundError:
+        assert False, f"Script file '{build_file}' not found"
+    assert not result.stderr, f"Build script produced an error:\n{result.stderr}"
 
-#subprocess.check_output(['git', 'diff', '--name-only', currentBranch + '..' + master])
 def test_diff():
-    # try:
-    #     base_ref = os.environ.get('github.base.ref')
-    #     head_ref = os.environ.get('github.head_ref')
-
-    #     print(base_ref)
-    #     print(head_ref)
-    #     diff = subprocess.check_output(['git', 'diff', 'origin/master', 'sa-3606-Update-CommandsJSON-CI'], universal_newlines=True, stderr=subprocess.STDOUT)
-
-    # except subprocess.CalledProcessError as e:
-    #     print("Exception on process, rc=", e.returncode, "output=", e.output)
+    # get script path
     scriptPath = os.path.dirname(os.path.realpath(__file__))
     # rootPath is root of the directory
     rootPath = dirname(dirname(dirname(scriptPath)))
-
+    # get the commands.json file
     cmd_filepath = os.path.join(rootPath, "PowerShell/JumpCloud Commands Gallery/commands.json")
-    print(cmd_filepath)
+    # set repo path:
     repo = git.Repo(rootPath)
     # Get current origin branch
     # Do diff between remote and current branch
     diff = repo.git.diff(cmd_filepath)
-
-
-
     assert '' == diff, "Changes detected in commands gallery. Please run `python .github/scripts/build_commands_gallery.py` locally and commit the changes."
