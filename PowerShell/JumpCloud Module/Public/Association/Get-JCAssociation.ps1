85fa0abc128673ec79f489754eb23a76bafc3b8e
Ken Maranion <ken.maranion@jumpcloud.com>
2023-10-17 06:58:19 -0700

    fix association bug

--- PowerShell/JumpCloud Module/Public/Association/Get-JCAssociation.ps1
+++ PowerShell/JumpCloud Module/Public/Association/Get-JCAssociation.ps1
@@ -9,7 +9,9 @@ Function Get-JCAssociation {
         $RuntimeParameterDictionary = If ($Type) {
             Get-DynamicParamAssociation -Action:($Action) -Force:($Force) -Type:($Type)
         } Else {
+            Write-Debug "Type is not set in PARAM block"
             Get-DynamicParamAssociation -Action:($Action) -Force:($Force)
+            $isDynamicPAram = $true
         }
         Return $RuntimeParameterDictionary
     }
@@ -27,6 +29,17 @@ Function Get-JCAssociation {
         # Add input parameters from function in to hash table and filter out unnecessary parameters
         $PSBoundParameters.GetEnumerator() | Where-Object { -not [System.String]::IsNullOrEmpty($_.Value) } | ForEach-Object { $FunctionParameters.Add($_.Key, $_.Value) | Out-Null }
         # Add action
+        # Check if the type is a manual type or a dynamic type
+        if ($isDynamicPAram) {
+            Write-Debug "Type is not set in PARAM block"
+            # Set the type to the dynamic type in functionParameters
+            $getDynamicType = Get-JCType -Type $type
+            Write-Debug "Manual Type: $($getDynamicType.targets.targetsingular)"
+            $FunctionParameters['TargetType'] = $getDynamicType.targets.targetsingular
+        } else {
+            Write-Debug "Type is set in PARAM block: $Type"
+        }
+
         ($FunctionParameters).Add('Action', $Action) | Out-Null
         # Run the command
         $Results += Invoke-JCAssociation @FunctionParameters
