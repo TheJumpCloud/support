6b5d96a2d2033d720b9127d2e06c458048f350b3
Joe Workman <joe.workman@jumpcloud.com>
2023-10-31 12:53:45 -0600

    at least one user should be bound to the test system

--- PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Get-JCSystemUser.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/SystemUsers/Get-JCSystemUser.Tests.ps1
@@ -7,6 +7,6 @@ Describe -Tag:('JCSystemUser') 'Get-JCSystemUser 1.0' {
     }
     It "Gets JumpCloud system users for a system using SystemID" {
         $SystemUsers = Get-JCSystemUser -SystemID $PesterParams_SystemLinux._id
-        $SystemUsers.username.Count | Should -BeGreaterThan 1
+        $SystemUsers.username.Count | Should -BeGreaterOrEqual 1
     }
 }
