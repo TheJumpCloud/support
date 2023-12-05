08bf42a5da6d221c9df754ccd33bdea35ffafb43
Ken Maranion <ken.maranion@jumpcloud.com>
2023-12-05 11:33:09 -0800

    build module helpfiles, pestertestfiles, changelog versioning

--- PowerShell/Deploy/Build-Module.ps1
+++ PowerShell/Deploy/Build-Module.ps1
@@ -19,7 +19,7 @@ param (
     [Boolean]
     $ManualModuleVersion
 )
-. "$PSScriptRoot/Get-Config.ps1" -GitSourceBranch:($GitSourceBranch) -GitSourceRepo:($GitSourceRepo) -ReleaseType:($ReleaseType) -RequiredModulesRepo:($RequiredModulesRepo)
+. "$PSScriptRoot/Get-Config.ps1"
 # Region Checking PowerShell Gallery module version
 Write-Host ('[status]Check PowerShell Gallery for module version info')
 $PSGalleryInfo = Get-PSGalleryModuleVersion -Name:($ModuleName) -ReleaseType:($RELEASETYPE) #('Major', 'Minor', 'Patch')
@@ -34,6 +34,38 @@ if ($ManualModuleVersion) {
 }
 Write-Host ('[status]PowerShell Gallery Name:' + $PSGalleryInfo.Name + ';CurrentVersion:' + $PSGalleryInfo.Version + '; NextVersion:' + $ModuleVersion )
 # EndRegion Checking PowerShell Gallery module version
+
+## Get the module version from the psd1 file and changelog
+$VersionPsd1Regex = [regex]"(?<=ModuleVersion\s*=\s*')(([0-9]+)\.([0-9]+)\.([0-9]+))"
+$VersionMatchPsd1 = Select-String -Path:($FilePath_psd1) -Pattern:($VersionPsd1Regex)
+$PSD1Version = $VersionMatchPsd1.Matches.Value
+
+
+$FilePath_ModuleChangelog = Join-Path -Path $PSScriptRoot -ChildPath '..\ModuleChangelog.md'
+$ModuleChangelog = Get-Content -Path:($FilePath_ModuleChangelog)
+$ModuleChangelogVersionRegex = "([0-9]+)\.([0-9]+)\.([0-9]+)"
+$ModuleChangelogVersionMatch = ($ModuleChangelog | Select-Object -First 1) | Select-String -Pattern:($ModuleChangelogVersionRegex)
+$ModuleChangelogVersion = $ModuleChangelogVersionMatch.Matches.Value
+
+# Update ModuleChangelog.md File:
+If ($ModuleChangelogVersion -ne $PSD1Version) {
+    # add a new version section to the module ModuleChangelog.md
+    Write-Host "[Status]: Appending new changelog for version: $PSD1Version"
+    $NewModuleChangelogRecord = New-ModuleChangelog -LatestVersion:($PSD1Version) -ReleaseNotes:('{{Fill in the Release Notes}}') -Features:('{{Fill in the Features}}') -Improvements:('{{Fill in the Improvements}}') -BugFixes('{{Fill in the Bug Fixes}}')
+
+    ($NewModuleChangelogRecord + ($ModuleChangelog | Out-String)).Trim() | Set-Content -Path:($FilePath_ModuleChangelog) -Force
+} else {
+    # Get content between latest version and last
+    $ModuleChangelogContent = Get-Content -Path:($FilePath_ModuleChangelog) | Select -First 3
+    $ReleaseDateRegex = [regex]'(?<=Release Date:\s)(.*)'
+    $ReleaseDateRegexMatch = $ModuleChangelogContent | Select-String -Pattern $ReleaseDateRegex
+    $ReleaseDate = $ReleaseDateRegexMatch.Matches.Value
+    $todaysDate = $(Get-Date -UFormat:('%B %d, %Y'))
+    if (($ReleaseDate) -and ($ReleaseDate -ne $todaysDate)) {
+        write-host "[Status] Updating Changelog date: $ReleaseDate to: $todaysDate)"
+        $ModuleChangelog.Replace($ReleaseDate, $todaysDate) | Set-Content $FilePath_ModuleChangelog
+    }
+}
 # Region Building New-JCModuleManifest
 Write-Host ('[status]Building New-JCModuleManifest')
 New-JCModuleManifest -Path:($FilePath_psd1) `
@@ -49,3 +81,12 @@ If (!(($ModuleChangelog | Select-Object -First 1) -match $ModuleVersion)) {
     ($NewModuleChangelogRecord + ($ModuleChangelog | Out-String)).Trim() | Set-Content -Path:($FilePath_ModuleChangelog) -Force
 }
 # EndRegion Updating module change log
+
+Write-Host "Building help files" -ForegroundColor Green
+."$PSScriptRoot/Build-HelpFiles.ps1" -ModuleName "JumpCloud" -ModulePath "./PowerShell/JumpCloud Module"
+
+Write-Host "Building Pester test files" -ForegroundColor Green
+."$PSScriptRoot/Build-PesterTestFiles.ps1"
+
+Write-Host "Building module" -ForegroundColor Green
+."$PSScriptRoot/SdkSync/jcapiToSupportSync.ps1"
