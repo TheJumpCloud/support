a3cee807e60bbb1d24036b376a75a144cd999d19
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-01-09 14:08:15 -0700

    remove o365 tests

---
+++ PowerShell/JumpCloud Module/Tests/Public/Directory/Remove-JCOffice365Member.Tests.ps1
@@ -0,0 +1,60 @@
+Describe -Tag:('JCCloudDirectory') 'Remove-JCOffice365Member' {
+    BeforeAll {
+        $Directories = Get-JCCloudDirectory -Type office_365
+
+        $NewUser = New-RandomUser -domain "delCloudDirUser.$(New-RandomString -NumberOfChars 5)" | New-JCUser
+        $NewGroup = New-JCUserGroup -GroupName 'CloudDirTest'
+    }
+    It 'Removes a user by username with directory id' {
+        $User = Remove-JCOffice365Member -Id $Directories.Id -Username $NewUser.username
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.UserID | Should -Be $NewUser.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a user by username with directory name' {
+        $User = Remove-JCOffice365Member -Name $Directories.Name -Username $NewUser.username
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.UserID | Should -Be $NewUser.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a user by userID with directory id' {
+        $User = Remove-JCOffice365Member -Id $Directories.Id -UserID $NewUser.Id
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.UserID | Should -Be $NewUser.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a user by userID with directory name' {
+        $User = Remove-JCOffice365Member -Id $Directories.Name -Username $NewUser.Id
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.UserID | Should -Be $NewUser.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a userGroup by Name with directory id' {
+        $User = Remove-JCOffice365Member -Id $Directories.Id -GroupName $NewGroup.Name
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.GroupID | Should -Be $NewGroup.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a userGroup by Name with directory Name' {
+        $User = Remove-JCOffice365Member -Id $Directories.Name -GroupName $NewGroup.Name
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.GroupID | Should -Be $NewGroup.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a userGroup by ID with directory id' {
+        $User = Remove-JCOffice365Member -Id $Directories.Id -GroupName $NewGroup.Id
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.GroupID | Should -Be $NewGroup.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    It 'Removes a userGroup by ID with directory Name' {
+        $User = Remove-JCOffice365Member -Id $Directories.Name -GroupName $NewGroup.Id
+        $User.DirectoryName | Should -Be $Directories.Name
+        $User.GroupID | Should -Be $NewGroup.ID
+        $User.Status | Should -Be 'Removed'
+    }
+    AfterAll {
+        Remove-JCUser -UserID $NewUser.Id -force
+        Remove-JCUserGroup -GroupID $NewGroup.Id -force
+    }
+}
