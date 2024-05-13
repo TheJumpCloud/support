d1c59a45d8478d770371ad094ed058524d7eecb4
Joe Workman <joe.workman@jumpcloud.com>
2024-05-13 08:11:36 -0600

    entityID parsing and validation

--- PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
+++ PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
@@ -8,18 +8,35 @@ function Get-MetadataFromXML {
         [ValidatePattern( '\.xml$' )]
         [string]$XMLFilePath
     )
+    begin {
 
-    [xml]$XMLInfo = Get-Content $XMLFilePath
-
-    $certificate = $XMLInfo.EntityDescriptor.IDPSSODescriptor.KeyDescriptor.KeyInfo.X509Data.X509Certificate
-    $domain = $XMLInfo.EntityDescriptor.entityID
-    $IDPUrl = $XMLInfo.EntityDescriptor.IDPSSODescriptor.SingleSignOnService.location[0]
-
-    $MetaData = [PSCustomObject]@{
-        Certificate = $certificate
-        Domain      = $domain
-        IDPUrl      = $IDPUrl
+        [xml]$XMLInfo = Get-Content $XMLFilePath
     }
+    process {
 
-    Return $MetaData
+        $certificate = $XMLInfo.EntityDescriptor.IDPSSODescriptor.KeyDescriptor.KeyInfo.X509Data.X509Certificate
+        # domain should be extracted from entityID
+        $domainMatches = $XMLInfo.EntityDescriptor.entityID | Select-String -Pattern '(https:\/\/|urn:uri:)(=?.*)'
+        if (($domainMatches.Matches.Groups[0].Value -match "https://") -or ($domainMatches.Matches.Groups[0].Value -match "urn:rui:")) {
+            # entity ID should match https: or urn:uri:
+            $entityID = $domainMatches.Matches.Groups[0].Value
+        }
+        # domain should be the second group match from the $domainMatches variable
+        $domain = $domainMatches.Matches.Groups[2].Value
+
+        $IDPUrl = $XMLInfo.EntityDescriptor.IDPSSODescriptor.SingleSignOnService.location[0]
+
+        $MetaData = [PSCustomObject]@{
+            Certificate = $certificate
+            Domain      = $domain
+            EntityID    = $entityID
+            IDPUrl      = $IDPUrl
+        }
+
+    }
+    end {
+
+        Return $MetaData
+    }
 }
+Get-MetadataFromXML -XMLFilePath "/Users/jworkman/Downloads/JumpCloud-office365-metadata (3).xml"
