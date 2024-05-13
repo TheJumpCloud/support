02d02766138ad1c42009557c778e8b2d50fc497d
Joe Workman <joe.workman@jumpcloud.com>
2024-05-13 09:58:08 -0600

    domain messaging

--- PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
+++ PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
@@ -17,12 +17,18 @@ function Get-MetadataFromXML {
         $certificate = $XMLInfo.EntityDescriptor.IDPSSODescriptor.KeyDescriptor.KeyInfo.X509Data.X509Certificate
         # domain should be extracted from entityID
         $domainMatches = $XMLInfo.EntityDescriptor.entityID | Select-String -Pattern '(https:\/\/|urn:uri:)(=?.*)'
-        if (($domainMatches.Matches.Groups[0].Value -match "https://") -or ($domainMatches.Matches.Groups[0].Value -match "urn:rui:")) {
-            # entity ID should match https: or urn:uri:
-            $entityID = $domainMatches.Matches.Groups[0].Value
+        if ($domainMatches) {
+            if (($domainMatches.Matches.Groups[0].Value -match "https://") -or ($domainMatches.Matches.Groups[0].Value -match "urn:rui:")) {
+                # entity ID should match https: or urn:uri:
+                $entityID = $domainMatches.Matches.Groups[0].Value
+            }
+            # domain should be the second group match from the $domainMatches variable
+            $domain = $domainMatches.Matches.Groups[2].Value
+
+        } else {
+            throw "The supplied EntityID: $($XMLInfo.EntityDescriptor.entityID) does not appear to be correct. The domain name may be missing 'https://' or 'urn:uri:' as a prefix to the domain name. EX: within the JumpCloud SSO application for O365, supply an EntityID value such as 'https://myDomain.com' or 'urn:uri:myDomain.com'"
         }
-        # domain should be the second group match from the $domainMatches variable
-        $domain = $domainMatches.Matches.Groups[2].Value
+
 
         $IDPUrl = $XMLInfo.EntityDescriptor.IDPSSODescriptor.SingleSignOnService.location[0]
 
@@ -39,4 +45,3 @@ function Get-MetadataFromXML {
         Return $MetaData
     }
 }
-Get-MetadataFromXML -XMLFilePath "/Users/jworkman/Downloads/JumpCloud-office365-metadata (3).xml"
