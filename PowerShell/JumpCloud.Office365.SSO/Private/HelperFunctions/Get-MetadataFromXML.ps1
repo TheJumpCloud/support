b0cc1a40a9a0aad62316eba2b1db66c7ccd8ce1c
Joe Workman <joe.workman@jumpcloud.com>
2024-05-16 08:54:02 -0600

    date and regex match

--- PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
+++ PowerShell/JumpCloud.Office365.SSO/Private/HelperFunctions/Get-MetadataFromXML.ps1
@@ -18,9 +18,11 @@ function Get-MetadataFromXML {
         # domain should be extracted from entityID
         $domainMatches = $XMLInfo.EntityDescriptor.entityID | Select-String -Pattern '(https:\/\/|urn:uri:)(=?.*)'
         if ($domainMatches) {
-            if (($domainMatches.Matches.Groups[0].Value -match "https://") -or ($domainMatches.Matches.Groups[0].Value -match "urn:rui:")) {
+            if (($domainMatches.Matches.Groups[0].Value -match "https://") -or ($domainMatches.Matches.Groups[0].Value -match "urn:uri:")) {
                 # entity ID should match https: or urn:uri:
                 $entityID = $domainMatches.Matches.Groups[0].Value
+            } else {
+                throw "The supplied EntityID: $($XMLInfo.EntityDescriptor.entityID) does not appear to be correct. The domain name may be missing 'https://' or 'urn:uri:' as a prefix to the domain name. EX: within the JumpCloud SSO application for O365, supply an EntityID value such as 'https://myDomain.com' or 'urn:uri:myDomain.com'"
             }
             # domain should be the second group match from the $domainMatches variable
             $domain = $domainMatches.Matches.Groups[2].Value
