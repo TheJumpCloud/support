ceeb6755ed204f3ab073b638c3bf669628767e48
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-06-11 12:30:10 -0600

    Get-JCScheduledUserstate tests

---
+++ PowerShell/JumpCloud Module/Tests/Public/Users/Get-JCScheduledUserstate.ps1
@@ -0,0 +1,48 @@
+Describe -Tag:('JCScheduledUserstate') 'Get-JCScheduledUserstate' {
+    BeforeAll {
+        # Create a few users to test suspension and activation
+        $NewStateUser1 = New-RandomUser -domain "delNewUser.$(New-RandomString -NumberOfChars 5)" | New-JCUser
+        $NewStateUser2 = New-RandomUser -domain "delNewUser.$(New-RandomString -NumberOfChars 5)" | New-JCUser
+        $NewStateUser3 = New-RandomUser -domain "delNewUser.$(New-RandomString -NumberOfChars 5)" | New-JCUser -state "SUSPENDED"
+
+        $ScheduledSuspension = New-JcSdkBulkUserState -UserIds "$($NewStateUser1.Id), $($NewStateUser2.Id)" -State 'SUSPENDED' -StartDate (Get-Date -Hour 12 -Minute 0 -Second 0).AddDays(1)
+        $ScheduledActivation = New-JcSdkBulkUserState -UserIds $NewStateUser3.Id -State 'ACTIVATED' -StartDate (Get-Date -Hour 12 -Minute 0 -Second 0).AddDays(1)
+    }
+    It "Gets scheduled SUSPENDED users" {
+        $scheduledUsers = Get-JcScheduledSuspension -State "SUSPENDED"
+        $scheduledUsers.count | Should -Be 2
+
+        $scheduledUsers.id | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.Firstname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Lastname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Email | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Username | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.ScheduledDate | Should -Be (Get-Date -Hour 12 -Minute 0 -Second 0).AddDays(1)
+    }
+    It "Gets scheduled ACTIVATED users" {
+        $scheduledUsers = Get-JcScheduledSuspension -State "ACTIVATED"
+        $scheduledUsers.count | Should -Be 1
+
+        $scheduledUsers.id | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.Firstname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Lastname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Email | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Username | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.ScheduledDate | Should -Be (Get-Date -Hour 12 -Minute 0 -Second 0).AddDays(1)
+    }
+    It "Gets scheduled user by ID" {
+        $scheduledUsers = Get-JcScheduledSuspension -UserID $NewStateUser1.Id
+
+        $scheduledUsers.id | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.Firstname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Lastname | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Email | Should -Not -BeNullOrEmpty
+        $scheduledUsers.Username | Should -Not -BeNullOrEmpty
+        $scheuldedUsers.ScheduledDate | Should -Be (Get-Date -Hour 12 -Minute 0 -Second 0).AddDays(1)
+    }
+    AfterAll {
+        Remove-JCUser -UserID $NewStateUser1.Id -ByID -force
+        Remove-JCUser -UserID $NewStateUser2.Id -ByID -force
+        Remove-JCUser -UserID $NewStateUser3.Id -ByID -force
+    }
+}
