159e557677da79f4ed9a7bda4028779a0f4df51c
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-06-13 10:28:50 -0600

    generate commandresults per test

--- PowerShell/JumpCloud Module/Tests/Public/Commands/CommandResults/Remove-JCCommandResult.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Commands/CommandResults/Remove-JCCommandResult.Tests.ps1
@@ -8,12 +8,19 @@ Describe -Tag:('JCCommandResult') 'Remove-JCCommandResult 1.0' {
     #>
 
     It "Removes a JumpCloud command result using -CommandResultID with the force parameter" {
+        # Invoke Command to generate result
+        $SingleTrigger = Get-JCCommand | Where-Object trigger -NotLike '' | Select-Object -Last 1 | Select-Object trigger
+        $SingleResult = Invoke-JCCommand -trigger $SingleTrigger.trigger
+
         $SingleCommandResult = Get-JCCommandResult | Select-Object -Last 1
         $DeletedResult = Remove-JCCommandResult -CommandResultID $SingleCommandResult._id -force
         $DeletedResult._id.count | Should -Be 1
     }
 
     It "Removes a JumpCloud command result passed through the pipeline with the force parameter without declaring -CommandResultID" {
+        # Invoke Command to generate result
+        $SingleTrigger = Get-JCCommand | Where-Object trigger -NotLike '' | Select-Object -Last 1 | Select-Object trigger
+        $SingleResult = Invoke-JCCommand -trigger $SingleTrigger.trigger
 
         $DeletedResult = Get-JCCommandResult | Select-Object -Last 1 | Remove-JCCommandResult -force
         $DeletedResult._id.count | Should -Be 1
@@ -21,6 +28,11 @@ Describe -Tag:('JCCommandResult') 'Remove-JCCommandResult 1.0' {
     }
 
     It "Removes two JumpCloud command results passed through the pipeline with force parameter without declaring -CommandResultID" {
+        for ($i = 0; $i -lt 2; $i++) {
+            # Invoke Command to generate result
+            $SingleTrigger = Get-JCCommand | Where-Object trigger -NotLike '' | Select-Object -Last 1 | Select-Object trigger
+            $SingleResult = Invoke-JCCommand -trigger $SingleTrigger.trigger
+        }
         $DeletedResult = Get-JCCommandResult | Select-Object -Last 2 | Remove-JCCommandResult -force
         $DeletedResult._id.count | Should -Be 2
 
