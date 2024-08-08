f017fab9a3f60b8d23e0da43c19911df34667d6c
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-08-08 10:03:13 -0600

    Get-JCAdmin tests

---
+++ PowerShell/JumpCloud Module/Tests/Public/Administrators/Get-JCAdmin.Tests.ps1
@@ -0,0 +1,32 @@
+Describe -Tag:('JCAdmin') 'Get-JCAdmin Tests' {
+    Context 'Get-JCAdmin Tests' {
+        It ('Get all administrators') {
+            $Admins = Get-JCAdmin
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get a specific administrator by email address') {
+            $Admins = Get-JCAdmin -email 'solutions-architecture+pestermtp@jumpcloud.com'
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get a specific administrator by email address with wildcard') {
+            $Admins = Get-JCAdmin -email 'solutions-architecture*'
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get administrators by enableMultifactor') {
+            $Admins = Get-JCAdmin -enableMultifactor $true
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get administrators by totpEnrolled') {
+            $Admins = Get-JCAdmin -totpEnrolled $true
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get administrators by roleName') {
+            $Admins = Get-JCAdmin -roleName 'Administrator With Billing'
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+        It ('Get administrators by email, enableMultifactor, totpEnrolled, roleName and organization') {
+            $Admins = Get-JCAdmin -email 'solutions-architecture*' -enableMultifactor $true -totpEnrolled $true -roleName 'Administrator With Billing' -organization $orgs[0].OrgID
+            $Admins | Should -Not -BeNullOrEmpty
+        }
+    }
+}
