17e05b76ee3209744a62f3dbbe8bf9e6c4783366
Joe Workman <joe.workman@jumpcloud.com>
2024-08-26 16:40:56 -0600

    cleanup certHash.json

--- scripts/automation/Radius/Functions/Private/CertJson/Get-CertHash.ps1
+++ scripts/automation/Radius/Functions/Private/CertJson/Get-CertHash.ps1
@@ -7,19 +7,7 @@ function Get-CertHash {
         $resultsArrayP = [System.Collections.Concurrent.ConcurrentDictionary[string, object]]::new()
     }
     process {
-        # $allCerts | Foreach-Object -ThrottleLimit 5 -Parallel {
-        #     #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
-        #     $resultsArrayP = $using:resultsArrayP
-        #     # . $using:JCScriptRoot/Functions/Private/CertResults/Get-CertBySHA.ps1
-        #     ${function:Get-CertBySHA} = $using:shaFuncDef
-        #     ${function:New-DeploymentTable} = $using:deploymentTableDef
-        #     $item = get-CertBySHA -sha1 $PSItem.sha1
-        #     if ($item) {
-        #         $resultsArrayP.AddOrUpdate("$($PSItem.sha1)", $item, { param($key, $oldValue) $item }) | Out-Null
-        #     }
-        # }
         $allCerts | Foreach-Object -ThrottleLimit 5 -Parallel {
-            #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
             $resultsArrayP = $using:resultsArrayP
             ${function:Get-CertBySHA} = $using:shaFuncDef
             ${function:New-DeploymentTable} = $using:deploymentTableDef
