155542f13949fb848e77d2178cd44a993869b547
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-12-17 13:32:38 -0700

    null value check test

--- PowerShell/JumpCloud Module/Tests/Public/Utilities/CSV_Import/Update-JCDeviceFromCSV.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Utilities/CSV_Import/Update-JCDeviceFromCSV.Tests.ps1
@@ -25,4 +25,29 @@ Describe -Tag:('JCDeviceFromCSV') 'Update-JCDeviceFromCSV' {
         $UpdatedDevice.allowPublicKeyAuthentication | Should -Be $CSVData.allowPublicKeyAuthentication
         $UpdatedDevice.systemInsights | Should -Be '@{state=enabled}'
     }
+    It 'Updates users from a CSV populated with a null value' {
+        $system = Get-JCSystem | Select-Object -First 1
+        $CSVData = @{
+            "DeviceID"                       = $system.id
+            "displayName"                    = ""
+            "description"                    = ""
+            "allowSshPasswordAuthentication" = $true
+            "allowSshRootLogin"              = $true
+            "allowMultiFactorAuthentication" = $false
+            "allowPublicKeyAuthentication"   = $true
+            "systemInsights"                 = $true
+        }
+        $CSVDATA | Export-Csv "$PesterParams_ImportPath/UpdateDeviceFromCSV.csv" -Force
+        $DeviceCSVUpdate = Update-JCDeviceFromCSV -CSVFilePath "$PesterParams_ImportPath/UpdateDeviceFromCSV.csv" -force
+
+        $UpdatedDevice = Get-JCSystem -SystemID $system.id
+
+        $UpdatedDevice.displayName | Should -Be $system.displayName
+        $UpdatedDevice.description | Should -Be $system.description
+        $UpdatedDevice.allowSshPasswordAuthentication | Should -Be $CSVData.allowSshPasswordAuthentication
+        $UpdatedDevice.allowSshRootLogin | Should -Be $CSVData.allowSshRootLogin
+        $UpdatedDevice.allowMultiFactorAuthentication | Should -Be $CSVData.allowMultiFactorAuthentication
+        $UpdatedDevice.allowPublicKeyAuthentication | Should -Be $CSVData.allowPublicKeyAuthentication
+        $UpdatedDevice.systemInsights | Should -Be '@{state=enabled}'
+    }
 }
