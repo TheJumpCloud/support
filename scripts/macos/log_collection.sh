94c33fc3063f92ca434db0af8e5815e74d1c34b4
Ludovic-Emo-Pyl-Tech <148278277+Ludovic-Emo-Pyl-Tech@users.noreply.github.com>
2025-03-20 08:14:11 +0100

    Update log_collection.sh - upgrade script patch number

--- scripts/macos/log_collection.sh
+++ scripts/macos/log_collection.sh
@@ -11,7 +11,7 @@ days=2           # number of days of OS logs to gather
 # do not edit below
 #######
 
-version=1.2.1
+version=1.2.2
 
 ## verify script is running as root.
 if [ $(/usr/bin/id -u) -ne 0 ]
