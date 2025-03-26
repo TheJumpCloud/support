677d499643589a9c73229d7e90c92680340a40e2
Joe Workman <joe.workman@jumpcloud.com>
2025-03-26 14:31:08 -0600

    first pass on update module function

---
+++ scripts/automation/Radius/Functions/Public/Module/Update-JCRModule.ps1
@@ -0,0 +1,88 @@
+function Update-JCRModule {
+    [CmdletBinding()]
+    param (
+        [Parameter(HelpMessage = 'ByPasses user prompts.')][Switch]$Force
+
+    )
+    begin {
+        # JumpCloud Module Name
+        $ModuleName = 'JumpCloud.Radius'
+        # for testing allow swtich to change repo
+        $localPSRepo = $true
+        $repo = if ($localPSRepo) {
+            "localPSRepo"
+        } else {
+            "PSGallery"
+        }
+    }
+    process {
+        # get the latest module
+        $latestModule = Find-Module -Name $ModuleName -Repository $repo
+        # get the currently installed module
+        $currentModule = Get-InstalledModule -Name $ModuleName
+
+        # compare the versions
+        if ($latestModule.Version -gt $currentModule.Version) {
+            Write-Host "A new version of the $ModuleName module is available: $($latestModule.Version) (current version: $($currentModule.Version))."
+            Write-Host "You can update the module by running: Update-Module -Name $ModuleName"
+        } else {
+            Write-Host "You have the latest version of the $ModuleName module: $($currentModule.Version)."
+        }
+
+        # prompts to update if force param is not set
+        if (-not $Force) {
+            Do {
+                Write-Host ('Enter ''Y'' to update the ' + $ModuleName + ' PowerShell module to the latest version or enter ''N'' to cancel:')
+                Write-Host (' ') -NoNewline
+                $UserInput = Read-Host
+            }
+            Until ($UserInput.ToUpper() -in ('Y', 'N'))
+        }
+        Else {
+            $UserInput = 'Y'
+        }
+
+        # if the user pressed "N" then exit
+        if ($UserInput.ToUpper() -eq 'N') {
+            Write-Host "Update cancelled."
+            continue
+        } else {
+            # else get the module config from the current module:
+            # TODO: get the json config with the private function not created yet
+
+            # now, attempt to update the module
+            try {
+                Update-Module -Name $ModuleName
+            } catch {
+                Write-Error "Failed to update the module: $_"
+            }
+
+            # Get the installed module versions
+            $installedModules = Get-InstalledModule -Name $moduleName -AllVersions
+
+            # Remove the modules from the current session
+            Get-Module -Name:($ModuleName) -ListAvailable -All | Remove-Module -Force
+
+            # uninstall the older versions
+            foreach ($module in $installedModules) {
+                if ($module.Version -ne $latestModule.Version) {
+                    Uninstall-Module -Name $ModuleName -RequiredVersion $module.Version -Force
+                }
+            }
+
+            # now validate the modules
+            $currentModule = Get-InstalledModule -Name $ModuleName
+            if ($currentModule.Version -eq $latestModule.Version) {
+                Write-Host "The $ModuleName module has been updated to version $($currentModule.Version)."
+            } else {
+                Write-Error "Failed to update the module to the latest version."
+            }
+            Import-module -Name $ModuleName -Force
+
+        }
+    }
+    end {
+
+    }
+
+}
