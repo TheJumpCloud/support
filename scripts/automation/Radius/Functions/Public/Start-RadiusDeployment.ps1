1dcd26e2648d595c5f128c809c94db66de83f86d
Joe Workman <joe.workman@jumpcloud.com>
2025-03-26 15:06:00 -0600

    create public functions

--- scripts/automation/Radius/Functions/Public/Start-RadiusDeployment.ps1
+++ scripts/automation/Radius/Functions/Public/Start-RadiusDeployment.ps1
@@ -1,45 +1,48 @@
-# Import Global Config:
-Write-Verbose 'Verifying JCAPI Key'
-if ($JCAPIKEY.length -ne 40) {
-    Connect-JCOnline -force
-}
-. "$psscriptroot/Config.ps1"
+function Start-RadiusDeployment {
 
-################################################################################
-# Do not modify below
-################################################################################
-# set script root
-#TODO: move to global functions
-$global:JCScriptRoot = $PSScriptRoot
-
-# Import the functions
-Import-Module "$JCScriptRoot/JumpCloud.Radius.psm1" -DisableNameChecking -Force
-
-# Check for Module Updates
-Update-JCRModule
-
-# Show user selection
-do {
-    #Output-Certs
-    Show-RadiusMainMenu
-    $selection = Read-Host "Please make a selection"
-    switch ($selection) {
-        '1' {
-            Start-GenerateRootCert
-        } '2' {
-            Start-GenerateUserCerts
-        } '3' {
-            Start-DeployUserCerts
-        } '4' {
-            Start-MonitorCertDeployment
-        } '5' {
-            Get-JCRGlobalVars -force
-        } '8' {
-            Get-JCRGlobalVars -force -associateManually
-        } '9' {
-            $theUser = Read-Host "Enter the username of the user to manually update their association data"
-            Get-JCRGlobalVars -force -associationUsername $theUser
-        }
+    # Import Global Config:
+    Write-Verbose 'Verifying JCAPI Key'
+    if ($JCAPIKEY.length -ne 40) {
+        Connect-JCOnline -force
     }
-    Pause
-} until ($selection.ToUpper() -eq 'Q')
+    . "$psscriptroot/Config.ps1"
+
+    ################################################################################
+    # Do not modify below
+    ################################################################################
+    # set script root
+    #TODO: move to global functions
+    # $global:JCScriptRoot = $PSScriptRoot
+
+    # Import the functions
+    # Import-Module "$JCScriptRoot/JumpCloud.Radius.psm1" -DisableNameChecking -Force
+
+    # Check for Module Updates
+    # Update-JCRModule
+
+    # Show user selection
+    do {
+        #Output-Certs
+        Show-RadiusMainMenu
+        $selection = Read-Host "Please make a selection"
+        switch ($selection) {
+            '1' {
+                Start-GenerateRootCert
+            } '2' {
+                Start-GenerateUserCerts
+            } '3' {
+                Start-DeployUserCerts
+            } '4' {
+                Start-MonitorCertDeployment
+            } '5' {
+                Get-JCRGlobalVars -force
+            } '8' {
+                Get-JCRGlobalVars -force -associateManually
+            } '9' {
+                $theUser = Read-Host "Enter the username of the user to manually update their association data"
+                Get-JCRGlobalVars -force -associationUsername $theUser
+            }
+        }
+        Pause
+    } until ($selection.ToUpper() -eq 'Q')
+}
