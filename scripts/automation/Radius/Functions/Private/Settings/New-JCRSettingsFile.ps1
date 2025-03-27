94ab9790496de1da625745fab97272c1c047f082
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2025-03-27 11:04:53 -0600

    new default config file

--- scripts/automation/Radius/Functions/Private/Settings/New-JCRSettingsFile.ps1
+++ scripts/automation/Radius/Functions/Private/Settings/New-JCRSettingsFile.ps1
@@ -11,21 +11,33 @@ function New-JCRSettingsFile {
     begin {
         # Config should be in /PowerShell/JumpCloudModule/Config.json
         $ModuleRoot = (Get-Item -Path:($global:JCScriptRoot))
-        $configFilePath = join-path -path $ModuleRoot -childpath 'settings.json'
-
+        $configFilePath = Join-Path -Path $ModuleRoot -ChildPath 'config.json'
+        $date = (Get-Date).ToUniversalTime()
     }
     process {
         # Define Default Settings for the Config file
-        $date = (Get-Date).ToUniversalTime()
         $config = @{
-            'globalVars' = @{
-                'lastUpdate' = @{value = $date; write = $true; copy = $true ;
-                }
+            'userGroup'                 = @{value = $null; write = $true; copy = $true }
+            'certSecretPass'            = @{value = $null; write = $true; copy = $true }
+            'userCertValidityDays'      = @{value = 365; write = $true; copy = $true }
+            'caCertValidityDays'        = @{value = 1095; write = $true; copy = $true }
+            'certExpirationWarningDays' = @{value = 15; write = $true; copy = $true }
+            'networkSSID'               = @{value = $null; write = $true; copy = $true }
+            'certSubjectHeaders'        = @{
+                'countryCode'      = @{value = $null; write = $true; copy = $true }
+                'stateCode'        = @{value = $null; write = $true; copy = $true }
+                'Locality'         = @{value = $null; write = $true; copy = $true }
+                'Organization'     = @{value = $null; write = $true; copy = $true }
+                'OrganizationUnit' = @{value = $null; write = $true; copy = $true }
+                'CommonName'       = @{value = $null; write = $true; copy = $true }
             }
+            'certType'                  = @{value = $null; write = $true; copy = $true }
+            'radiusDirectory'           = @{value = $null; write = $true; copy = $true }
+            'lastUpdate'                = @{value = $date; write = $true; copy = $true ;}
         }
     }
     end {
-        if ((test-path -Path $configFilePath) -And ($force)) {
+        if ((Test-Path -Path $configFilePath) -And ($force)) {
             $config | ConvertTo-Json | Out-File -FilePath $configFilePath
         } else {
             $config | ConvertTo-Json | Out-File -FilePath $configFilePath
