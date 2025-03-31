aeb5522733ffd4f6475b119112cc862eba60e3b5
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2025-03-31 10:22:45 -0600

    add required property

--- scripts/automation/Radius/Functions/Private/Config/New-JCRConfigFile.ps1
+++ scripts/automation/Radius/Functions/Private/Config/New-JCRConfigFile.ps1
@@ -18,21 +18,21 @@ function New-JCRConfigFile {
         # Define Default Settings for the Config file
         $config = @{
             "globalVars" = @{
-                'userGroup'                         = @{value = $null; write = $true; copy = $true }
-                'certSecretPass'                    = @{value = $null; write = $true; copy = $true }
-                'userCertValidityDays'              = @{value = 365; write = $true; copy = $true }
-                'caCertValidityDays'                = @{value = 1095; write = $true; copy = $true }
-                'certExpirationWarningDays'         = @{value = 15; write = $true; copy = $true }
-                'networkSSID'                       = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderCountryCode'      = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderStateCode'        = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderLocality'         = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderOrganization'     = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderOrganizationUnit' = @{value = $null; write = $true; copy = $true }
-                'certSubjectHeaderCommonName'       = @{value = $null; write = $true; copy = $true }
-                'certType'                          = @{value = $null; write = $true; copy = $true }
-                'radiusDirectory'                   = @{value = $null; write = $true; copy = $true }
-                'lastUpdate'                        = @{value = $date; write = $false; copy = $true }
+                'userGroup'                         = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSecretPass'                    = @{value = $null; write = $true; copy = $true; required = $true }
+                'userCertValidityDays'              = @{value = 365; write = $true; copy = $true; required = $true }
+                'caCertValidityDays'                = @{value = 1095; write = $true; copy = $true; required = $true }
+                'certExpirationWarningDays'         = @{value = 15; write = $true; copy = $true; required = $true }
+                'networkSSID'                       = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderCountryCode'      = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderStateCode'        = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderLocality'         = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderOrganization'     = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderOrganizationUnit' = @{value = $null; write = $true; copy = $true; required = $true }
+                'certSubjectHeaderCommonName'       = @{value = $null; write = $true; copy = $true; required = $true }
+                'certType'                          = @{value = $null; write = $true; copy = $true; required = $true }
+                'radiusDirectory'                   = @{value = $null; write = $true; copy = $true; required = $true }
+                'lastUpdate'                        = @{value = $date; write = $false; copy = $true; required = $false }
             }
         }
     }
