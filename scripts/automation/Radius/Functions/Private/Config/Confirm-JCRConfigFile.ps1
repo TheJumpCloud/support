e9033482a0c65ae86d3d94eae40ec72769533d76
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2025-03-31 14:48:03 -0600

    Create Confirm-JCRConfigFile.ps1

---
+++ scripts/automation/Radius/Functions/Private/Config/Confirm-JCRConfigFile.ps1
@@ -0,0 +1,48 @@
+function Confirm-JCRConfigFile {
+    [CmdletBinding()]
+    param (
+    )
+    begin {
+        if ($JCAPIKEY.length -ne 40) {
+            Connect-JCOnline -force | Out-Null
+        }
+
+        # Config should be in /PowerShell/JumpCloudModule/Config.json
+        $ModuleRoot = (Get-Item -Path:($PSScriptRoot)).Parent.Parent.FullName
+        $configFilePath = Join-Path -Path $ModuleRoot -ChildPath 'Config.json'
+        $rawConfig = Get-Content -Path $configFilePath | ConvertFrom-Json
+
+        $requiredAttributesNotSet = @{}
+    }
+
+    process {
+        # validate config settings
+        foreach ($item in $rawConfig.globalVars) {
+            foreach ($property in $item.PSObject.Properties) {
+                # check to see if the key is required and if the value is null
+                if ($property.Value.required -eq $true -and $property.Value.value -eq $null) {
+                    $requiredAttributesNotSet += @{ $property.Name = $property.value.placeholder }
+                }
+            }
+        }
+    }
+    end {
+        if ($requiredAttributesNotSet) {
+            $requiredAttributesNotSet = $requiredAttributesNotSet | Sort-Object
+            $requiredAttributesNotSetString = $requiredAttributesNotSet.Keys -join ","
+            Write-Warning @"
+There are required settings for this module that have not yet been set with the Set-JCRConfigFile function.
+The module requires you set: $requiredAttributesNotSetString
+To set these run the following command (changing the default settings for your own organization):
+
+`$settings = @{
+$($requiredAttributesNotSet.GetEnumerator() | ForEach-Object {
+"`t$($_.Key) = $($_.Value)" + [System.Environment]::NewLine
+})}
+
+Set-JCRConfigFile @settings
+"@
+            Write-Error "Please set the following variables with Set-JCConfigFile: $requiredAttributesNotSetString"
+        }
+    }
+}
