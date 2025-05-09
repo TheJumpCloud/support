f63d1213db36cd0f5a0772cfb36ae65a603b9621
ecourtneyjc <edward.courtney@jumpcloud.com>
2025-05-09 14:54:28 -0600

    SUP-1521 Win Log Script Fixes

--- scripts/windows/log_collection.ps1
+++ scripts/windows/log_collection.ps1
@@ -1,4 +1,4 @@
- ##################### Do Not Modify Below ######################
+##################### Do Not Modify Below ######################
 # set to $true if running via a JumpCloud command (recommended)
 $automate = $false
 
@@ -355,4 +355,4 @@ if ($automate) {
         $selectedSections = $selectedIndexes | ForEach-Object { $sections[$_] } -ErrorAction SilentlyContinue
         Gather-Logs -selections $selectedSections
     }
-} 
+}
