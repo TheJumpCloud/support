b856be6c49151052bf2476e502b6904e243b0301
Joe Workman <joe.workman@jumpcloud.com>
2025-06-02 14:13:12 -0600

    import module

--- scripts/automation/Radius/Tests/Invoke-Pester.ps1
+++ scripts/automation/Radius/Tests/Invoke-Pester.ps1
@@ -39,6 +39,7 @@ If ($PesterRunPaths) {
 }
 # Load private functions
 Write-Host ('[status]Load private functions: ' + "$PSScriptRoot/../Functions/Private/*.ps1")
+Import-Module -Name "$PSScriptRoot/../JumpCloud.Radius.psd1" -Force
 Write-Host ('[status]Load public functions: ' + "$PSScriptRoot/../Functions/Public/*.ps1")
 Get-ChildItem -Path:("$PSScriptRoot/../Functions/Private/*.ps1") -Recurse | ForEach-Object { . $_.FullName }
 
