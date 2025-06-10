80cb87c0ae1f16323870d15b6c3899c70e37bcea
Joe Workman <joe.workman@jumpcloud.com>
2025-06-10 14:30:26 -0600

    required modules

--- scripts/automation/Radius/Tests/Public/Module/Update-JCRModule.Tests.ps1
+++ scripts/automation/Radius/Tests/Public/Module/Update-JCRModule.Tests.ps1
@@ -22,6 +22,47 @@ Describe 'Module Update' -Tag "Module" {
         } else {
             Write-Host "LocalPSRepo already exists"
         }
+
+        $requiredModules = @(
+            'JumpCloud.SDK.V1',
+            'JumpCloud.SDK.V2',
+            'JumpCloud.SDK.DirectoryInsights',
+            'JumpCloud'
+        )
+        foreach ($module in $requiredModules) {
+            # Populate the local module path with the JumpCloud Module from PS Gallery
+            $latestJumpCloudModule = Find-Module -Name "$module" -Repository 'PSGallery' -ErrorAction Stop
+
+            $content = Invoke-WebRequest -UseBasicParsing -Uri "https://www.powershellgallery.com/api/v2/package/$module/$($latestJumpCloudModule.version)" `
+                -Headers @{
+                "authority"       = "www.powershellgallery.com"
+                "method"          = "GET"
+                "path"            = "/api/v2/package/$module/$($latestJumpCloudModule.version)"
+                "scheme"          = "https"
+                "accept"          = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+                "accept-encoding" = "gzip, deflate, br, zstd"
+                "accept-language" = "en-US,en;q=0.9,es;q=0.8"
+                "referer"         = "https://www.powershellgallery.com/packages/$module/$($latestJumpCloudModule.version)"
+            }
+            $binaryData = $content.Content
+            $filePath = Resolve-Path -Path $localRepoPath
+            # Save the binary data to a file
+            [System.IO.File]::WriteAllBytes("$($filePath.path)/$module.$($latestJumpCloudModule.version).zip", $binaryData)
+            # create a local module path if it does not exist
+            if (-not (Test-Path -Path "$($filePath.Path)/$module")) {
+                New-Item -Path "$($filePath.Path)/$module" -ItemType Directory
+                # create the version directory within the module path if it does not exist
+                New-Item -Path "$($filePath.Path)/$module/$($latestJumpCloudModule.version)" -ItemType Directory
+                # Unzip the file to the local module path
+                Expand-Archive -Path "$($filePath.path)/$module.$($latestJumpCloudModule.version).zip" -DestinationPath "$localRepoPath/$module/$($latestJumpCloudModule.version)" -Force
+            }
+            publish-module -Path "$($filePath.Path)/$module/$($latestJumpCloudModule.version)" -Repository $localRepoName
+            # remove the zip file after publishing
+            Remove-Item -Path "$($filePath.path)/$module.$($latestJumpCloudModule.version).zip" -Force
+            # remove the module from the local module path if it exists
+            Remove-Item -Path "$($filePath.path)/$module" -Recurse -Force -ErrorAction SilentlyContinue
+        }
+
         # get the local module path:
         $localModulePath = $env:PSModulePath.split(':')[0]
         # set the dev module path relative to this test:
@@ -62,25 +103,6 @@ Describe 'Module Update' -Tag "Module" {
         } else {
             write-host "JumpCloud.Radius module v$ModuleVersion already exists"
         }
-
-        # lastly populate the local module path with the JumpCloud Module from PS Gallery
-        $latestJumpCloudModule = Find-Module -Name "JumpCloud" -Repository 'PSGallery' -ErrorAction Stop
-
-        $content = Invoke-WebRequest -UseBasicParsing -Uri "https://www.powershellgallery.com/api/v2/package/JumpCloud/$($latestJumpCloudModule.version)" `
-            -Headers @{
-            "authority"       = "www.powershellgallery.com"
-            "method"          = "GET"
-            "path"            = "/api/v2/package/JumpCloud/$($latestJumpCloudModule.version)"
-            "scheme"          = "https"
-            "accept"          = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-            "accept-encoding" = "gzip, deflate, br, zstd"
-            "accept-language" = "en-US,en;q=0.9,es;q=0.8"
-            "referer"         = "https://www.powershellgallery.com/packages/JumpCloud/$($latestJumpCloudModule.version)"
-        }
-        $binaryData = $content.Content
-        $filePath = Resolve-Path -Path $localRepoPath
-        # Save the binary data to a file
-        [System.IO.File]::WriteAllBytes("$($filePath.path)/JumpCloud.$($latestJumpCloudModule.version).nupkg", $binaryData)
     }
     Context 'Module can be installed from the local repo' {
         It 'Install Module' {
