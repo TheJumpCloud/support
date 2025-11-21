9984661f8eaa5766a054c34caabee0f676ef2227
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2025-11-21 11:26:53 -0700

    Expand settings file modification test
    Added checks to verify JCEnvironment.location changes in addition to parallelOverride. Ensures both settings are properly updated and tested in Set-JCSettingsFile.

--- PowerShell/JumpCloud Module/Tests/Public/settings/Set-JCSettingsFile.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/settings/Set-JCSettingsFile.Tests.ps1
@@ -1,5 +1,5 @@
 Describe -Tag "JCSettingsFile" -Name "Set JCSettings Tests" {
-    it "Settings File can be modified" {
+    It "Settings File can be modified" {
         # Get previous file modified Time
         $previousModifiedDate = (Get-Item "$PSScriptRoot\..\..\..\Config.json").LastWriteTime
         # Get old config file:
@@ -10,10 +10,16 @@ Describe -Tag "JCSettingsFile" -Name "Set JCSettings Tests" {
         } else {
             Set-JCSettingsFile -parallelOverride $true
         }
+        if ($oldConfig.JCEnvironment.location -eq 'STANDARD') {
+            Set-JCSettingsFile -JCEnvironmentLocation 'EU'
+        } else {
+            Set-JCSettingsFile -JCEnvironmentLocation 'STANDARD'
+        }
         # Get modified config file:
         $modifiedConfig = Get-JCSettingsFile
         # Check that the setting was modified
         $oldConfig.parallel.override | Should -Not -Be $modifiedConfig.parallel.override
+        $oldConfig.JCEnvironment.location | Should -Not -Be $modifiedConfig.JCEnvironment.location
         # Get last modified date, this should be different from the file at the start of the test
         $lastModifiedDate = (Get-Item "$PSScriptRoot\..\..\..\Config.json").LastWriteTime
         # Test that new config file is actually modified
