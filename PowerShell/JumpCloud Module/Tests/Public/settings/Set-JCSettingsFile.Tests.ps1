caa2a24511278b559cf3a455671bc8ed5fb5930d
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2025-11-24 16:12:08 -0700

    Refactor and expand JCEnvironment tests
    Separated JCEnvironment location change tests into a dedicated test case and added assertions for both environment and global variables. This improves clarity and coverage of the Set-JCSettingsFile functionality.

--- PowerShell/JumpCloud Module/Tests/Public/settings/Set-JCSettingsFile.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/settings/Set-JCSettingsFile.Tests.ps1
@@ -10,20 +10,34 @@ Describe -Tag "JCSettingsFile" -Name "Set JCSettings Tests" {
         } else {
             Set-JCSettingsFile -parallelOverride $true
         }
-        if ($oldConfig.JCEnvironment.location -eq 'STANDARD') {
-            Set-JCSettingsFile -JCEnvironmentLocation 'EU'
-        } else {
-            Set-JCSettingsFile -JCEnvironmentLocation 'STANDARD'
-        }
         # Get modified config file:
         $modifiedConfig = Get-JCSettingsFile
         # Check that the setting was modified
         $oldConfig.parallel.override | Should -Not -Be $modifiedConfig.parallel.override
-        $oldConfig.JCEnvironment.location | Should -Not -Be $modifiedConfig.JCEnvironment.location
-        $env:JCEnvironment | Should -Be $modifiedConfig.JCEnvironment.location
         # Get last modified date, this should be different from the file at the start of the test
         $lastModifiedDate = (Get-Item "$PSScriptRoot\..\..\..\Config.json").LastWriteTime
         # Test that new config file is actually modified
         $lastModifiedDate | Should -BeGreaterThan $previousModifiedDate
+    }
+
+    It 'Changes JCEnvironment' {
+        # Get old config file:
+        $oldConfig = Get-JCSettingsFile
+        # Set JCEnvironment to EU
+        Set-JCSettingsFile -JCEnvironmentLocation 'EU'
+        # Get modified config file:
+        $modifiedConfig = Get-JCSettingsFile
+        # Check that the setting was modified
+        $oldConfig.JCEnvironment.Location | Should -Not -Be $modifiedConfig.JCEnvironment.Location
+        $modifiedConfig.JCEnvironment.Location | Should -Be 'EU'
+        # Check that env variable is set
+        $env:JCEnvironment | Should -Be 'EU'
+        $global:JCEnvironment | Should -Be 'EU'
+
+        # Revert changes
+        Set-JCSettingsFile -JCEnvironmentLocation 'STANDARD'
+        # Check that env variable is set
+        $env:JCEnvironment | Should -Be 'STANDARD'
+        $global:JCEnvironment | Should -Be 'STANDARD'
     }
 }
