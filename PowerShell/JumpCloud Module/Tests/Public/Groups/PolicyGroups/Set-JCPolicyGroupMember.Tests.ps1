99708498617be05bdb988e03c329b1aff3b34ee5
Joe Workman <joe.workman@jumpcloud.com>
2026-04-28 14:57:30 -0600

    missing tag

--- PowerShell/JumpCloud Module/Tests/Public/Groups/PolicyGroups/Set-JCPolicyGroupMember.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Groups/PolicyGroups/Set-JCPolicyGroupMember.Tests.ps1
@@ -1,4 +1,4 @@
-Describe "Get-JCPolicyGroup" {
+Describe -Tag:('JcPolicyGroup') "Get-JCPolicyGroup" {
     It "Should import the function" -Skip "Test not implemented yet" {
         # Placeholder test
     }
