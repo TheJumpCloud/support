83c03240e0581474cc33474541d2a4faeee14a54
Junior Almeida <junior.almeida@jumpcloud.com>
2026-06-10 15:59:00 -0300

    test: fix missing closing brace in device update template test

--- PowerShell/JumpCloud Module/Tests/Public/Utilities/CSV_Import/New-JCDeviceUpdateTemplate.Tests.ps1
+++ PowerShell/JumpCloud Module/Tests/Public/Utilities/CSV_Import/New-JCDeviceUpdateTemplate.Tests.ps1
@@ -1,10 +1,14 @@
 Describe -Tag:('JCDeviceFromCSV') 'New-JCDeviceUpdateTemplate' {
-    BeforeAll {  }
+    BeforeAll { }
+
     It "Forcefully creates a CSV Import Template" {
         New-JCDeviceUpdateTemplate -Force
+
         $items = Get-ChildItem -Path $PWD | Where-Object { $_.FullName -Match "JCDeviceUpdateImport*" }
         $items | Should -Exist
         $items | ForEach-Object { Remove-Item -Path $_.FullName }
+    }
+
     It "Creates a CSV Import Template with custom attributes" {
         New-JCDeviceUpdateTemplate -Force
 
@@ -15,6 +19,5 @@ Describe -Tag:('JCDeviceFromCSV') 'New-JCDeviceUpdateTemplate' {
         $templateContent | Get-Member -Name "NumberOfCustomAttributes" | Should -Not -BeNullOrEmpty
 
         $items | ForEach-Object { Remove-Item -Path $_.FullName }
-    }
     }
 }
