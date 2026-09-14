5480e5041ab98b3768298801be6e79d830b90337
lucasmendes-jc <lucas.mendes@jumpcloud.com>
2026-09-14 10:17:10 -0300

    fix Get-JCUserGroup pipped to Get-JCAssociation

--- PowerShell/JumpCloud Module/Public/Association/Remove-JCAssociation.ps1
+++ PowerShell/JumpCloud Module/Public/Association/Remove-JCAssociation.ps1
@@ -1,47 +1,66 @@
-Function Remove-JCAssociation {
+function Remove-JCAssociation {
     [CmdletBinding(DefaultParameterSetName = 'ById')]
-    Param(
+    param(
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'The type of the object.')][ValidateNotNullOrEmpty()][ValidateSet('command', 'ldap_server', 'policy', 'application', 'radius_server', 'system_group', 'system', 'user_group', 'user', 'g_suite', 'office_365')][Alias('TypeNameSingular')][System.String]$Type
         , [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Bypass user prompts and dynamic ValidateSet.')][ValidateNotNullOrEmpty()][Switch]$Force
     )
-    DynamicParam {
+    dynamicparam {
         $Action = 'remove'
-        $RuntimeParameterDictionary = If ($Type) {
+        $RuntimeParameterDictionary = if ($Type) {
             Get-DynamicParamAssociation -Action:($Action) -Force:($Force) -Type:($Type)
-        } Else {
+        } else {
             Get-DynamicParamAssociation -Action:($Action) -Force:($Force)
         }
-        Return $RuntimeParameterDictionary
+        return $RuntimeParameterDictionary
     }
-    Begin {
+    begin {
         Connect-JCOnline -force | Out-Null
         # Debug message for parameter call
         $PSBoundParameters | Out-DebugParameter | Write-Debug
         $Results = @()
     }
-    Process {
+    process {
         # For DynamicParam with a default value set that value and then convert the DynamicParam inputs into new variables for the script to use
         Invoke-Command -ScriptBlock:($ScriptBlock_DefaultDynamicParamProcess) -ArgumentList:($PsBoundParameters, $PSCmdlet, $RuntimeParameterDictionary) -NoNewScope
         # Create hash table to store variables
         $FunctionParameters = [ordered]@{}
-        # Add input parameters from function in to hash table and filter out unnecessary parameters
-        $PSBoundParameters.GetEnumerator() | Where-Object { -not [System.String]::IsNullOrEmpty($_.Value) } | ForEach-Object { $FunctionParameters.Add($_.Key, $_.Value) | Out-Null }
-        # Add action
-        ($FunctionParameters).Add('Action', $Action) | Out-Null
-        # Skip dynamic group associations - resources cannot be manually removed from dynamic groups
-        $TargetTypeValue = If ($FunctionParameters['TargetType'] -is [array]) {
-            $FunctionParameters['TargetType'] | Select-Object -First 1
-        } Else {
-            $FunctionParameters['TargetType']
+
+        #check if $PSBoundParameters is array of objects or a single object and convert to array if not
+        if ($PSBoundParameters -isnot [array]) {
+            $jc_list = @($PSBoundParameters)
         }
-        If (Test-JCDynamicGroupMembership -TargetType:($TargetTypeValue) -TargetId:($FunctionParameters['TargetId']) -TargetName:($FunctionParameters['TargetName'])) {
-            Write-Verbose ('Skipping removal of association with dynamic group. Resources cannot be manually removed from dynamic groups.')
-            Return
+
+        $jc_list | ForEach-Object {
+            # Add input parameters from function in to hash table and filter out unnecessary parameters
+            $_.GetEnumerator() | Where-Object { -not [System.String]::IsNullOrEmpty($_.Value) } | ForEach-Object { $FunctionParameters.Add($_.Key, $_.Value) | Out-Null }
+            # Add action
+            ($FunctionParameters).Add('Action', $Action) | Out-Null
+
+            @('TargetType', 'TargetId', 'Id', 'Type') | ForEach-Object {
+                if (-not $FunctionParameters.Contains($_)) {
+                    throw "Missing required parameter: $_"
+                }
+
+                # If the value is an array, extract the first item and overwrite the hashtable value
+                if ($FunctionParameters[$_] -is [array]) {
+                    $FunctionParameters[$_] = $FunctionParameters[$_] | Select-Object -First 1
+                }
+            }
+            $CleanType = $FunctionParameters['type']
+            $CleanTargetId = $FunctionParameters['targetid']
+            $CleanId = $FunctionParameters['id']
+
+            Write-Host $FunctionParameters
+            if (Test-JCDynamicGroupMembership -Type:($CleanType) -TargetId:($CleanTargetId) -Id:($CleanId)) {
+                Write-Verbose ('Skipping removal of association with dynamic group. Dynamic associations cannot be manually removed from dynamic groups.')
+                return
+            }
+
+            # Run the command
+            $Results += Invoke-JCAssociation @FunctionParameters
         }
-        # Run the command
-        $Results += Invoke-JCAssociation @FunctionParameters
     }
-    End {
-        Return $Results
+    end {
+        return $Results
     }
 }
